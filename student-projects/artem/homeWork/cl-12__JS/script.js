586dfff7b71e17dc773f26291ced925a7aa5e287
artёm rudoy <art.rudy@gmail.com>
2023-08-16 23:34:13 +0300

    change of aappearence

--- student-projects/artem/homeWork/cl-12__JS/script.js
+++ student-projects/artem/homeWork/cl-12__JS/script.js
@@ -3,14 +3,11 @@ const sideA = Number(window.prompt("Please enter the length of first leg of a ri
 const sideB = Number(window.prompt("Please enter the length of second leg of a right triangle"));
 // console.log(sideB, typeof(sideB));
 
-if ( isNaN(sideA) || isNaN(sideB)){
+if (isNaN(sideA) || isNaN(sideB)) {
     alert('Please enter numbers only....');
 } else if (sideA <= 0 || sideB <= 0) {
-    alert('Such triangle is not exist');     
+    alert('Such triangle is not exist');
 } else {
     const result = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
     alert(`The length of hypotenuse is to ${result}`);
 }
-
-
-
