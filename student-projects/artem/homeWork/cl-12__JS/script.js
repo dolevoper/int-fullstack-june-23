fb799d8ccaa0498ffdbc3cdc32b34c1da6cd1158
artёm rudoy <art.rudy@gmail.com>
2023-08-16 23:28:49 +0300

    the sequence of checking data changed

--- student-projects/artem/homeWork/cl-12__JS/script.js
+++ student-projects/artem/homeWork/cl-12__JS/script.js
@@ -2,11 +2,11 @@ const sideA = Number(window.prompt("Please enter the length of first leg of a ri
 // console.log(sideA, typeof(sideA));
 const sideB = Number(window.prompt("Please enter the length of second leg of a right triangle"));
 // console.log(sideB, typeof(sideB));
-if (sideA <= 0 || sideB <= 0) {
-    alert('Such triangle is not exist');     
-}
-else if ( isNaN(sideA) || isNaN(sideB)){
+
+if ( isNaN(sideA) || isNaN(sideB)){
     alert('Please enter numbers only....');
+} else if (sideA <= 0 || sideB <= 0) {
+    alert('Such triangle is not exist');     
 } else {
     const result = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
     alert(`The length of hypotenuse is to ${result}`);
