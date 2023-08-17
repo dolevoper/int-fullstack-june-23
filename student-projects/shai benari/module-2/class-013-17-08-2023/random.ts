ec428bcac645ab62616999e256c377496ef60544
shai-benari <s.benari1962@gmail.com>
2023-08-18 00:56:32 +0300

    home work stage 1

---
+++ student-projects/shai benari/module-2/class-013-17-08-2023/random.ts
@@ -0,0 +1,6 @@
+
+const randomnum =Math.floor(Math.random() * 11);
+alert(randomnum);
+const gussnum = Number(prompt("guss a number betwin 0 an 10"));
+if(randomnum===gussnum){alert("your guss is right")}
+else{alert("your guss is wrong")}
