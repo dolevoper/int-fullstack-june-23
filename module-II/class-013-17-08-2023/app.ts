75603384e9d292ac2dbdc5d075232f631c2f82ac
shai-benari <s.benari1962@gmail.com>
2023-08-18 00:14:23 +0300

    moving files module-1

--- module-II/class-013-17-08-2023/app.ts
+++ module-II/class-013-17-08-2023/app.ts
@@ -7,12 +7,14 @@ const a = Number(prompt("Enter length of leg a"));
 const b = Number(prompt("Enter length of leg b"));
 const cSquared = a ** 2 + b ** 2;
 const c = Math.sqrt(cSquared);
+if(a<0||b<0){alert("The side of the triangale canot be negative")}
+else{alert("the c side is: " + c)}
 
-if (isNaN(c)) {
-    alert("Please enter valid numbers.");
-} else {
-    alert("The length of the hypotenuse is: " + c);
-}
+//  if (isNaN(c)) {
+//     alert("Please enter valid numbers.");
+//  } else {
+//     alert("The length of the hypotenuse is: " + c);
+//  }
 
 // comparison operators: >, <, ===, <=, >=, !== (, ==, !=)
 // number -> number -> boolean
