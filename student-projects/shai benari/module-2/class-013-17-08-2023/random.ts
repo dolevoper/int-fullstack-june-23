c03055ed58f1abc2a32e41a1c3a531b87087324f
shai-benari <s.benari1962@gmail.com>
2023-08-18 01:20:31 +0300

    random stage 2

--- student-projects/shai benari/module-2/class-013-17-08-2023/random.ts
+++ student-projects/shai benari/module-2/class-013-17-08-2023/random.ts
@@ -1,6 +1,8 @@
 
-const randomnum =Math.floor(Math.random() * 11);
+const randomnum = Math.floor(Math.random() * 10) + 1;
 alert(randomnum);
-const gussnum = Number(prompt("guss a number betwin 0 an 10"));
+const gussnum = Number(prompt("guss a number betwin 1 and 10"));
 if(randomnum===gussnum){alert("your guss is right")}
+else if(gussnum<=1 || gussnum>10){alert("your guss is out of range")}
 else{alert("your guss is wrong")}
+
