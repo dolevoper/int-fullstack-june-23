0c6fdf5d17f96ef0c3f39ca6d06eb844458f2c85
shai-benari <s.benari1962@gmail.com>
2023-08-18 02:17:25 +0300

    random stage 3

--- student-projects/shai benari/module-2/class-013-17-08-2023/random.ts
+++ student-projects/shai benari/module-2/class-013-17-08-2023/random.ts
@@ -5,4 +5,6 @@ const gussnum = Number(prompt("guss a number betwin 1 and 10"));
 if(randomnum===gussnum){alert("your guss is right")}
 else if(gussnum<=1 || gussnum>10){alert("your guss is out of range")}
 else{alert("your guss is wrong")}
+if(gussnum<randomnum){alert("your guss is less then the number")}
+if(gussnum>randomnum){alert("your guss is bigger then the number")}
 
