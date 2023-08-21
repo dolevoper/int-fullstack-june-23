d2f3bbcb629d8c4268c6977facc8d29d5152f9f9
Internet21 <meir21@hotmail.com>
2023-08-21 21:51:44 +0300

    ver_1

--- student-projects/meir/class_014/Average/js/scripts.ts
+++ student-projects/meir/class_014/Average/js/scripts.ts
@@ -1,1 +1,17 @@
-alert(11);
+/*
+1. Average grade calculator:
+   - Ask the user how many grades he would like to enter
+   - Read all the numbers
+   - Calculate and print the average
+
+2. Re-write guess my number using loops and `let` variables
+   - Decide the number of guesses according to level
+   - Or ask the user for how many guesses
+   - Or infinite guesses
+   
+3. Choose any program we previously built and add number validation in a loop
+   - Invalid number - show message and ask to enter a number again
+   - Cancel - stop the program
+
+\*\* Submit home project through a PR!
+*/
