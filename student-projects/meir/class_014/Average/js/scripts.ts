11f37f598667fba1419ee3439010eebdbd22dcc8
Internet21 <meir21@hotmail.com>
2023-08-22 07:24:17 +0300

    ver_1

--- student-projects/meir/class_014/Average/js/scripts.ts
+++ student-projects/meir/class_014/Average/js/scripts.ts
@@ -4,15 +4,6 @@
    - Read all the numbers
    - Calculate and print the average
 
-2. Re-write guess my number using loops and `let` variables
-   - Decide the number of guesses according to level
-   - Or ask the user for how many guesses
-   - Or infinite guesses
-
-3. Choose any program we previously built and add number validation in a loop
-   - Invalid number - show message and ask to enter a number again
-   - Cancel - stop the program
-
 \*\* Submit home project through a PR!
 */
 
