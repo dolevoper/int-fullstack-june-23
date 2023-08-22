49805adafa4c74cbeb9fb84b967f5995a57ea2cd
Internet21 <meir21@hotmail.com>
2023-08-22 07:56:06 +0300

    ver_1

--- student-projects/meir/class_014/Average/js/scripts.ts
+++ student-projects/meir/class_014/Average/js/scripts.ts
@@ -11,7 +11,7 @@ initAvg();
 
 function initAvg(){
 
-    const gradesNum = Number(prompt("How many grades you would like to enter"));
+    const gradesNum = prompt("How many grades you would like to enter","");
 
     const Status = valiadte(gradesNum);
     
@@ -19,53 +19,56 @@ function initAvg(){
     if(Status === "exit"){
         alert("You pushed on 'Cancel' buttom for exit");
         return;
-    }else{
-        calcAvg(gradesNum,Status);
     }
-
-}
-
-function calcAvg(gradesNum:number, Status:boolean){
-    
-    let allNums = 0;
 
     if(Status){
-        
-        for(let i=1; i <= gradesNum; i++){
-            const num = Number(prompt("Enter number now"));
-            const avg = valiadte(num);
-
-            if(avg){
-                //alert("valid number");
-                allNums = allNums + num;
-            }else{
-                alert("Not valid number, try again");
-                initAvg();
-            }
-            
-        }
-
-        alert("Aveage is: " + allNums / gradesNum);
+        calcAvg(gradesNum);
     }else{
-        alert("Yot enter NOT valid number, Try again");
+        alert("You need to enter valid number, try again");
         initAvg();
     }
+
 }
 
-function valiadte(myGradesNum:number){
+function calcAvg(gradesNum){
+    
+    let allNums = 0;
+        
+    for(let i=1; i <= gradesNum; i++){
+        const num = Number(prompt("Enter number now"));
+        const avg = valiadte(num);
 
-    if(myGradesNum === 0){
-        //alert("You push on cancel);
+        if(avg){
+            //alert("valid number");
+            allNums = allNums + num;
+        }else{
+            alert("Not valid number, try again");
+            initAvg();
+        }
+        
+    }
+
+    alert("The aveage is: " + allNums / gradesNum);
+    
+}
+
+function valiadte(myGradesNum){
+
+    if(myGradesNum === null){
+        //alert("You pressed on cancel for exit");
         return "exit";
     }else if(isNaN(myGradesNum)){
-        //alert("הוזן קלט שאינו מספר");
+        alert("Not a number");
         return false;
-    }else if(myGradesNum <= 0){
-        //alert("you enter number less from one");
+    }else if(myGradesNum < 0){
+        alert("The number less from 0");
         return false;
-    } else {
-        //alert("הוזן מספר חיובי");
+    }else if(myGradesNum === ""){
+        alert("The input is empty");
+        return false;
+    }else{
+        //alert("OK, You Enter valid number");
         return true;
     }
-    
+
 }
