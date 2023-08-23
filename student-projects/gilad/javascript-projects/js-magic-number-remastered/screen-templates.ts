84ca9b88a9e6cc9fae739051a651f0e9537102af
BraveVlad <giladvladpinker@gmail.com>
2023-08-23 10:58:30 +0300

    Class 14 solution with difficulty object

--- student-projects/gilad/javascript-projects/js-magic-number-remastered/screen-templates.ts
+++ student-projects/gilad/javascript-projects/js-magic-number-remastered/screen-templates.ts
@@ -1,17 +1,18 @@
 
 export const messageExit = "Exiting Game.";
 
-export const messageGuessEmpty = "Did you even guess?";
-export const messageNotNumber = "That is not a number!";
-export const messageNotWhole = " is not a whole number!";
-export const messageNotInRange = "is out of range!";
+export const hintGuessEmpty = "Did you even guess?";
+export const hintNotNumber = "That is not a number!";
+export const hintNotWhole = " is not a whole number!";
+export const hintNotInRange = "is out of range!";
 
-export const messageGuessAbove = "is above the magic number";
-export const messageGuessBelow = "is below the magic number";
+export const hintGuessAbove = "is above the magic number";
+export const hintGuessBelow = "is below the magic number";
 
 export const messageGameWon = "WIN! You outsmared Houdini!";
 export const messageGameLost = "GAME OVER! Houdini outsmared you, again.";
 
+export const messageRepeatGame = "Would you like to play again?"
 
 export const messageGameStart = `
 ====================================
@@ -29,22 +30,5 @@ export const messageGameRules = `                                         • Ga
 ================= Are you ready? =================
 `;
 
-export function generateDifficultyMenu(hint: string, rangeEasy: number, rangeHard: number, rangeImpossible: number) {
-return `
-==========================================
-|                                Please enter game difficulty:                         |
-|                                 1) Easy             -         ${rangeEasy}                               |
-|                                 2) Hard            -         ${rangeHard}                             |
-|                                 3) Impossible   -         ${rangeImpossible}                           |
-==========================================
-${hint}
-`;}
 
-export function generateGameScreen(gameDifficulty: string, guessesLeft: number, guessesTotal: number , gameRange: number, houdiniHint: string) {
-return `======== Difficulty: ${gameDifficulty} | Guesses: ${guessesLeft} / ${guessesTotal} ========
 
-                         Guess the magic number between
-                                             1 - ${gameRange}
-
-${ houdiniHint === "" ? "" : `Houdini says: "${houdiniHint}"` }
-`;}
