d4fd35a9ee98c03973c3d1f9bf0286b2e3653983
Internet21 <meir21@hotmail.com>
2023-08-24 21:03:50 +0300

    ver_1

--- student-projects/meir/class_015/functionsPractise/js/scripts.ts
+++ student-projects/meir/class_015/functionsPractise/js/scripts.ts
@@ -1,1 +1,52 @@
-alert("asdf");
+/*
+Option I
+Write the following function:
+
+printTo
+Input - a number
+Output - print all the whole numbers from 1 to the input number
+isOdd
+Input - a number
+Output - return true if the number is odd, false otherwise
+isEven
+Input - a number
+Output - return true if the number is even, false otherwise
+isPalindrome
+Input - a string
+Output - return true if the string is a palindrome, false otherwise
+improved isPalindrome
+Input - a string or a number
+Output - return true if the input is a palindrome, false otherwise
+safeDiv
+Input - 2 numbers
+Output - return null if the second number is 0, the ratio between the numbers otherwise
+improved safeDiv
+Input - 2 numbers
+Output - the ratio between the numbers
+If the second number is 0, the program should not compile
+fizzBuzz
+Input - a number
+Output - print all the whole numbers from 1 to the input.
+If the number has a 5 or is divisible by 5, print "fizz" instead.
+If the number has a 7 or is divisible by 7, print "buzz" instead.
+If it's both, print "fizzbuzz"
+
+Option II
+Improved pythagoras.
+
+Ask the user if he wants to calculate the hypotenuse of know if 3 values are a valid right triangle edges.
+Run the correct program - divide to functions, validate inputs etc...
+
+Option III
+Improved guess my number
+
+Make sure you have all the required input validations
+User should have infinite guesses
+Separate to functions - some functions are used to avoid repeating code (like validation), and some are used for organization.
+Add a main menu
+Options: Play, Choose difficulty, Quit
+When player looses/wins/quits - go back to main menu
+Don't ask for the difficulty in the beginning. Default is normal, player can change the difficulty from the main menu
+*/
+
+alert(11);
