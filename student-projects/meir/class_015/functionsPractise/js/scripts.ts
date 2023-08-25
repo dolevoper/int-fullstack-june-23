b06e0876f90db2e3811aa90af6ab11ba8b8da8a8
Internet21 <meir21@hotmail.com>
2023-08-26 01:17:10 +0300

    ver_1

--- student-projects/meir/class_015/functionsPractise/js/scripts.ts
+++ student-projects/meir/class_015/functionsPractise/js/scripts.ts
@@ -5,25 +5,32 @@ Write the following function:
 printTo
 Input - a number
 Output - print all the whole numbers from 1 to the input number
+
 isOdd
 Input - a number
 Output - return true if the number is odd, false otherwise
+
 isEven
 Input - a number
 Output - return true if the number is even, false otherwise
+
 isPalindrome
 Input - a string
 Output - return true if the string is a palindrome, false otherwise
+
 improved isPalindrome
 Input - a string or a number
 Output - return true if the input is a palindrome, false otherwise
+
 safeDiv
 Input - 2 numbers
 Output - return null if the second number is 0, the ratio between the numbers otherwise
+
 improved safeDiv
 Input - 2 numbers
 Output - the ratio between the numbers
 If the second number is 0, the program should not compile
+
 fizzBuzz
 Input - a number
 Output - print all the whole numbers from 1 to the input.
@@ -31,22 +38,229 @@ If the number has a 5 or is divisible by 5, print "fizz" instead.
 If the number has a 7 or is divisible by 7, print "buzz" instead.
 If it's both, print "fizzbuzz"
 
-Option II
-Improved pythagoras.
-
-Ask the user if he wants to calculate the hypotenuse of know if 3 values are a valid right triangle edges.
-Run the correct program - divide to functions, validate inputs etc...
-
-Option III
-Improved guess my number
-
-Make sure you have all the required input validations
-User should have infinite guesses
-Separate to functions - some functions are used to avoid repeating code (like validation), and some are used for organization.
-Add a main menu
-Options: Play, Choose difficulty, Quit
-When player looses/wins/quits - go back to main menu
-Don't ask for the difficulty in the beginning. Default is normal, player can change the difficulty from the main menu
 */
 
-alert(112222);
+initMyChoosen()
+
+function initMyChoosen() {
+
+    const mySelected = Number(prompt("Select the function you want:\n1 - printTo\n2 - isOdd\n3 - isEven\n4 - isPalindrome\n5 - improved isPalindrome\n6 - safeDiv\n7 - improved safeDiv\n8 - fizzBuzz", ""));
+
+    switch (mySelected) {
+        case 1:
+            //alert("11111111");
+            printTo();
+            break;
+        case 2:
+            //alert("2222222");
+            isOdd();
+            break;
+        case 3:
+            //alert("3333333");
+            isEven();
+            break;
+        case 4:
+            //alert("444444444");
+            isPalindrome();
+            break;
+        case 5:
+            //alert("55555555");
+            improvedIsPalindrome();
+            break;
+        case 6:
+            //alert("6666666666");
+            safeDiv();
+            break;
+        case 7:
+            //alert("77777777");
+            improvedSafeDiv();
+            break;
+        case 8:
+            //alert("888888888");
+            fizzBuzz();
+            break;
+        default:
+            break;
+    }
+}
+
+function printTo() {
+    const myNumberPrintTo = Number(prompt("Enter number between 1 and ... ", ""));
+    printNumbers(myNumberPrintTo);
+}
+
+function printNumbers(myNumber: number) {
+    for (let i = 1; i <= myNumber; i++) {
+        alert(i);
+    }
+}
+
+/**********************************************************/
+
+function isOdd() {
+    const myIsOdd = Number(prompt("Enter number between 1 and check if odd ", ""));
+    printMyOdd(myIsOdd);
+}
+
+function printMyOdd(myIsOdd: number) {
+    const myNumberIsOdd = (myIsOdd % 2) ? true : false;
+    alert(myNumberIsOdd);
+}
+
+/**********************************************************/
+
+function isEven() {
+    const myIsEven = Number(prompt("Enter number between 1 and check if even ", ""));
+    printMyEven(myIsEven);
+}
+
+function printMyEven(myIsEven: number) {
+    const myNumberIsEven = (myIsEven % 2) ? false : true;
+    alert(myNumberIsEven);
+}
+
+/**********************************************************/
+
+function isPalindrome() {
+    const myPalindrome = prompt("Eneter string and check if palindrome", "");
+    checkmyPalindrome(myPalindrome);
+}
+
+function checkmyPalindrome(myPalindrome: string | null) {
+
+    let yesOrNo = false;
+    const palindromeLength = Number(myPalindrome?.length);
+
+    for (let i = 1; i <= palindromeLength; i++) {
+
+        const fromStart = myPalindrome?.charAt(i - 1);
+        const fromEnd = myPalindrome?.charAt(palindromeLength - i)
+
+        if (fromStart === fromEnd) {
+            yesOrNo = true;
+        } else {
+            yesOrNo = false;
+            break;
+        }
+
+    }
+
+    if (yesOrNo) {
+        alert("The string is pilandrom");
+    } else {
+        alert("The string is NOT pilandrom");
+    }
+
+}
+
+/**********************************************************/
+
+function improvedIsPalindrome() {
+    const myImproveDisPalindrome = prompt("Eneter string and check if palindrome", "");
+    checkImprovedIsPalindrome(myImproveDisPalindrome);
+}
+
+function checkImprovedIsPalindrome(myImproveDisPalindrome) {
+
+    let yesOrNo = false;
+    const palindromeLength = Number(myImproveDisPalindrome?.length);
+
+    for (let i = 1; i <= palindromeLength; i++) {
+
+        const fromStart = myImproveDisPalindrome?.charAt(i - 1);
+        const fromEnd = myImproveDisPalindrome?.charAt(palindromeLength - i)
+
+        if (fromStart === fromEnd) {
+            yesOrNo = true;
+        } else {
+            yesOrNo = false;
+            break;
+        }
+
+    }
+
+    if (yesOrNo) {
+        alert(yesOrNo);
+    } else {
+        alert(yesOrNo);
+    }
+
+}
+
+/**********************************************************/
+
+function safeDiv() {
+    const firstNumber = Number(prompt("Enter the first number", ""));
+    const secondNumber = Number(prompt("Enter the second number", ""));
+
+    if (secondNumber <= 0) {
+        alert("The second number less or equal 0");
+        return null;
+    } else {
+        clacNumbers(firstNumber, secondNumber);
+    }
+}
+
+function clacNumbers(firstNumber: number, secondNumber: number) {
+    const myRatio = firstNumber / secondNumber;
+    alert("The ratio between the numbers is: " + firstNumber + ":" + secondNumber + "\nand after it's: " + myRatio.toFixed(3));
+}
+
+/**********************************************************/
+
+function improvedSafeDiv() {
+    const firstNumberSafe = Number(prompt("Enter the first number", ""));
+    const secondNumberSafe = Number(prompt("Enter the second number", ""));
+
+    if (secondNumberSafe <= 0) {
+        alert("The second number less or equal 0, so EXIT");
+        return false;
+    } else {
+        clacNumbersSafe(firstNumberSafe, secondNumberSafe);
+    }
+
+}
+
+function clacNumbersSafe(firstNumberSafe: number, secondNumberSafe: number) {
+    const myRatio = firstNumberSafe / secondNumberSafe;
+    alert("The ratio between the numbers is: " + firstNumberSafe + ":" + secondNumberSafe + "\nand after it's: " + myRatio.toFixed(3));
+}
+
+/**********************************************************/
+
+function fizzBuzz() {
+
+    const myFizzBuzzNumber = Number(prompt("Enter number from 1 to ..."));
+
+    if (myFizzBuzzNumber <= 0) {
+        alert("The number must start from 1, try again");
+        fizzBuzz();
+    } else {
+        printTheNumbers(myFizzBuzzNumber);
+    }
+}
+
+function printTheNumbers(myFizzBuzzNumber: number) {
+
+    const myArray: any = [];
+    const myFizzBuzz = "fizzbuzz";
+    const myFizz = "fizz";
+    const myBuzz = "buzz";
+
+    for (let i = 1; i <= myFizzBuzzNumber; i++) {
+
+        if (i % 5 === 0 && i % 7 === 0) {
+            myArray.push(myFizzBuzz);
+        } else if (i % 5 === 0) {
+            myArray.push(myFizz);
+        } else if (i % 7 === 0) {
+            myArray.push(myBuzz);
+        } else {
+            myArray.push(i);
+        }
+
+    }
+
+    alert(myArray.join(", "));
+
+}
