9432513e2177da3291466204513f4914108e1491
eden shaviv <edenmishel@gmail.com>
2023-08-26 10:43:05 +0300

    eden

---
+++ student-projects/eden/class15/app.ts
@@ -0,0 +1,11 @@
+alert ("welcome to your grade")
+const walcome = prompt ("how many grades would you like to enter?")
+const firstNumber = Number (prompt("please enter a number"))
+const seconedNumber = Number (prompt("please enter a number"))
+const thirdNubmer = Number (prompt("please enter a number"))
+
+alert (firstNumber + seconedNumber + thirdNubmer)
+
+
+
+
