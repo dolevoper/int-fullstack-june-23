da202634857fde65f2598386d0617de170fe4291
Ravivos1 <ravivos5000@gmail.com>
2023-08-26 11:25:59 +0300

    cal fixes

--- student-projects/ravivos1/module-II/class14/averageCalculator/cal.ts
+++ student-projects/ravivos1/module-II/class14/averageCalculator/cal.ts
@@ -1,19 +1,25 @@
-const studentsGrades = Number (prompt("How many grades you want to enter?"))
-if (isNaN (studentsGrades) || studentsGrades===null || studentsGrades <= 0) {
+const numberOfStudents = Number (prompt("How many grades you want to enter?"))
+if (isNaN (numberOfStudents) || numberOfStudents === null || numberOfStudents <= 0) {
     alert("You didn't entered a valid number, please refresh the page and enter the number of grades you want to enter");
 }
 else {
     let sumGrades = 0;
     let average = 0;
-    for (let student = 1; student <= studentsGrades; student++) {
-        const grade = Number(prompt("Please enter the grade of student numer " + student));
-        sumGrades = sumGrades + grade;
-        const studentsleft = studentsGrades - student;
-        average = sumGrades/student;
-        if (student === studentsGrades) {
-            alert("Grade enterd!\nIt was the last grade, the average is " + average)
+    for (let student = 1; student <= numberOfStudents; student++) {
+        const grade = Number(prompt("Please enter the grade (0-100) for student numer " + student));
+        if (isNaN (grade) || grade===null || grade < 0 || grade > 100) {
+            alert("You didn't entered a valid number, student doesn't count");
+            student --;
         }
-        else {alert ("Grade enterd!\nStudents left " + studentsleft + "\nPlease enter the next grade")
+        else {
+            sumGrades = sumGrades + grade;
+            const studentsleft = numberOfStudents - student;
+            average = sumGrades/student;
+            if (student === numberOfStudents) {
+                alert("Grade enterd!\nIt was the last grade, the average is " + average)
+            }
+            else {alert ("Grade enterd!\nStudents left " + studentsleft + "\nPlease continue to enter the next grade")
+            }
         }
 
     }
