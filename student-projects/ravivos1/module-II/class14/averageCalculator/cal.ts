bd558fac7a29405a7b6d1ab05725decaa84dca98
Ravivos1 <ravivos5000@gmail.com>
2023-08-27 00:46:04 +0300

    average cal - omer fix

--- student-projects/ravivos1/module-II/class14/averageCalculator/cal.ts
+++ student-projects/ravivos1/module-II/class14/averageCalculator/cal.ts
@@ -5,17 +5,18 @@ if (isNaN (numberOfStudents) || numberOfStudents === null || numberOfStudents <=
 else {
     let sumGrades = 0;
     let average = 0;
-    for (let student = 1; student <= numberOfStudents; student++) {
-        const grade = Number(prompt("Please enter the grade (0-100) for student numer " + student));
+    for (let student = 0; student < numberOfStudents; student++) {
+        const realStudentNumber = student + 1;
+        const grade = Number(prompt("Please enter the grade (0-100) for student numer " + realStudentNumber));
         if (isNaN (grade) || grade===null || grade < 0 || grade > 100) {
             alert("You didn't entered a valid number, student doesn't count");
             student --;
         }
         else {
             sumGrades = sumGrades + grade;
-            const studentsleft = numberOfStudents - student;
-            average = sumGrades/student;
-            if (student === numberOfStudents) {
+            const studentsleft = numberOfStudents - realStudentNumber;
+            average = sumGrades/numberOfStudents;
+            if (realStudentNumber === numberOfStudents) {
                 alert("Grade enterd!\nIt was the last grade, the average is " + average)
             }
             else {alert ("Grade enterd!\nStudents left " + studentsleft + "\nPlease continue to enter the next grade")
