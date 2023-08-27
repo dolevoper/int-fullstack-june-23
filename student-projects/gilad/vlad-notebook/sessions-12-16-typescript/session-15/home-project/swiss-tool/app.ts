6842e259de652ebdd08e20acde4acd36e3ba7153
BraveVlad <giladvladpinker@gmail.com>
2023-08-27 22:41:48 +0300

    seperated function's tests to another file + styleguide formatting.

--- student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-15/home-project/swiss-tool/app.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-15/home-project/swiss-tool/app.ts
@@ -17,11 +17,6 @@ function printTo(targetNumber: number) {
     console.log(`${currentNumer}`);
   }
 }
-/* Tests:
-printTo(10);
-printTo(0);
-printTo(-5);
- */
 
 function isEven(targetNumber: number) {
   return targetNumber % 2 == 0 ? true : false;
@@ -30,12 +25,6 @@ function isEven(targetNumber: number) {
 function isOdd(targetNumber: number) {
   return !isEven(targetNumber);
 }
-/* Tests:
-print(`number 2 is even: ${isEven(2)}`);
-print(`number 7 is even: ${isEven(7)}`);
-print(`number -2 is odd: ${isOdd(2)}`);
-print(`number -7 is odd: ${isOdd(7)}`);
-*/
 
 function isPalindrome(text: string) {
   const loweredText = text.toLowerCase();
@@ -52,40 +41,10 @@ function isImprovedPalindrome(target: string | number) {
 
   return isPalindrome(target);
 }
-/* Tests:
-  // print(`is "Hello" palindrome? ${isPalindrome("Hello")}`);
-  // print(`is "Palindrome" palindrome? ${isPalindrome("Palindrome")}`);
-  // print(`is "redivider" palindrome? ${isPalindrome("redivider")}`);
-  // print(`is "RAcecar" palindrome? ${isPalindrome("RAcecar")}`);
-  // print(`is "x" palindrome? ${isPalindrome("x")}`);
-  // print(`is "" palindrome? ${isPalindrome("")}`);
-
-  // print(`is "hello" improved palindrome? ${isImprovedPalindrome("hello")}`);
-  // print(`is "wow" improved palindrome? ${isImprovedPalindrome("wow")}`);
-  // print(`is "" improved palindrome? ${isImprovedPalindrome("")}`);
-  // print(`is "123" improved palindrome? ${isImprovedPalindrome(123)}`);
-  // print(`is "45654" improved palindrome? ${isImprovedPalindrome(45654)}`);
-  // print(`is "-505" improved palindrome? ${isImprovedPalindrome(-505)}`);
-  // print(`is "0" improved palindrome? ${isImprovedPalindrome(0)}`);
-*/
-
-/*
-- [???] improved safeDiv  /// ??? 
-  **Input** - 2 numbers  
-  **Output** - the ratio between the numbers  
-  If the second number is 0, the program should not compile
-
-*/
 
 function safeDiv(numberA: number, numberB: number) {
   return numberB === 0 ? null : numberA / numberB;
 }
-
-// function improvedSafeDiv(numberA: number, numberB: number) {}
-/* Tests:
-print(`safe division of 3 / 2 = ${safeDiv(3, 2)}`);
-print(`safe division of 5 / 0 = ${safeDiv(5, 0)}`);
-*/
 
 function fizzBuzz(target: number) {
   let fizzbuzz = "";
@@ -95,13 +54,3 @@ function fizzBuzz(target: number) {
   if (fizzbuzz === "") printTo(target);
   else return fizzbuzz;
 }
-/* Tests:
-print(`fizzbuzz number 5: ${fizzBuzz(5)}`);
-print(`fizzbuzz number 7: ${fizzBuzz(7)}`);
-print(`fizzbuzz number 21: ${fizzBuzz(21)}`);
-print(`fizzbuzz number 52: ${fizzBuzz(52)}`);
-print(`fizzbuzz number 75: ${fizzBuzz(75)}`);
-print(`fizzbuzz number 174: ${fizzBuzz(174)}`);
-print(`fizzbuzz number 175: ${fizzBuzz(175)}`);
-print(`fizzbuzz number 8: ${fizzBuzz(8)}`);
-*/
