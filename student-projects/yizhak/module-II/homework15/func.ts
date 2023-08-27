74ec1a47d5323733c3d2d5a8c56a6aec36986afd
yizhak148 <yizhak208@gmail.com>
2023-08-28 00:37:27 +0300

    done more 3 func

--- student-projects/yizhak/module-II/homework15/func.ts
+++ student-projects/yizhak/module-II/homework15/func.ts
@@ -1,6 +1,6 @@
 //function number 1
 
-function printNumber(a){
+function printNumber(a: Number){
     for (let i = 1; i < a; i++) {
         alert(i);
     }
@@ -8,3 +8,30 @@ function printNumber(a){
 printNumber(7)
 
 //function number 2
+function isOdd(x: Number){
+    if (x % 2 === 0) {
+        return true;
+      }
+      else if (isNaN(x)) {
+        return "Give a number";
+      }
+      else {
+        return false;
+      }  
+}
+isOdd(5)
+
+//function number 3
+function isEven(n: Number){
+    if (x % 2 === 1) {
+        return true;
+      }
+      else if (isNaN(n)) {
+        return "Give a number";
+      }
+      else {
+        return false;
+      }  
+}
+isEven(3)
+//function number 4
