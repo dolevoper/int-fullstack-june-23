913ba099868fb7e62f48075f3b1b9a053a89b284
artёm rudoy <art.rudy@gmail.com>
2023-08-27 22:15:30 +0300

    ratio or null function

--- student-projects/artem/homeWork/class_15_24.08/separeteFunc/script.ts
+++ student-projects/artem/homeWork/class_15_24.08/separeteFunc/script.ts
@@ -1,9 +1,19 @@
-function isPalindrome(input: string) {
-    const stringToInput = prompt("Please enter a string");
-    let reversedString = stringToInput?.split("").reverse().join("");
-    alert(
-      reversedString === stringToInput
-        ? "This string is palindrome."
-        : "This string is not palindrome."
-    );
-  }
+function isPalindrome() {
+  const stringToInput = prompt("Please enter a string");
+  let reversedString = stringToInput?.split("").reverse().join("");
+  alert(
+    reversedString === stringToInput
+      ? "This string is palindrome."
+      : "This string is not palindrome."
+  );
+}
+isPalindrome();
+
+
+function ratio() {
+  const a: number = Number(prompt("Please input first number."));
+  const b: number = Number(prompt("Please input second number."));
+  alert(b === 0 ? null : a / b);
+  return b === 0 ? null : a / b;
+}
+ratio();
