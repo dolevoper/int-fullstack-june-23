07efe6db5c1a9c1ad1b5eb425d9ac6090485936d
BraveVlad <giladvladpinker@gmail.com>
2023-08-27 12:00:58 +0300

    Added several swiss-tool function solutions

--- student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-15/home-project/swiss-tool/app.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-15/home-project/swiss-tool/app.ts
@@ -1,1 +1,67 @@
-alert("Welcome to swiss tool app")
+function print(message: string) {
+  console.log(message);
+}
+
+// [] TODO - Add support to negative numbers
+function printTo(targetNumber: number) {
+  print(`Target number: ${targetNumber}`);
+
+  console.log(targetNumber / targetNumber);
+  for (
+    let currentNumer = 1;
+    currentNumer <= Math.abs(targetNumber);
+    currentNumer++
+  ) {
+    console.log(`${currentNumer}`);
+  }
+}
+/* Tests:
+  printTo(10);
+  printTo(-5);
+  */
+
+function isEven(targetNumber: number) {
+  return targetNumber % 2 == 0 ? true : false;
+}
+
+function isOdd(targetNumber: number) {
+  return !isEven(targetNumber);
+}
+/* Tests:
+print(`number 2 is even: ${isEven(2)}`);
+print(`number 7 is even: ${isEven(7)}`);
+print(`number -2 is odd: ${isOdd(2)}`);
+print(`number -7 is odd: ${isOdd(7)}`);
+*/
+
+function isPalindrome(text: string) {
+  const loweredText = text.toLowerCase();
+
+  for (let currentChar = 0; currentChar < loweredText.length; currentChar++) {
+    if (loweredText[currentChar] !== loweredText[text.length - 1 - currentChar])
+      return false;
+  }
+  return true;
+}
+
+function isImprovedPalindrome(target: string | number) {
+  target = typeof target !== "number" ? target : Math.abs(target).toString();
+
+  return isPalindrome(target);
+}
+/* Tests:
+  // print(`is "Hello" palindrome? ${isPalindrome("Hello")}`);
+  // print(`is "Palindrome" palindrome? ${isPalindrome("Palindrome")}`);
+  // print(`is "redivider" palindrome? ${isPalindrome("redivider")}`);
+  // print(`is "RAcecar" palindrome? ${isPalindrome("RAcecar")}`);
+  // print(`is "x" palindrome? ${isPalindrome("x")}`);
+  // print(`is "" palindrome? ${isPalindrome("")}`);
+
+  // print(`is "hello" improved palindrome? ${isImprovedPalindrome("hello")}`);
+  // print(`is "wow" improved palindrome? ${isImprovedPalindrome("wow")}`);
+  // print(`is "" improved palindrome? ${isImprovedPalindrome("")}`);
+  // print(`is "123" improved palindrome? ${isImprovedPalindrome(123)}`);
+  // print(`is "45654" improved palindrome? ${isImprovedPalindrome(45654)}`);
+  // print(`is "-505" improved palindrome? ${isImprovedPalindrome(-505)}`);
+  // print(`is "0" improved palindrome? ${isImprovedPalindrome(0)}`);
+*/
