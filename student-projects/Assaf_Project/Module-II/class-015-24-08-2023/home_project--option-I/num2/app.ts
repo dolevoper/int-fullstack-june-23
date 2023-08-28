b65fe13829dc9c1a87d2057038dde41a3c17cebf
Assaf Basri <Faskoman90@gmail.com>
2023-08-28 14:35:21 +0300

    added task #2

--- student-projects/Assaf_Project/Module-II/class-015-24-08-2023/home_project--option-I/num2/app.ts
+++ student-projects/Assaf_Project/Module-II/class-015-24-08-2023/home_project--option-I/num2/app.ts
@@ -1,25 +1,25 @@
-// #1 in array
-function printTo(inputNumber) {
-  let numbers = [];
-  for (let i = 0; i < inputNumber; i++) {
-    numbers.push(i + 1);
-  }
-  return numbers;
-}
-
-let inputNumber = Number(prompt("Choose a NUM to see whole numbers to it"));
-while (!inputNumber || inputNumber < 1 || !Number.isInteger(inputNumber)) {
-  inputNumber = Number(prompt("Choose a positive whole number above 1"));
-}
-const numbersArray = printTo(inputNumber);
-alert(numbersArray);
-
-// // #1 in alerts
-// function printTo(inputNumber) {
-//   for (let i = 0; i < inputNumber; i++) {
-//     alert(i+1);
-//   }
+// function isEven(number) {
+// const evenNumber = Number.isInteger(number / 2)
+// return (evenNumber)
 // }
 
-// const inputNumber = Number(prompt("Choose a NUM to see whole numbers to it!"));
-// printTo(inputNumber);
+// const number = Number(prompt("Choose a number!"))
+// if (isEven(number)) {
+//     alert("Number is even!")
+// }
+
+function isOdd(number) {
+  if (Number.isInteger(number / 2)) {
+    const oddNumber = "false";
+    return oddNumber;
+  } else {
+    const oddNumber = "true";
+    return oddNumber;
+  }
+}
+
+let number = Number(prompt("Choose a number!"));
+while (!number || number < 0) {
+  number = Number(prompt("Choose a valid number!"));
+}
+alert("Is the number " + number + " an odd number?\n" + isOdd(number));
