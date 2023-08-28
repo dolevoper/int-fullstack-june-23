b1422ef9162ca7bb9fe384cef7f6fb4a74e6ae18
Assaf Basri <Faskoman90@gmail.com>
2023-08-28 15:14:11 +0300

    added task #3

--- student-projects/Assaf_Project/Module-II/class-015-24-08-2023/home_project--option-I/num2/app.ts
+++ student-projects/Assaf_Project/Module-II/class-015-24-08-2023/home_project--option-I/num2/app.ts
@@ -1,14 +1,4 @@
-// function isEven(number) {
-// const evenNumber = Number.isInteger(number / 2)
-// return (evenNumber)
-// }
-
-// const number = Number(prompt("Choose a number!"))
-// if (isEven(number)) {
-//     alert("Number is even!")
-// }
-
-function isOdd(number) {
+function checkIfOdd(number) {
   if (Number.isInteger(number / 2)) {
     const oddNumber = "false";
     return oddNumber;
@@ -19,7 +9,7 @@ function isOdd(number) {
 }
 
 let number = Number(prompt("Choose a number!"));
-while (!number || number < 0) {
+while (!number || number === 0) {
   number = Number(prompt("Choose a valid number!"));
 }
-alert("Is the number " + number + " an odd number?\n" + isOdd(number));
+alert("Is the number " + number + " an odd number?\n" + checkIfOdd(number));
