95f89b9b567a5612035b99688fb6287abeb9f684
Assaf Basri <Faskoman90@gmail.com>
2023-08-28 13:47:41 +0300

    done #1. printTo task

---
+++ student-projects/Assaf_Project/Module-II/class-015-24-08-2023/home_project--option-I/app.ts
@@ -0,0 +1,23 @@
+// function printTo(inputNumber) {
+//   for (let i = 0; i < inputNumber; i++) {
+//     alert(i+1);
+//   }
+// }
+
+// const inputNumber = Number(prompt("Choose a NUM to see whole numbers to it!"));
+// printTo(inputNumber);
+
+function printTo(inputNumber) {
+  let numbers = [];
+  for (let i = 0; i < inputNumber; i++) {
+    numbers.push(i + 1);
+  }
+  return numbers;
+}
+
+let inputNumber = Number(prompt("Choose a NUM to see whole numbers to it"));
+while (!inputNumber || inputNumber < 1 || !Number.isInteger(inputNumber)) {
+  inputNumber = Number(prompt("Choose a positive whole number above 1"));
+}
+const numbersArray = printTo(inputNumber);
+alert(numbersArray);
