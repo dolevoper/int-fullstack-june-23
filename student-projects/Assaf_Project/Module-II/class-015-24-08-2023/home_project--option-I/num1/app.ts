b981a68418310cbfb52d054520add6087562971f
Assaf Basri <Faskoman90@gmail.com>
2023-08-28 13:57:56 +0300

    created subfolders for each assignment

--- student-projects/Assaf_Project/Module-II/class-015-24-08-2023/home_project--option-I/num1/app.ts
+++ student-projects/Assaf_Project/Module-II/class-015-24-08-2023/home_project--option-I/num1/app.ts
@@ -1,12 +1,4 @@
-// function printTo(inputNumber) {
-//   for (let i = 0; i < inputNumber; i++) {
-//     alert(i+1);
-//   }
-// }
-
-// const inputNumber = Number(prompt("Choose a NUM to see whole numbers to it!"));
-// printTo(inputNumber);
-
+// #1 in array
 function printTo(inputNumber) {
   let numbers = [];
   for (let i = 0; i < inputNumber; i++) {
@@ -21,3 +13,13 @@ while (!inputNumber || inputNumber < 1 || !Number.isInteger(inputNumber)) {
 }
 const numbersArray = printTo(inputNumber);
 alert(numbersArray);
+
+// // #1 in alerts
+// function printTo(inputNumber) {
+//   for (let i = 0; i < inputNumber; i++) {
+//     alert(i+1);
+//   }
+// }
+
+// const inputNumber = Number(prompt("Choose a NUM to see whole numbers to it!"));
+// printTo(inputNumber);
