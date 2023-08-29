e65db6af06537665d3f58370434125c5e4536ca9
yizhak148 <yizhak208@gmail.com>
2023-08-30 01:10:26 +0300

    SP done in success

--- student-projects/yizhak/module-II/pseudo(16)/isPalidrome.ts
+++ student-projects/yizhak/module-II/pseudo(16)/isPalidrome.ts
@@ -9,10 +9,19 @@
 
 // Code
 const userInput = String(prompt("enter a string"));
-
+let rev_str = "";
 function isPalindrome(userInput: string){
-    let rev_str = "";
+    
     for (let i = userInput.length - 1; i >= 0; i--) {
         rev_str += userInput[i]
+
+    }if (rev_str === userInput) {
+        console.log("passed string is palindrome ");
+    }
+    else {
+        console.log("passed string is not palindrome")
+    } 
 }
-}
+
+// הפונקציה עובדת בלי שימוש ברוורסינג.
+
