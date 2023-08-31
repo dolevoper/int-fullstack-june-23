d42373fc1239e118d378ca330aa1168219970c99
elad-her <eladher18@gmail.com>
2023-08-31 17:01:21 +0300

    elad

---
+++ student-projects/Elad/class-016-28-08-2023/isPalidrome.ts
@@ -0,0 +1,18 @@
+// Problem definition
+// Write a function that accepts a string and returns true if it is a palindrome and false otherwise.
+// Do not use reverse function.
+// Hint: strings has a length property and a charAt function
+
+// Pseudo code
+// define a function called "PolindromeChecker" V
+// define a variable that will be a string and receive it from the user namde "userInputString"
+// check langth of the string
+// check the first and last leter
+// continues to check if the other corresponding letters are equal
+// return an answer whether the word is a palindrome or not
+
+// Code
+
+function PolindromeChecker (){
+const userInputString  = prompt ("Please enter a word and we will check if it is a palindrome or not")
+}
