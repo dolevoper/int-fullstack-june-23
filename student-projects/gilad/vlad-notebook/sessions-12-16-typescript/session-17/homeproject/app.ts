31ead5d2bc7f7387b8200e036b9d6233fde82c5a
BraveVlad <giladvladpinker@gmail.com>
2023-09-02 17:22:38 +0300

    added index normalization helper functions

--- student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-17/homeproject/app.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-17/homeproject/app.ts
@@ -1,12 +1,28 @@
+function normalizeNegativeIndex(index: number, arrayLength: number) {
+	return index < 0 ? (index < -arrayLength ? 0 : arrayLength + index) : index;
+}
+
+function normalizeNegativeEndIndex(
+	end: number,
+	start: number,
+	arrayLength: number
+) {
+	end = normalizeNegativeIndex(end, arrayLength);
+	end = end > arrayLength ? arrayLength : end;
+
+	if (end <= start) return null;
+	else return end;
+}
+
 function at(array: any[], index: number) {
-	if (!array) return undefined;
+	if (!array) return null;
 
 	if (index >= 0) return array[index];
 	else return array[array.length + index];
 }
 
 function concat(array: any[], ...arrays: any[]) {
-	if (!array) return undefined;
+	if (!array) return null;
 
 	const newArray = [...array];
 
@@ -35,12 +51,13 @@ function copyWithin(
 	start: number = 0,
 	end: number = array.length
 ) {
+	if (!array) return null;
+
 	start = normalizeNegativeIndex(start, array.length);
 	target = normalizeNegativeIndex(target, array.length);
-	end = normalizeNegativeIndex(end, array.length);
-	end = end > array.length ? array.length : end;
+	end = normalizeNegativeEndIndex(end, start, array.length);
 
-	if (end < start) return;
+	if (!end) return array;
 
 	const copyOfArray = [...array];
 
@@ -55,13 +72,8 @@ function copyWithin(
 	return array;
 }
 
-function normalizeNegativeIndex(index: number, arrayLength: number) {
-	return index < 0 ? (index < -arrayLength ? 0 : arrayLength + index) : index;
-}
-
 function entries(array: any[]) {
-	if (!array) return undefined;
-	if (array.length === 0) return array[Symbol.iterator]();
+	if (!array) return null;
 
 	const newArray = [];
 
@@ -73,7 +85,7 @@ function entries(array: any[]) {
 }
 
 function every(array: any[], callback: Function) {
-	if (!array) return undefined;
+	if (!array) return null;
 
 	const arrayLength = array.length;
 	for (let index = 0; index < arrayLength; index++) {
@@ -84,3 +96,72 @@ function every(array: any[], callback: Function) {
 
 	return true;
 }
+
+function fill(
+	array: any[],
+	value: any,
+	start: number = 0,
+	end: number = array.length
+) {
+	if (!array) return null;
+
+	start = normalizeNegativeIndex(start, array.length);
+	end = normalizeNegativeEndIndex(end, start, array.length);
+
+	if (!end) return array;
+
+	for (let index = start; index < end; index++) {
+		array[index] = value;
+	}
+
+	return array;
+}
+
+/* fill() tests */
+
+/* Mutability tests */
+const array = [1, 2, 3, 4];
+
+// Fill with 0 from position 2 until position 4
+console.log(fill(array, 0, 2, 4)); // [1, 2, 0, 0]
+
+// Fill with 5 from position 1
+console.log(fill(array, 5, 1)); // [1, 5, 5, 5]
+
+// Fill everything with 6
+console.log(fill(array, 6)); // [6, 6, 6, 6]
+
+// Check original array
+console.log(array);
+
+/* Different usecases tests */
+console.log(fill([1, 2, 3], 4)); // [4, 4, 4]
+console.log(fill([1, 2, 3], 4, 1)); // [1, 4, 4]
+console.log(fill([1, 2, 3], 4, 1, 2)); // [1, 4, 3]
+console.log(fill([1, 2, 3], 4, 1, 1)); // [1, 2, 3]
+console.log(fill([1, 2, 3], 4, 3, 3)); // [1, 2, 3]
+console.log(fill([1, 2, 3], 4, -3, -2)); // [4, 2, 3]
+console.log(fill([1, 2, 3], 4, NaN, NaN)); // [1, 2, 3]
+console.log(fill([1, 2, 3], 4, 3, 5)); // [1, 2, 3]
+console.log(fill(Array(3), 4)); // [4, 4, 4]
+
+/* Fill with objects test */
+// A single object, referenced by each slot of the array:
+const arr1 = fill(Array(3), {}); // [{}, {}, {}]
+arr1[0].hi = "hi"; // [{ hi: "hi" }, { hi: "hi" }, { hi: "hi" }]
+console.log(arr1);
+
+/* Create a matrix full of 1 */
+const arr2 = new Array(3);
+for (let i = 0; i < arr2.length; i++) {
+	arr2[i] = fill(new Array(4), 1); // Creating an array of size 4 and filled of 1
+}
+arr2[0][0] = 10;
+console.log(arr2[0][0]); // 10
+console.log(arr2[1][0]); // 1
+console.log(arr2[2][0]); // 1
+console.log(arr2);
+
+/* Populate an empty array */
+const tempMicrowaves = fill(Array(5), "microwave", 0);
+console.log(tempMicrowaves);
