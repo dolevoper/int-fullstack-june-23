af03bb7f407a18617399bbd618d05cdab8b45f8d
BraveVlad <giladvladpinker@gmail.com>
2023-09-02 14:59:09 +0300

    Added entries() method implementation

--- student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-17/homeproject/app.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-17/homeproject/app.ts
@@ -1,13 +1,13 @@
 function at(array: any[], index: number) {
-  if (!array) {
-    return undefined;
-  }
+	if (!array) {
+		return undefined;
+	}
 
-  if (index >= 0) {
-    return array[index];
-  } else {
-    return array[array.length + index];
-  }
+	if (index >= 0) {
+		return array[index];
+	} else {
+		return array[array.length + index];
+	}
 }
 // console.log("at() funciton");
 // console.log(at([], 0));
@@ -16,28 +16,28 @@ function at(array: any[], index: number) {
 // console.log(at([1, 2, 3, 4], 1));
 // console.log(at([1, 2, 3, 4], -1));
 
-function concat(targetArray: any[], ...arrays: any[]) {
-  if (!targetArray) return undefined;
+function concat(array: any[], ...arrays: any[]) {
+	if (!array) return undefined;
 
-  const newArray = [...targetArray];
+	const newArray = [...array];
 
-  if (arrays.length === 0) return newArray;
+	if (arrays.length === 0) return newArray;
 
-  let addedArray: any;
-  let totalCellsAdded = newArray.length;
+	let addedArray: any;
+	let totalCellsAdded = newArray.length;
 
-  for (let currentArray = 0; currentArray < arrays.length; currentArray++) {
-    addedArray = arrays[currentArray];
-    addedArray = Array.isArray(addedArray) ? addedArray : [addedArray];
+	for (let currentArray = 0; currentArray < arrays.length; currentArray++) {
+		addedArray = arrays[currentArray];
+		addedArray = Array.isArray(addedArray) ? addedArray : [addedArray];
 
-    for (let currentCell = 0; currentCell < addedArray.length; currentCell++) {
-      newArray[totalCellsAdded + currentCell] = addedArray[currentCell];
-    }
+		for (let currentCell = 0; currentCell < addedArray.length; currentCell++) {
+			newArray[totalCellsAdded + currentCell] = addedArray[currentCell];
+		}
 
-    totalCellsAdded += addedArray.length;
-  }
+		totalCellsAdded += addedArray.length;
+	}
 
-  return newArray;
+	return newArray;
 }
 
 // console.log(concat(null));
@@ -46,30 +46,30 @@ function concat(targetArray: any[], ...arrays: any[]) {
 // console.log(concat([1, 2, 3], 5, ["hello", "world"], [4, 5, 6]));
 
 function normalizeNegativeIndex(index: number, arrayLength: number) {
-  return index < 0 ? (index < -arrayLength ? 0 : arrayLength + index) : index;
+	return index < 0 ? (index < -arrayLength ? 0 : arrayLength + index) : index;
 }
 function copyWithin(
-  array: any[],
-  target: number,
-  start: number = 0,
-  end: number = array.length
+	array: any[],
+	target: number,
+	start: number = 0,
+	end: number = array.length
 ) {
-  start = normalizeNegativeIndex(start, array.length);
-  target = normalizeNegativeIndex(target, array.length);
-  end = normalizeNegativeIndex(end, array.length);
-  end = end > array.length ? array.length : end;
+	start = normalizeNegativeIndex(start, array.length);
+	target = normalizeNegativeIndex(target, array.length);
+	end = normalizeNegativeIndex(end, array.length);
+	end = end > array.length ? array.length : end;
 
-  if (end < start) return;
+	if (end < start) return;
 
-  const copyOfArray = [...array];
-  for (
-    let cell = start;
-    cell < end && target < array.length;
-    cell++, target++
-  ) {
-    array[target] = copyOfArray[cell];
-  }
-  return array;
+	const copyOfArray = [...array];
+	for (
+		let cell = start;
+		cell < end && target < array.length;
+		cell++, target++
+	) {
+		array[target] = copyOfArray[cell];
+	}
+	return array;
 }
 
 /* CopyWithin Tests: */
@@ -98,3 +98,34 @@ function copyWithin(
 // // Copy to index 0 the element at index 3
 // console.log(copyWithin(array1, 0, 3, 4));
 // // Expected output: Array ["d", "b", "c", "d", "e"]
+
+function entries(array: any[]) {
+	if (!array) return undefined;
+	if (array.length === 0) return array[Symbol.iterator]();
+	const newArray = [];
+
+	for (let index = 0; index < array.length; index++) {
+		newArray[index] = [index, array[index]];
+	}
+
+	const iterableArray = newArray[Symbol.iterator]();
+	return iterableArray;
+}
+
+/* null array example */
+// console.log(entries(null));
+
+/* empty array example */
+// console.log(entries([]));
+
+/* Return Iterable with index example */
+// const array1 = ["a", "b", "c"];
+
+// const iterator1 = entries(array1);
+// console.log(iterator1);
+
+// console.log(iterator1.next().value);
+// // Expected output: Array [0, "a"]
+
+// console.log(iterator1.next().value);
+// // Expected output: Array [1, "b"]
