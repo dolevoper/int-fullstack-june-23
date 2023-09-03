a41dae0f1c15857464d1be206d7d6459c1458c39
shai-benari <s.benari1962@gmail.com>
2023-09-03 12:21:10 +0300

    map-mathod

---
+++ student-projects/shai benari/module-2/class-17-class-exer/app.ts
@@ -0,0 +1,6 @@
+const singlArray =[1,4,9,16];
+alert(singlArray)
+const rootAraay = singlArray.map((x) => Math.sqrt(x) );
+alert(rootAraay)
+const dubleArray = rootAraay.map((x) => x*2);
+alert(dubleArray)
