6dd18573d95aaacdffbd1b2746466ea901844a56
Assaf Basri <Faskoman90@gmail.com>
2023-09-04 20:29:10 +0300

    merged works

--- student-projects/Assaf_Project/Module-II/class-018-04-09-2023/group=project/app.ts
+++ student-projects/Assaf_Project/Module-II/class-018-04-09-2023/group=project/app.ts
@@ -1,12 +1,11 @@
 function itemsApp() {
   const userInput = simplePrompt(
-    `Items List:\n` +
-      items.join(`\n`) +
-      `
+    `Items List:\n` + items.join(`\n`) + `${items.Name}${quote.author}`
+  );
+  `
       
 Enter "1" or "add" to add an item.
-Enter "2" or "remove" to delete an item.`
-  );
+Enter "2" or "remove" to delete an item.`;
   switch (userInput) {
     case undefined:
     case "exit":
@@ -38,8 +37,8 @@ function addItemToList() {
 }
 
 function removeItemFromList() {
-  const index = Number(prompt("Choose line number to remove.")) - 1;
-  if (index > -1 && index < items.length) {
+  const index = Number(prompt("Choose item by name to remove.")) - 1;
+  if (!name || items.name.length <= 0) {
     items.splice(index, 1);
   } else {
     alert("Invalid line number. No item removed.");
@@ -57,66 +56,102 @@ function announceUnknownInput(input: string) {
 }
 
 const items = [
-    {
-      Name: "milk",
-      price: "3.5",
-      Amount: "100",
-      aisle: "4",
-    },
-    {
-      Name: "cheese",
-      price: "2.3",
-      Amount: "50",
-      aisle: "4",
-    },
-    {
-      Name: "Chocolate treat",
-      price: "4",
-      Amount: "40",
-      aisle: "4",
-    },
-    {
-      Name: "Goat Cheese",
-      price: "4.7",
-      Amount: "25",
-      aisle: "4",
-    },
-    {
-      Name: "Red meat",
-      price: "8",
-      Amount: "250",
-      aisle: "1",
-    },
-    {
-      Name: "sausage",
-      price: "5",
-      Amount: "145",
-      aisle: "1",
-    },
-    {
-      Name: "A whole chicken",
-      price: "9",
-      Amount: "10",
-      aisle: "1",
-    },
-    {
-      Name: "tomato",
-      price: "1.3",
-      Amount: "200",
-      aisle: "2",
-    },
-    {
-      Name: "cucumber",
-      price: "1.4",
-      Amount: "365",
-      aisle: "2",
-    },
-    {
-      Name: "Banana",
-      price: "2",
-      Amount: "123",
-      aisle: "2",
-    },
-]
+  {
+    Name: "milk",
+    price: "3.5",
+    Amount: "100",
+    aisle: "4",
+  },
+  {
+    Name: "cheese",
+    price: "2.3",
+    Amount: "50",
+    aisle: "4",
+  },
+  {
+    Name: "Chocolate treat",
+    price: "4",
+    Amount: "40",
+    aisle: "4",
+  },
+  {
+    Name: "Goat Cheese",
+    price: "4.7",
+    Amount: "25",
+    aisle: "4",
+  },
+  {
+    Name: "Red meat",
+    price: "8",
+    Amount: "250",
+    aisle: "1",
+  },
+  {
+    Name: "sausage",
+    price: "5",
+    Amount: "145",
+    aisle: "1",
+  },
+  {
+    Name: "A whole chicken",
+    price: "9",
+    Amount: "10",
+    aisle: "1",
+  },
+  {
+    Name: "tomato",
+    price: "1.3",
+    Amount: "200",
+    aisle: "2",
+  },
+  {
+    Name: "cucumber",
+    price: "1.4",
+    Amount: "365",
+    aisle: "2",
+  },
+  {
+    Name: "Banana",
+    price: "2",
+    Amount: "123",
+    aisle: "2",
+  },
+  {
+    Name: "watermelon",
+    price: "3.2",
+    Amount: "65",
+    aisle: "2",
+  },
+  {
+    Name: "cabbage",
+    price: "2.1",
+    Amount: "32",
+    aisle: "2",
+  },
+  {
+    Name: "disposable cups",
+    price: "5.2",
+    Amount: "300",
+    aisle: "3",
+  },
+  {
+    Name: "Disposable plates",
+    price: "5.7",
+    Amount: "176",
+    aisle: "3",
+  },
+  {
+    Name: "tablespoon",
+    price: "1.4",
+    Amount: "2765",
+    aisle: "3",
+  },
+  {
+    Name: "fork",
+    price: "1.1",
+    Amount: "2343",
+    aisle: "3",
+  },
+];
 
 itemsApp();
