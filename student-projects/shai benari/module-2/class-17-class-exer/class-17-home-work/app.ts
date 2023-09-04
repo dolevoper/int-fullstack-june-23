91da21d28b76d6fec0e0691385f10a8ee5ecebd1
shai-benari <s.benari1962@gmail.com>
2023-09-04 13:45:23 +0300

    median-with-loop

--- student-projects/shai benari/module-2/class-17-class-exer/class-17-home-work/app.ts
+++ student-projects/shai benari/module-2/class-17-class-exer/class-17-home-work/app.ts
@@ -6,24 +6,52 @@
 // eles the median is averge of  lengh/2 -1 and  lengh/2 +1
 // returen and print the median 
 
-const medianArray = [1,2,3,4,5,6,8,8,9,10];
-alert(medianArray);
+
+// const medianArray = [1,2,3,4,5,6,8,8,9,10];
+// alert(medianArray);
+// const madianLength = medianArray.length;
+// const resultMedian = madian(madianLength);
+// alert(resultMedian);
+
+
+// function madian(item: number){
+//    alert(madianLength);
+//    if (item % 2 !==0){
+//    const resultMedian = medianArray[(madianLength-1)/2 ];
+//    return(resultMedian)
+//    }
+//    else {
+//     const lowLimet = medianArray[(madianLength)/2 -1];
+//     const upLimet = medianArray[(madianLength)/2 ];
+//     alert(upLimet);
+//     alert(lowLimet);
+//     const resultMedian = (upLimet+lowLimet)/2;
+//     return(resultMedian);
+//    }
+// }
+let medianArray: Number[] =[];
+const arrayLength = Number(prompt("Enter the number of item in the array"));
+for (let i=0; i < arrayLength; i++){
+    const number = Number(prompt("Enter a number to add to the array"));
+    medianArray.push(number);
+} 
+alert("The array you enterd is: " + medianArray);
 const madianLength = medianArray.length;
 const resultMedian = madian(madianLength);
-alert(resultMedian);
+alert("the median of the array is: " + resultMedian);
 
 
 function madian(item: number){
-   alert(madianLength);
+//    alert(madianLength);
    if (item % 2 !==0){
    const resultMedian = medianArray[(madianLength-1)/2 ];
    return(resultMedian)
    }
    else {
-    const lowLimet = medianArray[(madianLength)/2 -1];
-    const upLimet = medianArray[(madianLength)/2 ];
-    alert(upLimet);
-    alert(lowLimet);
+    const lowLimet =Number(medianArray[(madianLength)/2 -1]);
+    const upLimet = Number(medianArray[(madianLength)/2 ]);
+    // alert(upLimet);
+    // alert(lowLimet);
     const resultMedian = (upLimet+lowLimet)/2;
     return(resultMedian);
    }
