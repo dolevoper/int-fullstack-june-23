fa3a37d1ed4b0018c2bdebedf8dc11186e0d5425
BraveVlad <giladvladpinker@gmail.com>
2023-09-04 20:13:21 +0300

    init car object list

--- student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-18/exercise-group-5/app.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-16-typescript/session-18/exercise-group-5/app.ts
@@ -1,1 +1,35 @@
-const car = {};
+// const Car = {
+// 	model: "Audi",
+// 	licensePlate: "0123456789",
+// 	fuel: 0,
+// 	engine: false,
+// };
+
+const carList = [
+	{
+		model: "Audi",
+		licensePlate: "0123456789",
+		fuel: 100,
+		engine: false,
+	},
+	{
+		model: "BMW",
+		licensePlate: "0123456789",
+		fuel: 75,
+		engine: true,
+	},
+	{
+		model: "Seat",
+		licensePlate: "12210902",
+		fuel: 0,
+		engine: false,
+	},
+];
+
+function app() {
+	// create a new car
+	// get car license plate
+	// refuel car
+	// turn engine on
+	// turn engine off
+}
