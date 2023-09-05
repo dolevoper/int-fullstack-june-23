9314f0132cef3449acca476f964d77d9203f7f6f
Assaf Basri <Faskoman90@gmail.com>
2023-09-05 13:32:13 +0300

    messing arround

--- student-projects/Assaf_Project/Module-II/class-018-04-09-2023/HP/app.ts
+++ student-projects/Assaf_Project/Module-II/class-018-04-09-2023/HP/app.ts
@@ -1,41 +1,44 @@
-
 function arcApp() {
-  
-    const menuPromptText = `Welcome to the Arc app!
-This app will help you ("Noah"), arrange your animals.
+  const menuPromptText = `Welcome to the Arc app!
+This app will help you, "Noah", arrange your animals.
   
   What would you like to do?
       1. Show all animals
-      2. Show all rooms
-      3. Pair up animals
-      4. Pair up pairs
-      5. Show problems`;
-  
-    let userInput = prompt(menuPromptText);
-  
-    while (userInput !== null) {
-      handleUserInput(userInput);
-      userInput = prompt(menuPromptText);
-    }
+      2. Show occupied rooms
+      3. Show available rooms
+      4. Pair up animals
+      5. Pair up pairs
+      6. Show problems`;
+
+  let userInput = prompt(menuPromptText);
+
+  while (userInput !== null) {
+    handleUserInput(userInput);
+    userInput = prompt(menuPromptText);
   }
-  
-  function handleUserInput(userInput: string) {
+}
+
+function handleUserInput(userInput: string) {
     switch (userInput.trim()) {
       case "1":
-        alert(animals);
+        alert("Animals list: " + animalNames.join(", "));
         arcApp();
         break;
       case "2":
-        alert(rooms);
+        alert(occupiedRooms);
         arcApp();
         break;
       case "3":
-        pairUp();
+        alert(availableRooms);
+        arcApp();
         break;
       case "4":
-        pairCouples();
+        pairUp();
         break;
       case "5":
+        pairCouples();
+        break;
+      case "6":
         conflicts();
         break;
       default:
@@ -43,4 +46,24 @@ This app will help you ("Noah"), arrange your animals.
     }
   }
 
-arcApp ();
+
+const lions = {class: "Mammalia", order: "Carnivora", family: "Felidae", isTerrestrial: true, isHerbivore: false, isSexual: true, isTerritorial: true }
+
+const animals = [lions, ]
+const animalNames = animals.map(animal => Object.values(animal)[0]);
+const availableRooms = [
+    {
+name: "Aquatic room",
+isAquatic: true,
+isOccupied: false,
+isConflicted: false, 
+contains: []
+}
+]
+
+
+
+arcApp();
+
+const lion = {class: "Mammalia", order: "Carnivora", family: "Felidae", isTerrestrial: true, isHerbivore: false, isSexual: true, isTerritorial: true }
+const animals = [lion, bear, pig, ]
