59c6d44726c036b377ca997bf23a19239e173c81
Assaf Basri <Faskoman90@gmail.com>
2023-09-05 11:56:54 +0300

    basic menu for app

--- student-projects/Assaf_Project/Module-II/class-018-04-09-2023/HP/app.ts
+++ student-projects/Assaf_Project/Module-II/class-018-04-09-2023/HP/app.ts
@@ -1,1 +1,46 @@
-alert("hello")
+
+function arcApp() {
+  
+    const menuPromptText = `Welcome to the Arc app!
+This app will help you ("Noah"), arrange your animals.
+  
+  What would you like to do?
+      1. Show all animals
+      2. Show all rooms
+      3. Pair up animals
+      4. Pair up pairs
+      5. Show problems`;
+  
+    let userInput = prompt(menuPromptText);
+  
+    while (userInput !== null) {
+      handleUserInput(userInput);
+      userInput = prompt(menuPromptText);
+    }
+  }
+  
+  function handleUserInput(userInput: string) {
+    switch (userInput.trim()) {
+      case "1":
+        alert(animals);
+        arcApp();
+        break;
+      case "2":
+        alert(rooms);
+        arcApp();
+        break;
+      case "3":
+        pairUp();
+        break;
+      case "4":
+        pairCouples();
+        break;
+      case "5":
+        conflicts();
+        break;
+      default:
+        alert("Please choose an option from the menu using their numbers.");
+    }
+  }
+
+arcApp ();
