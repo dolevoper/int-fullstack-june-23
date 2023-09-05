6d20992ec1602ab00afa9c11b5cc12cf63c68257
Lidorsalach <lidor41425@gmail.com>
2023-09-05 19:34:50 +0300

    lidor

---
+++ student-projects/lidor/module-II/class-017-31-08-2023/homework.ts
@@ -0,0 +1,30 @@
+const numbers =  [5,3,55,22,443,88787,6555,4234,56865,233,1]
+alert (numbers);
+numbers.sort(( a,b) => a-b)
+alert (numbers)   
+
+const names =  ["apple","lidor","kill","banana","gitar","tom"]
+alert (names);
+names.sort()
+alert (names)   
+
+
+
+
+
+const a = prompt ("give a number");
+const c = prompt ("give another number")
+const b = prompt ("give another number")
+const numbers1 = Number [3]
+function sorting (a,b,c){
+    for (let i = 0; i < 3; i++) {
+     if (a-b <0 ){
+        numbers1[i] = b;
+        
+     }
+}
+    
+} 
+
+
+
