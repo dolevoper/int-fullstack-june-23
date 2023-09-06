ab45ab6494e0ee11dc5a95719f5b20046b44411a
yizhak148 <yizhak208@gmail.com>
2023-09-06 15:20:13 +0300

    done

--- student-projects/yizhak/module-II/class-17/homework/arrays.ts
+++ student-projects/yizhak/module-II/class-17/homework/arrays.ts
@@ -13,6 +13,12 @@ for (let i = 0; i < animals.length; i++) {
 }
 alert(result);
 
-
-
+// home project
+const median = arr => {
+  const mid = Math.floor(arr.length / 2),
+    nums = [...arr].sort((a, b) => a - b);
+  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+};
+console.log(median([5, 6, 50, 1, -5]));
+console.log(median([1, 2, 3, 4, 5]));
 
