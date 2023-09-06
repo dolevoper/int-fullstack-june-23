1128ac18b71e7fedb32f133748e37b9730816dcb
Ayala Perlovich <ap47046@gmail.com>
2023-09-06 21:10:32 +0300

    hp class-017

---
+++ student-projects/Ayala-perlovich/class-017/arrys.ts
@@ -0,0 +1,21 @@
+// reduce -סריקה על המערך 
+
+const arr = [2, 5, 7, 20, 30, 40, 44, 21, 27];
+function sum(a, b){
+    return a+b;
+
+}
+
+function sum(a, b){
+    return a*b;
+}
+
+function sum(a, b){
+    return a > b ? a:b;
+}
+
+const asum = arr.reduce(sum);
+alert(asum);
+
+//************************************ */
+// (1 Median
