032ea3739f0555f6a1a16a4e45b95720b53bb60b
Gili <gilimena95@gmail.com>
2023-09-07 18:54:02 +0300

    class-19-DOM Manipulation basics

--- module-II/class-019-07-09-2023/example/app.ts
+++ module-II/class-019-07-09-2023/example/app.ts
@@ -1,1 +1,35 @@
-console.log("connected")
+console.log("connected");
+
+//truthy values
+// "withContent", {} , [] , 2 (numbers other than 0), true
+
+// //falsy values
+// "", 0 , false, null undefiend
+
+
+
+// DOM Manipulation
+
+// GetElementByID()
+
+const title = document.getElementById("main-heading") as HTMLHeadingElement;
+
+
+// GetElementByClassName()
+const listItems = document.getElementsByClassName("list-item") as HTMLCollectionOf<HTMLLIElement>
+
+// for (let i = 0; i <listItems.length ; i++) {
+//     listItems[i].style.color = "blue"
+// }
+
+// GetElementByTagName()
+const listItemsTag = document.getElementsByTagName("li")
+
+// querySelector()
+const containerTag = document.querySelector("div")
+const containerClass = document.querySelector(".container")
+const h1Query = document.querySelector("#main-heading")
+
+// querySelectorAll()
+const listItemsQuery = document.querySelectorAll(".list-item")
+console.log(listItemsQuery)
