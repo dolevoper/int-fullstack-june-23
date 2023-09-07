a773f3077436b08ba3688ae4e5d4a0227236b506
BraveVlad <giladvladpinker@gmail.com>
2023-09-07 16:10:28 +0300

    added Cage getter, setters, new bars and alert listeners

--- student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-18/home-project/Cage/Cage.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-18/home-project/Cage/Cage.ts
@@ -3,7 +3,7 @@ enum Biome {
 	dessert,
 	antartica,
 	jungle,
-	forst,
+	forest,
 }
 
 class Cage {
@@ -11,12 +11,26 @@ class Cage {
 	private animalsList: AnimalList;
 
 	private biome: Biome;
-	private slots: number;
 
-	private cleanliness: NeedBar;
+	private cleanlinessBar: NeedBar;
+	private foodBar: NeedBar;
+	private waterBar: NeedBar;
 
-	constructor(name: string) {
+	constructor(name: string, startBiome?: Biome) {
 		this.name = name;
+
+		this.setSlots(4);
+		this.setBiome(startBiome !== undefined ? startBiome : Biome.grassland);
+		this.cleanlinessBar = new NeedBar(
+			"cleanliness",
+			0,
+			2,
+			2,
+			1,
+			this.dirtyCageAlert
+		);
+		this.foodBar = new NeedBar("food tray", 0, 4, 5, 0, this.noFoodAlert);
+		this.waterBar = new NeedBar("water tray", 0, 4, 5, 0, this.noWaterAlert);
 	}
 
 	setBiome(biome: Biome) {
@@ -25,5 +39,47 @@ class Cage {
 
 	setSlots(slots: number) {
 		this.slots = slots;
+	}
+
+	getName() {
+		return this.name;
+	}
+
+	getAnimalsList() {
+		return this.animalsList;
+	}
+
+	getBiome() {
+		return this.biome;
+	}
+
+	getCleanlinessBar() {
+		return this.cleanlinessBar;
+	}
+
+	getFoodBar() {
+		return this.foodBar;
+	}
+
+	getWaterBar() {
+		return this.waterBar;
+	}
+
+	isDirty() {
+		return this.cleanlinessBar.getValue() <= this.cleanlinessBar.getAlertValue()
+			? true
+			: false;
+	}
+
+	dirtyCageAlert() {
+		console.log(`Cage ${this.getName()} is dirty! `);
+	}
+
+	noFoodAlert() {
+		console.log(`Cage ${this.getName()} has no food! `);
+	}
+
+	noWaterAlert() {
+		console.log(`Cage ${this.getName()} has no water! `);
 	}
 }
