cd93eddfed934b4f56abb7dff5a12ea402c35fe4
BraveVlad <giladvladpinker@gmail.com>
2023-09-07 19:23:46 +0300

    get all projects with images

--- student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-19/exercise-single/app.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-19/exercise-single/app.ts
@@ -1,3 +1,19 @@
+// Get an image of a single project
 const androidProject = window.document.querySelector(".android>.project_item");
 const projectImage = androidProject?.querySelector("img");
+console.log("image of the first project image:");
 console.log(projectImage?.src);
+
+// Get all project items with an image
+
+const allPorjects = document.querySelectorAll(".project_item");
+
+// Covnert to arry
+const allProjectsArray = Array.from(allPorjects);
+const projectsWithImage = Array.from(
+	allProjectsArray.filter(function (element) {
+		return element.querySelector("img") as HTMLImageElement;
+	})
+);
+
+console.log(`There are ${projectsWithImage.length} projects with images`);
