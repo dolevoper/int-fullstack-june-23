dfaf5cf0bc68429e706c15685c304052ce1d701d
shai-benari <s.benari1962@gmail.com>
2023-09-07 23:25:18 +0300

    class-work

---
+++ module-II/class-19-exer/app.ts
@@ -0,0 +1,10 @@
+const TEST = document.querySelector("#ss") as HTMLUListElement
+console.log(TEST)
+const s2 = document.getElementById("ss")
+console.log(s2)
+const aa = document.createElement("li")
+TEST.append(aa)
+aa.innerText= "aaaaaaa"
+const hedar = document.getElementById("#hedar") as HTMLHeadElement;
+hedar.style.color = "red"
+hedar.style.fontSize = "10rem"
