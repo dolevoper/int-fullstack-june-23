e2648c17124a4814ba3d25519be3e80efbc9d83a
BraveVlad <giladvladpinker@gmail.com>
2023-09-10 19:38:30 +0300

    Food holds AnimalDiet data and added food types

--- student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-18/home-project/zoomoji-app/AnimalDiet.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-18/home-project/zoomoji-app/AnimalDiet.ts
@@ -6,26 +6,18 @@ export enum Diets {
 }
 
 export class Food {
-	constructor(public name: string, public diet: Diets) {
+	constructor(public name: string, private animalDiet: AnimalDiet) {
 		this.name = name;
-		this.diet = diet;
+		this.animalDiet = animalDiet;
+	}
+
+	getAnimalDiet() {
+		return this.animalDiet;
 	}
 
 	toString(): string {
 		return `${this.name}`;
 	}
-}
-
-export const foodList = [
-	new Food("Apple", Diets.Herbivore),
-	new Food("Carrot", Diets.Herbivore),
-	new Food("Meat", Diets.Carnivore),
-	new Food("Fish", Diets.Fishivore),
-	new Food("Dry Food", Diets.Omnivore),
-];
-
-function getFoodFromList(name: string) {
-	return foodList.find((food) => food.name === name);
 }
 
 export class AnimalDiet {
@@ -43,6 +35,36 @@ export class AnimalDiet {
 
 	canEat(food: Food) {
 		if (this.getDiet() === Diets.Omnivore) return true;
-		return this.diet === food.diet ? true : false;
+		return this.diet === food.getAnimalDiet().getDiet() ? true : false;
 	}
 }
+
+export const dietHerbivore = new AnimalDiet(Diets.Herbivore);
+export const dietCarnivore = new AnimalDiet(Diets.Carnivore);
+export const dietOmnivore = new AnimalDiet(Diets.Omnivore);
+export const dietFishivore = new AnimalDiet(Diets.Fishivore);
+
+export const foodList = [
+	new Food("Apple", dietHerbivore),
+	new Food("Carrot", dietHerbivore),
+	new Food("Banana", dietHerbivore),
+	new Food("Berries", dietHerbivore),
+	new Food("Lettuce", dietHerbivore),
+	new Food("Peanuts", dietHerbivore),
+	new Food("Tomato", dietHerbivore),
+	new Food("Watermelon", dietHerbivore),
+
+	new Food("Meat", dietCarnivore),
+	new Food("Bacon", dietCarnivore),
+	new Food("Chicken", dietCarnivore),
+	new Food("Steak", dietCarnivore),
+
+	new Food("Fish", dietFishivore),
+	new Food("Cuttlefish", dietFishivore),
+	new Food("Shrimp", dietFishivore),
+
+	new Food("Hamburger", dietOmnivore),
+	new Food("Bubble-Tea", dietOmnivore),
+	new Food("Pizza", dietOmnivore),
+	new Food("Cookie", dietOmnivore),
+];
