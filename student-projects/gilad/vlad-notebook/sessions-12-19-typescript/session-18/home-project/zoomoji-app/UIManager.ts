de7da5b8a489bc7a74d92deee832f2db8ba14116
BraveVlad <giladvladpinker@gmail.com>
2023-09-10 03:16:02 +0300

    implemented UIManager to manage UI elements

---
+++ student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-18/home-project/zoomoji-app/UIManager.ts
@@ -0,0 +1,164 @@
+import { Animal } from "./Animal.js";
+import {
+	AnimalType,
+	Gender,
+	alpaca,
+	animalTypes,
+	camel,
+	elephant,
+	giraffe,
+	penguin,
+} from "./AnimalType.js";
+import { Biome, Cage } from "./Cage.js";
+import { List } from "./List.js";
+
+export class UIManager {
+	constructor() {}
+
+	initMainUI() {
+		// getMenuButton();
+		// initExpandableMenu();
+	}
+}
+
+function getMenuButton(): HTMLElement {
+	console.log("Getting main menu button...");
+	return document.querySelector(".js-menu-button-main") as HTMLElement;
+}
+
+function getMenuAnimalsButton(): HTMLElement {
+	return document.querySelector(".js-menu-button-animals") as HTMLElement;
+}
+
+function getMenuCagesButton(): HTMLElement {
+	return document.querySelector(".js-menu-button-cages") as HTMLElement;
+}
+
+function getMenu(): HTMLElement {
+	return document.querySelector(".js-menu") as HTMLElement;
+}
+
+function getMenuList(menuList: HTMLElement): HTMLUListElement {
+	return menuList.querySelector(".js-menu-list") as HTMLUListElement;
+}
+
+function createMenuListItem(
+	title: string,
+	imgUrl: string,
+	infoIcon?: string
+): HTMLElement {
+	const elementAnimalItem = document.createElement("li");
+	elementAnimalItem.classList.add("ui-menu__item");
+	elementAnimalItem.classList.add("js-menu-list-item");
+
+	const card_perfix = "object-card";
+
+	const elementCard = document.createElement("div") as HTMLDivElement;
+	elementCard.classList.add(card_perfix);
+
+	const elementCardInfo = document.createElement("div") as HTMLDivElement;
+	elementCardInfo.classList.add(card_perfix + "__info");
+
+	const elementCardTitle = document.createElement("h3") as HTMLHeadElement;
+	elementCardTitle.classList.add(card_perfix + "__title");
+	elementCardTitle.innerText = title;
+
+	const elementCardImage = document.createElement("img") as HTMLImageElement;
+	elementCardImage.classList.add(card_perfix + "__img");
+	elementCardImage.src = imgUrl;
+
+	elementCardInfo.append(elementCardTitle);
+
+	if (infoIcon) {
+		const elementCardInfoIcon = document.createElement("div") as HTMLDivElement;
+		elementCardInfoIcon.classList.add(card_perfix + "__info-icon");
+		elementCardInfoIcon.classList.add(card_perfix + "__info-icon--" + infoIcon);
+		elementCardInfo.append(elementCardInfoIcon);
+	}
+
+	elementCard.append(elementCardInfo);
+	elementCard.append(elementCardImage);
+
+	elementAnimalItem.append(elementCard);
+
+	return elementAnimalItem;
+}
+
+function createMenuListItemAnimal(type: AnimalType): HTMLElement {
+	const title = type.getName();
+	const dietName = type.getDiet().getName().toLowerCase();
+	const imageURL = getImageByAnimalType(type);
+
+	return createMenuListItem(title, imageURL, dietName);
+}
+
+function createMenuListItemCage(biome: Biome): HTMLElement {
+	const title = Biome[biome];
+	const imageURL = getImageByBiomeType(biome);
+
+	return createMenuListItem(title, imageURL);
+}
+
+function getImageByAnimalType(type: AnimalType) {
+	const animalName = type.getName().toLowerCase();
+	return `/assets/animals/${animalName}.svg`;
+}
+function getImageByBiomeType(biome: Biome) {
+	const biomeName = Biome[biome];
+	return `/assets/cage/cage-${biomeName}.svg`;
+}
+
+function initExpandableMenu(
+	animalsList: List<HTMLElement>,
+	biomesList: List<HTMLElement>
+) {
+	const menuMainButton = getMenuButton();
+
+	const menuAnimalsButton = getMenuAnimalsButton();
+	const menuCagesButton = getMenuCagesButton();
+
+	const menu = getMenu();
+	const menuList = getMenuList(menu);
+	menu.hidden = true;
+
+	const expandableButtons = document.querySelector(
+		".js-expandable-buttons"
+	) as HTMLElement;
+	expandableButtons.hidden = true;
+
+	menuAnimalsButton.addEventListener("click", (e) => {
+		menuList.innerHTML = "";
+		animalsList.forEach((animal) => {
+			menuList.appendChild(animal);
+		});
+		menu.hidden = false;
+	});
+
+	menuCagesButton.addEventListener("click", (e) => {
+		menuList.innerHTML = "";
+		biomesList.forEach((biome) => {
+			menuList.appendChild(biome);
+		});
+		menu.hidden = false;
+	});
+
+	menuMainButton.addEventListener("click", (e) => {
+		menu.hidden = true;
+		expandableButtons.hidden = !expandableButtons.hidden;
+	});
+}
+
+const biomesMenuItems = new List<HTMLElement>("Biomes UI Items");
+
+biomesMenuItems.add(createMenuListItemCage(Biome.dessert));
+biomesMenuItems.add(createMenuListItemCage(Biome.mudland));
+biomesMenuItems.add(createMenuListItemCage(Biome.tundra));
+biomesMenuItems.add(createMenuListItemCage(Biome.grassland));
+biomesMenuItems.add(createMenuListItemCage(Biome.forest));
+
+const animalsMenuItems = new List<HTMLElement>("Animals UI Items");
+animalTypes.forEach((type) => {
+	animalsMenuItems.add(createMenuListItemAnimal(type));
+});
+
+initExpandableMenu(animalsMenuItems, biomesMenuItems);
--- student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-18/home-project/zoomoji-app/app.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-18/home-project/zoomoji-app/app.ts
@@ -1,39 +1,38 @@
 import { Animal } from "./Animal.js";
-import { foodList } from "./AnimalDiet.js";
-import {
-	AnimalType,
-	Gender,
-	bear,
-	camel,
-	human,
-	lizard,
-	penguin,
-} from "./AnimalType.js";
+import { Gender, bear, camel, human, lizard, penguin } from "./AnimalType.js";
 import { Cage } from "./Cage.js";
 import { GameManager } from "./GameManager.js";
+import { UIManager } from "./UIManager.js";
 
 const gameManager = new GameManager();
+const uiManager = new UIManager();
 
-const cage = new Cage("First");
+function initGame() {
+	const cage = new Cage("First");
 
-const animalA = new Animal(1, "Yulia", Gender.Female, penguin);
-const animalB = new Animal(2, "Vlad", Gender.Male, camel);
-const animalC = new Animal(3, "Artsiom", Gender.Male, bear);
-const animalD = new Animal(4, "Masha", Gender.Female, human);
-const animalE = new Animal(5, "Dana", Gender.Female, lizard);
+	const animalA = new Animal(1, "Yulia", Gender.Female, penguin);
+	const animalB = new Animal(2, "Vlad", Gender.Male, camel);
+	const animalC = new Animal(3, "Artsiom", Gender.Male, bear);
+	const animalD = new Animal(4, "Masha", Gender.Female, human);
+	const animalE = new Animal(5, "Dana", Gender.Female, lizard);
 
-gameManager.addGameObject(animalA);
-gameManager.addGameObject(animalB);
-gameManager.addGameObject(animalC);
-gameManager.addGameObject(animalD);
-gameManager.addGameObject(animalE);
-gameManager.addGameObject(cage);
+	gameManager.addGameObject(animalA);
+	gameManager.addGameObject(animalB);
+	gameManager.addGameObject(animalC);
+	gameManager.addGameObject(animalD);
+	gameManager.addGameObject(animalE);
+	gameManager.addGameObject(cage);
 
-cage.addAnimal(animalA);
-cage.addAnimal(animalB);
-cage.addAnimal(animalC);
-cage.addAnimal(animalD);
-cage.addAnimal(animalE);
+	cage.addAnimal(animalA);
+	cage.addAnimal(animalB);
+	cage.addAnimal(animalC);
+	cage.addAnimal(animalD);
+	cage.addAnimal(animalE);
+}
+
+function initUI() {
+	uiManager.initMainUI();
+}
 
 function zoo() {
 	let userInput = true;
@@ -51,4 +50,6 @@ function zoo() {
 	console.log("Exiting Zoo Tycoon");
 }
 
-zoo();
+initGame();
+initUI();
+// zoo();
