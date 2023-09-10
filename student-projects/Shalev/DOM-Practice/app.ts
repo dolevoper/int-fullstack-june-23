f83f30789690a48d5c3de0bbb5d66cd674783bab
shalev <shalevari2008@gmail.com>
2023-09-10 17:06:58 +0300

    add(buttons): add

--- student-projects/Shalev/DOM-Practice/app.ts
+++ student-projects/Shalev/DOM-Practice/app.ts
@@ -1,10 +1,26 @@
 const title = document.querySelector<HTMLHeadingElement>(
 	'.title-container__title'
 )
-const button = document.querySelector<HTMLButtonElement>(
-	'.button-container__btn'
+const buttonRED = document.querySelector<HTMLButtonElement>(
+	'.button-container-red__btn'
+)
+const buttonWHITE = document.querySelector<HTMLButtonElement>(
+	'.button-container-white__btn'
 )
 
-button?.addEventListener('click', (e) => {
-	title!.style.color = 'red'
+buttonRED?.addEventListener('click', (e) => {
+	if (title?.className !== 'isRed') {
+		title!.style.color = 'red'
+		title?.classList.add('isRed')
+	} else {
+		console.log('ERR.')
+	}
+})
+buttonWHITE?.addEventListener('click', (e) => {
+	if (title?.className !== 'isWhite') {
+		title!.style.color = 'white'
+		title!.classList.add('isWhite')
+	} else {
+		console.log('ERR.')
+	}
 })
