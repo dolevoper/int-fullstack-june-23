ea22173407eaecd99a72843e5f2d7e4ebf89cc40
elad-her <eladher18@gmail.com>
2023-09-11 17:24:51 +0300

    eladHomeWorkTry

---
+++ student-projects/Elad/class-019/shopping-list.ts
@@ -0,0 +1,18 @@
+//fathers//
+const fruitsList = document.querySelector(".fruitsList") as HTMLUListElement;
+const vegetablesList = document.querySelector(".vegetablesList") as HTMLUListElement;
+const DairyProductsList = document.querySelector(".Dairy-productsList") as HTMLUListElement;
+//sons//
+const liFruitsList = document.createElement("li")
+const liVegetablesList = document.createElement("li")
+const liDairyProductsList = document.createElement("li") 
+//conected//
+fruitsList.append(liFruitsList)
+vegetablesList.append(liFruitsList)
+DairyProductsList.append(liDairyProductsList)
+//get a value to enter//
+
+//--------------------------------------------------------------------//
+
+
+//--------------------------------------------------------------------//
