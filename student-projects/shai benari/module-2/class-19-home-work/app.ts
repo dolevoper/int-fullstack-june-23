92080a6410f73333d1f3ba67bbb082ab667c1534
shai-benari <s.benari1962@gmail.com>
2023-09-12 13:41:09 +0300

    button-one

--- student-projects/shai benari/module-2/class-19-home-work/app.ts
+++ student-projects/shai benari/module-2/class-19-home-work/app.ts
@@ -1,1 +1,12 @@
+const buttonOne = document.querySelector(".button1") as HTMLButtonElement
+function buttomAlert(){
+    alert("button one is clicked");
+}
+buttonOne.addEventListener("click" ,buttomAlert );
 
+const Bgcolor = document.querySelector(".button2") as HTMLButtonElement
+function changeBgcolor(){
+    Bgcolor.style.backgroundColor = "red"
+    alert("MOUSE OVER IS APPLIDE")
+}
+Bgcolor.addEventListener("mouseover" ,changeBgcolor );
