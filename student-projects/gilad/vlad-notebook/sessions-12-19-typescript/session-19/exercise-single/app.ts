6ac90739fe47ecd84fb1363dbb81e53708c500f9
BraveVlad <giladvladpinker@gmail.com>
2023-09-12 22:10:35 +0300

    added class 19 single exercise

---
+++ student-projects/gilad/vlad-notebook/sessions-12-19-typescript/session-19/exercise-single/app.ts
@@ -0,0 +1,35 @@
+// Get an image of a single project
+const androidProject = window.document.querySelector(".android>.project_item");
+const projectImage = androidProject?.querySelector("img");
+console.log("image of the first project image:");
+console.log(projectImage?.src);
+
+// Get all project items with an image
+
+const allPorjects = document.querySelectorAll(".project_item");
+
+// Covnert to arry
+const allProjectsArray = Array.from(allPorjects);
+const projectsWithImage = Array.from(
+	allProjectsArray.filter(function (element) {
+		return element.querySelector("img") as HTMLImageElement;
+	})
+);
+
+console.log(`There are ${projectsWithImage.length} projects with images`);
+
+// Apply default images to all projects without an image
+
+for (const element of allProjectsArray) {
+	// console.log("current element: " + element.innerHTML);
+	if (!element.querySelector("img")) {
+		const defaultProjectImageElement = document.createElement(
+			"Img"
+		) as HTMLImageElement;
+		defaultProjectImageElement.src = "./assets/projects/default.png";
+		defaultProjectImageElement.setAttribute("width", "32");
+		defaultProjectImageElement.setAttribute("height", "32");
+		element.append(defaultProjectImageElement);
+		console.log("Found project without image");
+	}
+}
