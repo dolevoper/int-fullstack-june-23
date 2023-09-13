ba1664c29764b7139c6ccfcd89d910b9e67f4192
BraveVlad <giladvladpinker@gmail.com>
2023-09-13 10:53:37 +0300

    Implemented Point and Rectangle classes

---
+++ student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-20/ninja-chef/ninjachef-app/Rectangle.ts
@@ -0,0 +1,50 @@
+import { Point } from "./Point";
+
+export enum RectangleSide {
+	TOP,
+	BOTTOM,
+	LEFT,
+	RIGHT,
+}
+
+export class Rectangle extends DOMRect {
+	readonly center;
+
+	readonly topLeft;
+	readonly topRight;
+	readonly bottomLeft;
+	readonly bottomRight;
+
+	constructor(domRectangle: DOMRect) {
+		super();
+
+		this.topLeft = new Point(domRectangle.left, domRectangle.top);
+		this.topRight = new Point(domRectangle.right, domRectangle.top);
+		this.bottomLeft = new Point(domRectangle.left, domRectangle.bottom);
+		this.bottomRight = new Point(domRectangle.right, domRectangle.bottom);
+
+		this.center = Rectangle.getCenterOfRectangle(this);
+	}
+
+	public contains(point: Point): boolean {
+		return (
+			this.topLeft.x <= point.x &&
+			point.x <= this.bottomRight.x &&
+			this.topLeft.y <= point.y &&
+			point.y <= this.bottomRight.y
+		);
+	}
+
+	static getCenterOfRectangle(rectangle: Rectangle): Point {
+		const xMiddle = Point.getMiddleOfLine(
+			rectangle.topLeft,
+			rectangle.topRight
+		);
+		const yMiddle = Point.getMiddleOfLine(
+			rectangle.bottomLeft,
+			rectangle.bottomRight
+		);
+
+		return new Point(xMiddle.x, yMiddle.y);
+	}
+}
