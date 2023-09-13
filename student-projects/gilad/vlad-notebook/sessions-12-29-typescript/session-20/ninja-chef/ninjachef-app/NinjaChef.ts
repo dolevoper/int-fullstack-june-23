a2d584b2900df8e2bd252ab86a85ddb8549e8f80
BraveVlad <giladvladpinker@gmail.com>
2023-09-13 13:14:22 +0300

    Implemented Parabola and sine wave movement!!!!!!!! * finally omg

--- student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-20/ninja-chef/ninjachef-app/NinjaChef.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-20/ninja-chef/ninjachef-app/NinjaChef.ts
@@ -1,58 +1,77 @@
 import { Game } from "./Game.js";
+import { GameObject } from "./GameObject.js";
+import { GameScreen } from "./GameScreen.js";
+import { log } from "./helpers.js";
 
 export class NinjaChef extends Game {
-	box!: HTMLElement;
+	static TAG = "NinjaChef";
+
+	screen!: GameScreen;
+
+	box!: GameObject;
 	xPos!: number;
 	yPos!: number;
+	xVertex: number;
+	yVertex: number;
+	time: number;
+	amplitude: number;
 
-	speed!: HTMLElement;
-	xPosDelta!: number;
+	deltaView!: HTMLElement;
 	delta!: string;
 
 	constructor() {
 		super();
 
-		this.debugGameStateLogs(true);
-
-		this.start();
+		this.initGameScreen();
+		this.startGame();
 	}
 
 	onLoad = () => {
-		let state = false;
-		this.box = document.querySelector(".box") as HTMLElement;
-		this.speed = document.querySelector(".delta") as HTMLElement;
-		this.speed.addEventListener("click", () => {
-			if (!state) {
-				this.pause();
-				state = !state;
-				console.log(state);
-			} else {
-				this.resume();
-				state = !state;
-				console.log(state);
-			}
-		});
-		this.xPos = Number(this.box.style.left);
-		this.yPos = Number(this.box.style.top);
+		const boxElement = document.querySelector(".box") as HTMLElement;
+		this.deltaView = document.querySelector(".delta") as HTMLElement;
 
-		this.xPosDelta = 0;
+		// this.xPos = Number(this.boxView.style.left);
+		// this.yPos = Number(this.boxView.style.top);
+		this.box = new GameObject(boxElement);
+		// this.xVertex = this.screen.getCenter().x - this.box.hitbox.center.x;
+		this.yVertex = this.screen.getCenter().y - this.box.hitbox.height;
+		this.xVertex = this.screen.getCenter().x - this.box.hitbox.center.x;
+		this.yPos = 0;
+		this.xPos = this.xVertex / 1.2;
+		this.time = 0;
+		this.amplitude = 200;
 	};
 
 	onUpdate = (deltaTime: number) => {
-		this.xPos += 0.05 * deltaTime;
-		this.yPos += 0.05 * deltaTime;
-
-		this.xPosDelta = deltaTime;
 		this.delta = deltaTime.toString();
+		// this.time += 1 / (10 * deltaTime);
+
+		// sin wave
+		// this.yPos += 1;
+		// this.yPos = this.yVertex + this.amplitude * Math.sin(this.time);
+		// this.xPos = Math.sqrt(-this.yPos / 1) + 1;
+		this.xPos += (0.5 * deltaTime) / 16;
+		this.yPos = this.yVertex + 0.5 * Math.pow(this.xPos - this.xVertex, 2);
 	};
 
 	onRender = () => {
-		this.box.style.left = `${this.xPos}px`;
-		this.box.style.top = `${this.yPos}px`;
-		this.speed.textContent = `DELTA: ${this.delta} `;
+		this.box.view.style.left = `${this.xPos}px`;
+		this.box.view.style.top = `${this.yPos}px`;
+		this.deltaView.textContent = `DELTA: ${this.delta} `;
 	};
 
-	onPause = () => {
-		// console.log("NINJA CHEF ON ON PAUSE!");
-	};
+	onPause = () => {};
+
+	onResume = () => {};
+
+	onExit = () => {};
+
+	initGameScreen() {
+		const screenElement = document.querySelector(
+			".game-screen"
+		) as HTMLBodyElement;
+		this.screen = new GameScreen(screenElement);
+		log(NinjaChef.TAG, "Created game screen. middle point is: ");
+		console.log(this.screen.boundaries.center);
+	}
 }
