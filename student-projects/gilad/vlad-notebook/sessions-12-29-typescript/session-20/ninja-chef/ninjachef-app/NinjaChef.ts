7456955fdde0dcac84b17563250c4c8bfe3d2c29
BraveVlad <giladvladpinker@gmail.com>
2023-09-13 06:44:37 +0300

    Game, animation and frame rate testing

--- student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-20/ninja-chef/ninjachef-app/NinjaChef.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-20/ninja-chef/ninjachef-app/NinjaChef.ts
@@ -1,5 +1,14 @@
 import { Game } from "./Game.js";
+
 export class NinjaChef extends Game {
+	box!: HTMLElement;
+	xPos!: number;
+	yPos!: number;
+
+	speed!: HTMLElement;
+	xPosDelta!: number;
+	delta!: string;
+
 	constructor() {
 		super();
 
@@ -9,6 +18,41 @@ export class NinjaChef extends Game {
 	}
 
 	onLoad = () => {
-		console.log("NINJA CHEF ON LOAD!");
+		let state = false;
+		this.box = document.querySelector(".box") as HTMLElement;
+		this.speed = document.querySelector(".delta") as HTMLElement;
+		this.speed.addEventListener("click", () => {
+			if (!state) {
+				this.pause();
+				state = !state;
+				console.log(state);
+			} else {
+				this.resume();
+				state = !state;
+				console.log(state);
+			}
+		});
+		this.xPos = Number(this.box.style.left);
+		this.yPos = Number(this.box.style.top);
+
+		this.xPosDelta = 0;
+	};
+
+	onUpdate = (deltaTime: number) => {
+		this.xPos += 0.05 * deltaTime;
+		this.yPos += 0.05 * deltaTime;
+
+		this.xPosDelta = deltaTime;
+		this.delta = deltaTime.toString();
+	};
+
+	onRender = () => {
+		this.box.style.left = `${this.xPos}px`;
+		this.box.style.top = `${this.yPos}px`;
+		this.speed.textContent = `DELTA: ${this.delta} `;
+	};
+
+	onPause = () => {
+		// console.log("NINJA CHEF ON ON PAUSE!");
 	};
 }
--- student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-20/ninja-chef/ninjachef-app/helpers.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-20/ninja-chef/ninjachef-app/helpers.ts
@@ -1,7 +1,3 @@
 export function log(tag: string, message: string) {
 	console.log(`${tag} | ${message}`);
 }
-
-export function calculateDeltaTime(time: number, previousTime: number): number {
-	return time - previousTime;
-}
