8e8709971d5811112542454af8fd54b751e6a528
Assaf Basri <Faskoman90@gmail.com>
2023-09-14 11:28:22 +0300

    advanced

--- student-projects/Assaf_Project/Module-II/class-020-11-09-2023/HP/app.ts
+++ student-projects/Assaf_Project/Module-II/class-020-11-09-2023/HP/app.ts
@@ -1,32 +1,47 @@
 console.log("connected");
 
+let score = 0;
+updateScore();
+
+function updateScore() {
+  const scoreSpan = document.querySelector(".score");
+  if (scoreSpan) {
+    scoreSpan.textContent = `Score: ${score}`;
+  }
+}
+
 function hitDisc() {
   console.log("Disc Hit!");
 }
 
-const gameLoop = setInterval(addDisc, 4000);
+const gameLoop = setInterval(addDisc, 1000);
 function addDisc() {
   const disc = document.createElement("div");
   disc.className = "disc";
   disc.addEventListener("click", hitDisc);
-  const randomTop = Math.floor(Math.random() * (400 - 90 + 1)) + 90;
+  const randomTop = Math.floor(Math.random() * (400 - 100 + 1)) + 100;
   disc.style.top = `${randomTop}px`;
   document.body.appendChild(disc);
 
   disc.onclick = () => {
     disc.remove();
+    score += 10;
+    updateScore();
   };
 
-  const discLeft = parseFloat(getComputedStyle(disc).left);
-  const discWidth = parseFloat(getComputedStyle(disc).width);
-  const screenWidth = window.innerWidth;
-  if (discLeft + discWidth >= screenWidth) {
-    disc.remove();
-  }
-  window.addEventListener("keydown", (ev) => {
-    if (ev.key === "Escape") {
-      clearInterval(gameLoop);
+  const missedDisc = setInterval(() => {
+    const discLeft = parseFloat(getComputedStyle(disc).left);
+    const discWidth = parseFloat(getComputedStyle(disc).width);
+    const screenWidth = window.innerWidth;
+    if (discLeft + discWidth * 0.2 > screenWidth) {
       disc.remove();
+      clearInterval(missedDisc);
     }
+    window.addEventListener("keydown", (ev) => {
+      if (ev.key === "Escape") {
+        clearInterval(gameLoop);
+        disc.remove();
+      }
+    });
   });
 }
