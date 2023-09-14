ad761721349c81b230844a0b1613518a3c8ced59
shai-benari <s.benari1962@gmail.com>
2023-09-14 11:31:31 +0300

    project-end

--- student-projects/shai benari/module-2/class-19-home-work/app.ts
+++ student-projects/shai benari/module-2/class-19-home-work/app.ts
@@ -6,14 +6,21 @@ buttonOne.addEventListener("click" ,buttomAlert );
 
 const Bgcolor = document.querySelector(".button2") as HTMLButtonElement
 function changeBgcolor(){
-    Bgcolor.style.backgroundColor = "red"
-    alert("MOUSE OVER IS APPLIDE")
+    Bgcolor.style.backgroundColor = "red";
+
+    alert("MOUSE OVER IS APPLIDE");
 }
 Bgcolor.addEventListener("mouseover" ,changeBgcolor );
 
-// const Status = document.querySelector(".button3") as HTMLButtonElement
-// function changStatus(){
-   
-//     Status.target.inenrText = "clicked"
-// }
-// Status.addEventListener("click" ,changStatus );
+
+const Status = document.querySelector(".button3") as HTMLButtonElement
+function changStatus(){
+    Status.innerText = "clicked"
+}
+Status.addEventListener("click" ,changStatus );
+
+const Status1 = document.querySelector(".button3") as HTMLButtonElement
+function changStatus1(){
+    Status.innerText = "Enter"
+}
+Status.addEventListener("mouseover" ,changStatus1 );
