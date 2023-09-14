fc83d7f0a40ebf5e0afc8b9b641fd5543acba2d8
Assaf Basri <Faskoman90@gmail.com>
2023-09-14 09:54:44 +0300

    added function hitDisc

--- student-projects/Assaf_Project/Module-II/class-020-11-09-2023/HP/app.ts
+++ student-projects/Assaf_Project/Module-II/class-020-11-09-2023/HP/app.ts
@@ -1,5 +1,9 @@
 console.log("connected");
 
 function hitDisc() {
-console.log("Disc Hit!")
+  console.log("Disc Hit!");
+}
+function addDisc() {
+  const disc = document.createElement("div" + ".disc");
+  disc.addEventListener("click", hitDisc);
 }
