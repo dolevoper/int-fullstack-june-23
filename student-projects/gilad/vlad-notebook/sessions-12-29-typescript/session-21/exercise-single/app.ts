527b8adc50790086eda49f51c80f4b74bdda0213
BraveVlad <giladvladpinker@gmail.com>
2023-09-14 20:16:11 +0300

    added status field with form and validation

--- student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-21/exercise-single/app.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-21/exercise-single/app.ts
@@ -1,76 +1,94 @@
 // Class project
-// 1. Add new field to car: status. Can be "normal", "free", "discount", "banned". In the form, "free" should be the default.
+// [x]1. Add new field to car: status. Can be "normal", "free", "discount", "banned". In the form, "free" should be the default.
 // 2. Show the cars list and allow to edit car status
 // 3. Add a new field "discountRate" - when status is "discount" this should have the discount rate
 // (Choose 2 or 3, or do both if you finish the first quickly)
 
 type LicenseType = "A" | "B" | "C";
+type Status = "normal" | "free" | "discount" | "banned";
 
 type Car = {
-    registrationNumber: string;
-    brand: string;
-    type?: string;
-    color: string;
-    licenseType: LicenseType;
+	registrationNumber: string;
+	brand: string;
+	type?: string;
+	color: string;
+	licenseType: LicenseType;
+	status: Status;
 };
 
 type CarArray = Car[];
 
 const cars: CarArray = [];
 
-const addCarForm = document.querySelector("form[name='add-new-car']") as HTMLFormElement | null;
+const addCarForm = document.querySelector(
+	"form[name='add-new-car']"
+) as HTMLFormElement | null;
 if (!addCarForm) {
-    console.error("Couldn't find add car form.");
+	console.error("Couldn't find add car form.");
 } else {
-    addCarForm.addEventListener("submit", function (e) {
-        e.preventDefault();
+	addCarForm.addEventListener("submit", function (e) {
+		e.preventDefault();
 
-        const formData = new FormData(e.target as HTMLFormElement);
+		const formData = new FormData(e.target as HTMLFormElement);
 
-        cars.push({
-            registrationNumber: getRequiredString(formData, "registrationNumber"),
-            brand: getRequiredString(formData, "brand"),
-            type: getString(formData, "type"),
-            color: getRequiredString(formData, "color"),
-            licenseType: parseLicenseType(getRequiredString(formData, "licenseType"))
-        });
+		cars.push({
+			registrationNumber: getRequiredString(formData, "registrationNumber"),
+			brand: getRequiredString(formData, "brand"),
+			type: getString(formData, "type"),
+			color: getRequiredString(formData, "color"),
+			licenseType: parseLicenseType(getRequiredString(formData, "licenseType")),
+			status: parseStatus(getRequiredString(formData, "status")),
+		});
 
-        console.log(cars);
-    });
+		console.log(cars);
+	});
 }
 
 function getString(formData: FormData, key: string) {
-    const value = formData.get(key);
+	const value = formData.get(key);
 
-    if (value == null) {
-        throw new Error(`Field ${key} doesn't exist!`);
-    }
+	if (value == null) {
+		throw new Error(`Field ${key} doesn't exist!`);
+	}
 
-    if (typeof value !== "string") {
-        throw new Error(`Value of field ${key} is not a string!`);
-    }
+	if (typeof value !== "string") {
+		throw new Error(`Value of field ${key} is not a string!`);
+	}
 
-    if (!value) {
-        return undefined;
-    }
+	if (!value) {
+		return undefined;
+	}
 
-    return value;
+	return value;
 }
 
 function getRequiredString(formData: FormData, key: string) {
-    const value = getString(formData, key);
+	const value = getString(formData, key);
 
-    if (!value) {
-        throw new Error(`Value for ${key} is required!`);
-    }
+	if (!value) {
+		throw new Error(`Value for ${key} is required!`);
+	}
 
-    return value;
+	return value;
 }
 
 function parseLicenseType(value: string): LicenseType {
-    if (value !== "A" && value !== "B" && value !== "C") {
-        throw new Error(`Invalid license type: ${value}`);
-    }
+	if (value !== "A" && value !== "B" && value !== "C") {
+		throw new Error(`Invalid license type: ${value}`);
+	}
 
-    return value;
+	return value;
+}
+
+function parseStatus(value: string): Status {
+	if (
+		value !== "normal" &&
+		value !== "free" &&
+		value !== "discount" &&
+		value !== "banned"
+	) {
+		throw new Error(`Invalid status type: ${value}`);
+	}
+
+	return value;
 }
