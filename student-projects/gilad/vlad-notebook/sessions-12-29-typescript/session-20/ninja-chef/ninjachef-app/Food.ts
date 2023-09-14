6eefa9fd4f585b08fe2d7d15e297b15c97a199f7
BraveVlad <giladvladpinker@gmail.com>
2023-09-14 15:27:58 +0300

    Food now generate its own element, now can dynamically set jump parameters

--- student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-20/ninja-chef/ninjachef-app/Food.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-20/ninja-chef/ninjachef-app/Food.ts
@@ -1,26 +1,25 @@
+import { FoodElement } from "./FoodElement.js";
+import { FoodType } from "./FoodType.js";
 import { GameObject } from "./GameObject.js";
 import { GameScreen } from "./GameScreen.js";
 import { Point } from "./Point.js";
 
 export class Food extends GameObject {
+	foodType: FoodType;
 	jumpStart!: Point;
 	jumpEnd!: Point;
 	jumpHeight!: number;
 	jumpSpeed!: number;
 
-	constructor(
-		id: number,
-		name: string,
-		view: HTMLElement,
-		imageUrl: string,
-		screen: GameScreen
-	) {
-		super(id, name, view, imageUrl, screen);
-
+	constructor(id: number, foodType: FoodType, screen: GameScreen) {
+		super(id, foodType.name, screen);
+		this.foodType = foodType;
 		this.initialise();
 	}
 
 	public initialise(): void {
+		this.generateFoodElement();
+
 		this.position.x = 50;
 		this.position.y = this.screen.boundaries.height;
 		const start = new Point(50, this.screen.boundaries.height);
@@ -30,13 +29,6 @@ export class Food extends GameObject {
 		);
 
 		this.setJump(start, end, 500, 1);
-	}
-
-	public setJump(start: Point, end: Point, height: number, speed: number) {
-		this.jumpStart = start;
-		this.jumpEnd = end;
-		this.jumpHeight = height;
-		this.jumpSpeed = speed;
 	}
 
 	public update(gameTime: number): void {
@@ -55,5 +47,17 @@ export class Food extends GameObject {
 	public draw(): void {
 		this.view.style.left = `${this.position.x}px`;
 		this.view.style.top = `${this.position.y}px`;
+	}
+
+	public setJump(start: Point, end: Point, height: number, speed: number) {
+		this.jumpStart = start;
+		this.jumpEnd = end;
+		this.jumpHeight = height;
+		this.jumpSpeed = speed;
+	}
+
+	private generateFoodElement() {
+		const foodElement = new FoodElement(this.foodType);
+		this.setElement(foodElement);
 	}
 }
