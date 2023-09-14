12916ed44f80843005f7458815affc4f5ae65bfe
Assaf Basri <Faskoman90@gmail.com>
2023-09-14 10:47:02 +0300

    added random number (90-400px) for height of throw

--- student-projects/Assaf_Project/Module-II/class-020-11-09-2023/HP/app.ts
+++ student-projects/Assaf_Project/Module-II/class-020-11-09-2023/HP/app.ts
@@ -3,7 +3,30 @@ console.log("connected");
 function hitDisc() {
   console.log("Disc Hit!");
 }
+
+const gameLoop = setInterval(addDisc, 4000);
 function addDisc() {
-  const disc = document.createElement("div" + ".disc");
+  const disc = document.createElement("div");
+  disc.className = "disc";
   disc.addEventListener("click", hitDisc);
+  const randomTop = Math.floor(Math.random() * (400 - 90 + 1)) + 90;
+  disc.style.top = `${randomTop}px`;
+  document.body.appendChild(disc);
+
+  disc.onclick = () => {
+    disc.remove();
+  };
+
+  const discLeft = parseFloat(getComputedStyle(disc).left);
+  const discWidth = parseFloat(getComputedStyle(disc).width);
+  const screenWidth = window.innerWidth;
+  if (discLeft + discWidth >= screenWidth) {
+    disc.remove();
+  }
+  window.addEventListener("keydown", (ev) => {
+    if (ev.key === "Escape") {
+      clearInterval(gameLoop);
+      disc.remove();
+    }
+  });
 }
