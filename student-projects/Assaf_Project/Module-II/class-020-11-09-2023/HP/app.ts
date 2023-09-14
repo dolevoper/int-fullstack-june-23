645fe23f9e2a8b5eed718f1aef2e6acef34e36c0
Assaf Basri <Faskoman90@gmail.com>
2023-09-14 14:15:54 +0300

    adding li.lives__hearts from ts

--- student-projects/Assaf_Project/Module-II/class-020-11-09-2023/HP/app.ts
+++ student-projects/Assaf_Project/Module-II/class-020-11-09-2023/HP/app.ts
@@ -1,7 +1,22 @@
 console.log("connected");
 
 let score = 0;
+
+const livesContainer = document.querySelector(".lives");
 let remainingLives = 5;
+
+const heartsArray: HTMLLIElement[] = [];
+
+for (let i = 0; i < remainingLives; i++) {
+  const livesHeart = document.createElement("li");
+  livesHeart.className = "lives__hearts";
+  const heartImg = document.createElement("img");
+  heartImg.src = "assets/heart.png";
+  heartImg.alt = "life heart";
+  livesHeart.appendChild(heartImg);
+  livesContainer?.appendChild(livesHeart);
+  heartsArray.push(livesHeart);
+}
 
 updateScore();
 
@@ -14,6 +29,9 @@ function updateScore() {
 
 function hitDisc() {
   console.log("Disc Hit!");
+  
+  score += 10;
+  updateScore();
 }
 
 const gameLoop = setInterval(addDisc, 3000);
@@ -25,35 +43,37 @@ function addDisc() {
   disc.style.top = `${randomTop}px`;
   document.body.appendChild(disc);
 
-  disc.onclick = () => {
+  function hitDisc() {
+    console.log("Disc Hit!");
     disc.remove();
     score += 10;
     updateScore();
-  };
+  }
 
   window.addEventListener("keydown", (ev) => {
     if (ev.key === "Escape") {
       clearInterval(gameLoop);
       disc.remove();
     }
-  })
+  });
 
   const missedDisc = setInterval(() => {
     const discLeft = parseFloat(getComputedStyle(disc).left);
     const discWidth = parseFloat(getComputedStyle(disc).width);
     const screenWidth = window.innerWidth;
-    const heart = document.querySelector(".lives__hearts");
-    // const lives = document.getElementsByClassName("lives");
     if (discLeft + discWidth * 0.2 > screenWidth) {
       disc.remove();
       clearInterval(missedDisc);
-      heart?.remove();
-      remainingLives--;
-      if (remainingLives === 0) {
+      if (remainingLives > 0) {
+        remainingLives--;
+        heartsArray.pop();
+        livesContainer?.removeChild(livesContainer.lastChild as HTMLElement);
+      }
+      if (heartsArray.length === 0) {
         clearInterval(gameLoop);
         alert(`Your score is ${score}`);
+        disc.remove();
       }
-      ;
     }
   });
 }
