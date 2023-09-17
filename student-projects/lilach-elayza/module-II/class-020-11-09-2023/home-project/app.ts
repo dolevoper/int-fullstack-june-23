c2d117abb76b24bd762a047a7e9ca407dab6e04d
Lilach E <lilachelayza@gmail.com>
2023-09-17 11:24:42 +0300

    finish up baloons homework

--- student-projects/lilach-elayza/module-II/class-020-11-09-2023/home-project/app.ts
+++ student-projects/lilach-elayza/module-II/class-020-11-09-2023/home-project/app.ts
@@ -1,6 +1,12 @@
 const popSound = document.getElementById("popSound") as HTMLAudioElement;
+const strikeSound = document.getElementById("strikeSound") as HTMLAudioElement;
 let gameLoop = setInterval(createBalloon, randomInterval(500, 2000));
 let score = 0;
+let lives = 3;
+
+window.addEventListener("load", () => {
+  updateHearts();
+});
 
 window.addEventListener("keydown", (ev) => {
   if (ev.key === "Escape") {
@@ -28,11 +34,28 @@ function createBalloon() {
   const moveBalloonUp = () => {
     let position = window.innerHeight;
     const interval = setInterval(() => {
-      position -= 2+ (score / 100);
+      position -= 2 + (score / 100);
+      balloon.onclick = () => {
+        popSound.play();
+        balloon.remove();
+        clearInterval(interval)
+        score += 10;
+        document.getElementById("score")!.textContent = `Score: ${score}`;
+      };
       balloon.style.top = position + "px";
       if (position <= -100) {
+        strikeSound.play();
         clearInterval(interval);
         balloon.remove();
+        if (lives > 0) {
+          lives--;
+          if (lives === 0) {
+            //clearInterval(gameLoop);
+            alert(`Game over\nYour score is ${score}`);
+            location.reload();
+          }
+          updateHearts();
+        }
       }
     }, 16);
 
@@ -41,11 +64,17 @@ function createBalloon() {
   };
 
   moveBalloonUp();
+
   
-    balloon.onclick = () => {
-      popSound.play();
-      balloon.remove();
-      score += 10;
-      document.getElementById("score")!.textContent = `Score: ${score}`;
-  };
+}
+
+function updateHearts() {
+  const heartsContainer = document.getElementById("hearts");
+  heartsContainer!.innerHTML = "";
+  for (let i = 0; i < lives; i++) {
+    const heartIcon = document.createElement("i");
+    heartIcon.classList.add("fa-solid", "fa-heart", "fa-beat", "fa-xl");
+    heartIcon.style.color = "#ff0000";
+    heartsContainer!.appendChild(heartIcon);
+  }
 }
