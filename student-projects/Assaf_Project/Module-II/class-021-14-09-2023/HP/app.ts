7d917cbf7cbd64386cc052a502daa4b6f3be2688
Assaf Basri <Faskoman90@gmail.com>
2023-09-21 00:07:50 +0300

    text align center for options

--- student-projects/Assaf_Project/Module-II/class-021-14-09-2023/HP/app.ts
+++ student-projects/Assaf_Project/Module-II/class-021-14-09-2023/HP/app.ts
@@ -1,17 +1,9 @@
 console.clear();
 
-const elementsToToggleDarkMode = document.querySelectorAll(
-  ".main, .card, .brand, .button, input, select"
-);
-const darkModeButton = document.querySelector(
-  "#dark-mode-button"
-) as HTMLButtonElement;
-darkModeButton.addEventListener("click", darkModeToggle);
+const elementsToToggleDarkMode = document.querySelectorAll(".main, .card, .brand, .button, input, select");
+const darkModeButton = document.querySelector("#dark-mode-button") as HTMLButtonElement;
 
-function darkModeToggle() {
-  darkModeButton.innerText =
-    darkModeButton.innerText === "DARK MODE" ? "light mode" : "dark mode";
-  elementsToToggleDarkMode.forEach((element) => {
-    element.classList.toggle("--dark-mode");
-  });
-}
+darkModeButton.addEventListener("click", () => { 
+  darkModeButton.innerText = darkModeButton.innerText === "DARK MODE" ? "light mode" : "dark mode";
+  elementsToToggleDarkMode.forEach((element) => { element.classList.toggle("--dark-mode")});
+});
