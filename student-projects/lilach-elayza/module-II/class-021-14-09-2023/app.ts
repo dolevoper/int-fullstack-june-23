11777245cf00a14ce2faf2a02756ded056e287c4
Lilach E <lilachelayza@gmail.com>
2023-09-20 16:44:19 +0300

    class 021 homework section 3 unfinished

--- student-projects/lilach-elayza/module-II/class-021-14-09-2023/app.ts
+++ student-projects/lilach-elayza/module-II/class-021-14-09-2023/app.ts
@@ -6,7 +6,6 @@
 
 type LicenseType = "A" | "B" | "C";
 type status = "free" | "discount" | "normal" | "banned";
-
 type Car = {
   registrationNumber: string;
   brand: string;
@@ -14,15 +13,13 @@ type Car = {
   color: string;
   licenseType: LicenseType;
   status: status;
+  discount?: string;
 };
-
 type CarArray = Car[];
-
 const cars: CarArray = [];
+const addCarForm = document.querySelector("form[name='add-new-car']") as HTMLFormElement | null;
+let carList = document.getElementById("car-list") as HTMLElement;
 
-const addCarForm = document.querySelector(
-  "form[name='add-new-car']"
-) as HTMLFormElement | null;
 if (!addCarForm) {
   console.error("Couldn't find add car form.");
 } else {
@@ -38,30 +35,46 @@ if (!addCarForm) {
       color: getRequiredString(formData, "color"),
       licenseType: parseLicenseType(getRequiredString(formData, "licenseType")),
       status: parseStatus(getRequiredString(formData, "status")),
+      discount: getOptionalString(formData, "discount"),
     });
 
-    let carList = document.getElementById("car-list") as HTMLElement;
-    if (!carList) {
-      carList = document.createElement("div");
-      carList.id = "car-list";
-    }
-    const listHTML = cars.map((car, index) => `
-        <div class="car" data-index="${index}">
-            <p>Registration #: ${car.registrationNumber}</p>
-            <p>Brand: ${car.brand}</p>
-            <p>Type: ${car.type}</p>
-            <p>Color: ${car.color}</p>
-            <p>License Type: ${car.licenseType}</p>
-            <p>Status: <span class="editable" contenteditable="true">${car.status}</span></p>
-        </div>`).join("");
-    carList.innerHTML = listHTML;
+    refreshCarList();
 
-    const editables = document.querySelectorAll(".editable");
-    editables.forEach((editable) => {
-      editable.addEventListener("blur", function () {
-        const index = this.parentElement.parentElement.dataset.index;
-        const newStatus = this.innerText;
-        cars[index].status = parseStatus(newStatus);
+    const editableStatus = document.querySelectorAll(".editable");
+    editableStatus.forEach((status: HTMLElement) => {
+      status.addEventListener("keypress", function (e: KeyboardEvent) {
+        if (e.key === "Enter") {
+          const index = parseInt(status.parentElement!.parentElement!.dataset.index || "");
+
+          if (!isNaN(index)) {
+            const newStatus = status.innerText;
+            cars[index].status = parseStatus(newStatus);
+
+            if (newStatus === "discount") {
+              const discountSpan = status.parentElement!.parentElement!.querySelector(".listedDiscount") as HTMLElement;
+              discountSpan.setAttribute("contenteditable", "true");
+              discountSpan.focus();
+
+              discountSpan.addEventListener("keypress", (e: KeyboardEvent) => {
+                if (e.key === "Enter") {
+                  const newIndex = parseInt(discountSpan.parentElement!.parentElement!.dataset.index || "");
+                  if (!isNaN(newIndex)) {
+                    const newRate = discountSpan.innerText;
+                    cars[newIndex].discount = validateRate(newRate);
+                  }
+                  discountSpan.blur();
+                  refreshCarList();
+                }
+              });
+            } else {
+              const discountSpan = status.parentElement!.parentElement!.querySelector(".listedDiscount") as HTMLElement;
+              discountSpan.removeAttribute("contenteditable");
+              cars[index].discount = undefined;
+              refreshCarList();
+            }
+          }
+          status.blur();
+        }
       });
     });
     document.body.appendChild(carList);
@@ -69,28 +82,72 @@ if (!addCarForm) {
 }
 
 const statusField = document.getElementById("status") as HTMLSelectElement;
-statusField.addEventListener('change', function () {
-    const selectedStatus = statusField.value;
-    if (selectedStatus === "discount") {
-        const discountRateHTML = `
-                <label for="discount">Discount Rate</label>
-                <input type="numeric" id="discount" name="discount" />
+statusField.addEventListener("change", function () {
+  const selectedStatus = statusField.value;
+  if (selectedStatus === "discount") {
+    const discountRateHTML = `
+            <label for="discount">Discount Rate (%)</label>
+            <input type="numeric" id="discount" name="discount" pattern="\d+" minlength="1" maxlength="2" />
         `;
-        const discountRate = document.createElement("div");
-        discountRate.className = "form-input discountClass";
-        discountRate.innerHTML = discountRateHTML;
-        statusField.parentElement!.appendChild(discountRate);
-    } else {
-        let discountRate = document.querySelector(".discountClass");
-        discountRate?.remove();
-    }
+    const discountRate = document.createElement("div");
+    discountRate.className = "form-input";
+    discountRate.id = "discountDiv";
+    discountRate.innerHTML = discountRateHTML;
+    const statusDiv = document.querySelector("#statusDiv") as HTMLDivElement;
+    statusDiv.parentElement?.insertBefore(discountRate, statusDiv.nextSibling);
+  } else {
+    let discountRate = document.querySelector("#discountDiv");
+    discountRate?.remove();
+  }
 });
+
+function refreshCarList() {
+    const listHTML = cars.map((car, index) => `
+    <div class="car" data-index="${index}">
+    <p>Registration #: ${car.registrationNumber}</p>
+    <p>Brand: ${car.brand}</p>
+    <p>Type: ${car.type}</p>
+    <p>Color: ${car.color}</p>
+    <p>License Type: ${car.licenseType}</p>
+    <p>Status: <span class="editable" contenteditable="true">${car.status}</span></p>
+    <p>Discount Rate: <span class="listedDiscount">${car.discount}</span></p>
+    </div>
+    <br />
+    `).join("");
+    carList.innerHTML = listHTML;
+}
+
+function validateRate(rate: string) {
+    if (isNaN(Number(rate)) || Number(rate) < 1 || Number(rate) > 99) {
+        throw new Error(`Discount rate must be a number between 1-99!`);
+    }
+
+    return rate;
+}
 
 function getString(formData: FormData, key: string) {
   const value = formData.get(key);
 
   if (value == null) {
     throw new Error(`Field ${key} doesn't exist!`);
+  }
+
+  if (typeof value !== "string") {
+    throw new Error(`Value of field ${key} is not a string!`);
+  }
+
+  if (!value) {
+    return undefined;
+  }
+
+  return value;
+}
+
+function getOptionalString(formData: FormData, key: string) {
+  const value = formData.get(key);
+
+  if (value == null) {
+    return;
   }
 
   if (typeof value !== "string") {
@@ -123,12 +180,7 @@ function parseLicenseType(value: string): LicenseType {
 }
 
 function parseStatus(value: string): status {
-  if (
-    value !== "free" &&
-    value !== "discount" &&
-    value !== "normal" &&
-    value !== "banned"
-  ) {
+  if (value !== "free" && value !== "discount" && value !== "normal" && value !== "banned") {
     throw new Error(`Invalid status: ${value}`);
   }
 
