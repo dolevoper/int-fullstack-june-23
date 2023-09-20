30d34a9fc3965ce8783e5457590eca78978f3906
Assaf Basri <Faskoman90@gmail.com>
2023-09-20 23:44:11 +0300

    made it shorter with toggle forEach

--- student-projects/Assaf_Project/Module-II/class-021-14-09-2023/HP/app.ts
+++ student-projects/Assaf_Project/Module-II/class-021-14-09-2023/HP/app.ts
@@ -1,30 +1,37 @@
 console.clear();
 
-const main = document.querySelector(".main");
-const card = document.querySelector(".card");
-const brand = document.querySelector(".brand");
-const buttons = document.querySelectorAll(".button");
-const inputs = document.querySelectorAll("input");
-const selects = document.querySelectorAll("select");
+// const main = document.querySelector(".main");
+// const card = document.querySelector(".card");
+// const brand = document.querySelector(".brand");
+// const buttons = document.querySelectorAll(".button");
+// const inputs = document.querySelectorAll("input");
+// const selects = document.querySelectorAll("select");
+const elementsToToggleDarkMode = document.querySelectorAll(
+  ".main, .card, .brand, .button, input, select"
+);
 const darkModeButton = document.querySelector(
   "#dark-mode-button"
 ) as HTMLButtonElement;
 darkModeButton.addEventListener("click", darkModeToggle);
 
 function darkModeToggle() {
-    darkModeButton.innerText = darkModeButton.innerText === "DARK MODE" ? "light mode" : "dark mode";
-if (darkModeButton) {
-    main?.classList.toggle("--dark-mode");
-    card?.classList.toggle("--dark-mode");
-    brand?.classList.toggle("--dark-mode");
-    buttons.forEach((element) => {
-      element.classList.toggle("--dark-mode");
-    });
-    inputs.forEach((element) => {
-      element.classList.toggle("--dark-mode");
-    });
-    selects.forEach((element) => {
-      element.classList.toggle("--dark-mode");
-    });
-  }
+  darkModeButton.innerText =
+    darkModeButton.innerText === "DARK MODE" ? "light mode" : "dark mode";
+  // if (darkModeButton) {
+  //     main?.classList.toggle("--dark-mode");
+  //     card?.classList.toggle("--dark-mode");
+  //     brand?.classList.toggle("--dark-mode");
+  //     buttons.forEach((element) => {
+  //       element.classList.toggle("--dark-mode");
+  //     });
+  //     inputs.forEach((element) => {
+  //       element.classList.toggle("--dark-mode");
+  //     });
+  //     selects.forEach((element) => {
+  //       element.classList.toggle("--dark-mode");
+  //     });
+  //   }
+  elementsToToggleDarkMode.forEach((element) => {
+    element.classList.toggle("--dark-mode");
+  });
 }
