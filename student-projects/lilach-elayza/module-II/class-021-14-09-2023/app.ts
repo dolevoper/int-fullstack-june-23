cc6ebbd65b26855dd5318a540d61efffd4c4cdbb
Lilach E <lilachelayza@gmail.com>
2023-09-20 17:25:08 +0300

    class 021 HP unfinished

--- student-projects/lilach-elayza/module-II/class-021-14-09-2023/app.ts
+++ student-projects/lilach-elayza/module-II/class-021-14-09-2023/app.ts
@@ -1,9 +1,3 @@
-// Class project
-// 1. Add new field to car: status. Can be "normal", "free", "discount", "banned". In the form, "free" should be the default.
-// 2. Show the cars list and allow to edit car status
-// 3. Add a new field "discountRate" - when status is "discount" this should have the discount rate
-// (Choose 2 or 3, or do both if you finish the first quickly)
-
 type LicenseType = "A" | "B" | "C";
 type status = "free" | "discount" | "normal" | "banned";
 type Car = {
@@ -40,43 +34,6 @@ if (!addCarForm) {
 
     refreshCarList();
 
-    const editableStatus = document.querySelectorAll(".editable");
-    editableStatus.forEach((status: HTMLElement) => {
-      status.addEventListener("keypress", function (e: KeyboardEvent) {
-        if (e.key === "Enter") {
-          const index = parseInt(status.parentElement!.parentElement!.dataset.index || "");
-
-          if (!isNaN(index)) {
-            const newStatus = status.innerText;
-            cars[index].status = parseStatus(newStatus);
-
-            if (newStatus === "discount") {
-              const discountSpan = status.parentElement!.parentElement!.querySelector(".listedDiscount") as HTMLElement;
-              discountSpan.setAttribute("contenteditable", "true");
-              discountSpan.focus();
-
-              discountSpan.addEventListener("keypress", (e: KeyboardEvent) => {
-                if (e.key === "Enter") {
-                  const newIndex = parseInt(discountSpan.parentElement!.parentElement!.dataset.index || "");
-                  if (!isNaN(newIndex)) {
-                    const newRate = discountSpan.innerText;
-                    cars[newIndex].discount = validateRate(newRate);
-                  }
-                  discountSpan.blur();
-                  refreshCarList();
-                }
-              });
-            } else {
-              const discountSpan = status.parentElement!.parentElement!.querySelector(".listedDiscount") as HTMLElement;
-              discountSpan.removeAttribute("contenteditable");
-              cars[index].discount = undefined;
-              refreshCarList();
-            }
-          }
-          status.blur();
-        }
-      });
-    });
     document.body.appendChild(carList);
   });
 }
@@ -85,14 +42,15 @@ const statusField = document.getElementById("status") as HTMLSelectElement;
 statusField.addEventListener("change", function () {
   const selectedStatus = statusField.value;
   if (selectedStatus === "discount") {
-    const discountRateHTML = `
-            <label for="discount">Discount Rate (%)</label>
-            <input type="numeric" id="discount" name="discount" pattern="\d+" minlength="1" maxlength="2" />
-        `;
     const discountRate = document.createElement("div");
     discountRate.className = "form-input";
     discountRate.id = "discountDiv";
+    const discountRateHTML = `
+    <label for="discount">Discount Rate (%)</label>
+    <input type="numeric" id="discount" name="discount" pattern="\d+" minlength="1" maxlength="2" required />
+    `;
     discountRate.innerHTML = discountRateHTML;
+
     const statusDiv = document.querySelector("#statusDiv") as HTMLDivElement;
     statusDiv.parentElement?.insertBefore(discountRate, statusDiv.nextSibling);
   } else {
@@ -102,7 +60,8 @@ statusField.addEventListener("change", function () {
 });
 
 function refreshCarList() {
-    const listHTML = cars.map((car, index) => `
+  const listHTML = cars
+    .map((car, index) => `
     <div class="car" data-index="${index}">
     <p>Registration #: ${car.registrationNumber}</p>
     <p>Brand: ${car.brand}</p>
@@ -113,16 +72,53 @@ function refreshCarList() {
     <p>Discount Rate: <span class="listedDiscount">${car.discount}</span></p>
     </div>
     <br />
-    `).join("");
-    carList.innerHTML = listHTML;
+    `
+    ).join("");
+  carList.innerHTML = listHTML;
+  applyEditables();
 }
 
-function validateRate(rate: string) {
-    if (isNaN(Number(rate)) || Number(rate) < 1 || Number(rate) > 99) {
-        throw new Error(`Discount rate must be a number between 1-99!`);
-    }
+function applyEditables() {
+  const editableStatus = document.querySelectorAll(".editable");
+  editableStatus.forEach((status: HTMLElement) => {
+    status.addEventListener("keypress", function (e: KeyboardEvent) {
+      if (e.key === "Enter") {
+        const index = parseInt(status.parentElement!.parentElement!.dataset.index || "");
 
-    return rate;
+        if (!isNaN(index)) {
+          const newStatus = status.innerText;
+          cars[index].status = parseStatus(newStatus);
+
+          if (newStatus === "discount") {
+            const discountSpan =
+              status.parentElement!.parentElement!.querySelector(
+                ".listedDiscount"
+              ) as HTMLElement;
+            discountSpan.setAttribute("contenteditable", "true");
+            discountSpan.focus();
+
+            discountSpan.addEventListener("keypress", (e: KeyboardEvent) => {
+              if (e.key === "Enter") {
+                const newIndex = parseInt(discountSpan.parentElement!.parentElement!.dataset.index || "");
+                if (!isNaN(newIndex)) {
+                  const newRate = discountSpan.innerText;
+                  cars[newIndex].discount = newRate;
+                }
+                discountSpan.blur();
+                refreshCarList();
+              }
+            });
+          } else {
+            const discountSpan = status.parentElement!.parentElement!.querySelector(".listedDiscount") as HTMLElement;
+            discountSpan.removeAttribute("contenteditable");
+            cars[index].discount = undefined;
+            refreshCarList();
+          }
+        }
+        status.blur();
+      }
+    });
+  });
 }
 
 function getString(formData: FormData, key: string) {
@@ -180,7 +176,13 @@ function parseLicenseType(value: string): LicenseType {
 }
 
 function parseStatus(value: string): status {
-  if (value !== "free" && value !== "discount" && value !== "normal" && value !== "banned") {
+  if (
+    value !== "free" &&
+    value !== "discount" &&
+    value !== "normal" &&
+    value !== "banned"
+  ) {
+    refreshCarList();
     throw new Error(`Invalid status: ${value}`);
   }
 
