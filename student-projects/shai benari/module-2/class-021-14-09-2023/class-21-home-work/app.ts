c1782129d7cc6f9d433ba92ecf431d491cb0a720
shai-benari <s.benari1962@gmail.com>
2023-09-20 12:33:46 +0300

    without-discount

---
+++ student-projects/shai benari/module-2/class-021-14-09-2023/class-21-home-work/app.ts
@@ -0,0 +1,61 @@
+
+document.body.style.background = "#f3f3f3 url('flwer.jpg') no-repeat center"
+const price = {economic:90 ,standard:140 , luxurie: 180 }
+
+type evantType = "Birthday" | "Wedding" | "Bar mitzva" | "Funetral" | "Other";
+type bouquetType = "Economic" | "Standard" | "Luxurie" ;
+
+type flwer = {
+    name: string
+    address: string
+    evant: evantType;
+    bouquetType: bouquetType;
+    priceTag: string;
+};
+
+type flwerArry = flwer[];
+const flwers: flwerArry =[];
+
+
+
+const addFlwerForm = document.querySelector("form[name='send-flowers']") as HTMLFormElement | null;
+if (!addFlwerForm){
+    console.error("Couldn't find send flwer form.");
+} 
+else{
+    addFlwerForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target as HTMLFormElement);
+
+    flwers.push({
+    name: getRequiredString(formData, "name"),
+    address: getRequiredString(formData, "address"),
+    Evant:  parseEvent(getRequiredString(formData, "evant")),
+    bouquetType:parseBouquet(getRequiredString(formData, "bouquetType")),
+    priceTag: getRequiredString(formData, "priceTag"),
+});
+    console.log(flwers);
+});
+}
+function getString(formData:FormData , key: string ){
+    const value = formData.get(key);
+   
+
+    return value;
+};
+
+function getRequiredString(formData:FormData , key: string ){
+    const value = getString(formData, key);
+    
+
+    return value;
+};
+
+function parseEvent(value: evantType): evantType{
+   
+    return value;
+} 
+function parseBouquet(value: bouquetType): bouquetType{
+    return value;
+} 
