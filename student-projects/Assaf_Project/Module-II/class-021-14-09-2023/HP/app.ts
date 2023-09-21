58876e1a53ab4fa30121cff123b2f7faefeffb32
Assaf Basri <Faskoman90@gmail.com>
2023-09-21 14:06:39 +0300

    localStorage.setItem

--- student-projects/Assaf_Project/Module-II/class-021-14-09-2023/HP/app.ts
+++ student-projects/Assaf_Project/Module-II/class-021-14-09-2023/HP/app.ts
@@ -1,9 +1,24 @@
 console.clear();
 
+let lightOrDarkMode = JSON.parse(localStorage.getItem("lightOrDarkMode")!) || "light";
+
 const elementsToToggleDarkMode = document.querySelectorAll(".main, .card, .brand, .button, input, select");
 const darkModeButton = document.querySelector("#dark-mode-button") as HTMLButtonElement;
 
 darkModeButton.addEventListener("click", () => { 
-  darkModeButton.innerText = darkModeButton.innerText === "DARK MODE" ? "light mode" : "dark mode";
+  lightOrDarkMode = lightOrDarkMode === "dark" ? "light" : "dark";
+  darkModeButton.innerText = lightOrDarkMode === "dark" ? "dark mode" : "light mode";
   elementsToToggleDarkMode.forEach((element) => { element.classList.toggle("--dark-mode")});
+  saveLightOrDarkToLocalStorage();
 });
+
+function saveLightOrDarkToLocalStorage() {
+  localStorage.setItem("lightOrDarkMode", JSON.stringify(lightOrDarkMode));
+}
+
+if (lightOrDarkMode === "dark") {
+  darkModeButton.innerText = "dark mode";
+  elementsToToggleDarkMode.forEach((element) => { element.classList.add("--dark-mode") });
+} else {
+  saveLightOrDarkToLocalStorage();
+}
