5dd2b6c0d9d2768f0cb63c590cbc6955aaded18f
shai-benari <s.benari1962@gmail.com>
2023-10-03 10:28:13 +0300

    sqwer-line

--- student-projects/shai benari/module-2/class-024-28-09-2023/canvas-exer/canvas.ts
+++ student-projects/shai benari/module-2/class-024-28-09-2023/canvas-exer/canvas.ts
@@ -1,4 +1,7 @@
 const canvas = document.getElementById("exer-canvas");
 const sqwer = canvas.getContext("2d");
 sqwer.fillStyle = "red";
-sqwer.fillRect(0, 0, 150, 75);
+sqwer.fillRect(50, 50, 150, 75);
+sqwer.moveTo(50,50);
+sqwer.lineTo(200,125);
+sqwer.stroke();
