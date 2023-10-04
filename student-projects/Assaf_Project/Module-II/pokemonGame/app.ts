7298984c05ebf90ab498b929d797f60a8b395eca
Assaf Basri <Faskoman90@gmail.com>
2023-10-04 12:01:10 +0300

    added boundaries

--- student-projects/Assaf_Project/Module-II/pokemonGame/app.ts
+++ student-projects/Assaf_Project/Module-II/pokemonGame/app.ts
@@ -1,12 +1,55 @@
 const canvas = document.querySelector("canvas");
-
 const c = canvas?.getContext("2d");
 
 canvas!.width = 1024;
 canvas!.height = 576;
 
-c!.fillStyle = "white";
-c?.fillRect(0, 0, canvas!.width, canvas!.height);
+const collisionsMap = [];
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, i + 70));
+}
+
+class Boundary {
+  static width = 48;
+  static height = 48;
+  constructor({ position }) {
+    this.position = position;
+    this.width = 48;
+    this.height = 48;
+  }
+
+  draw() {
+    c!.fillStyle = "red";
+    c!.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+const boundaries = [];
+const offset = {
+  x: -1900,
+  y: -975,
+};
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+  });
+});
+
+console.log(boundaries);
 
 const image = new Image();
 image.src = "./assets/zoomedmap.png";
@@ -15,7 +58,7 @@ const playerImage = new Image();
 playerImage.src = "./assets/playerDown.png";
 
 class Sprite {
-  constructor({ position, velocity, image }) {
+  constructor({ position, image }) {
     this.position = position;
     this.image = image;
   }
@@ -27,30 +70,33 @@ class Sprite {
 
 const background = new Sprite({
   position: {
-    x: -1900,
-    y: -975,
+    x: offset.x,
+    y: offset.y,
   },
-  image: image
+  image: image,
 });
 
 const keys = {
   w: {
-    pressed: false
+    pressed: false,
   },
   a: {
-    pressed: false
+    pressed: false,
   },
   s: {
-    pressed: false
+    pressed: false,
   },
   d: {
-    pressed: false
+    pressed: false,
   },
-}
+};
 
 function animate() {
   window.requestAnimationFrame(animate);
   background.draw();
+  boundaries.forEach((boundary) => {
+    boundary.draw();
+  });
   c?.drawImage(
     playerImage,
     0,
@@ -61,16 +107,20 @@ function animate() {
     canvas!.height / 2 - playerImage.height / 2,
     playerImage.width / 4,
     playerImage.height
-  )
+  );
 
-  if (keys.w.pressed && lastKey === "w") background.position.y = background.position.y += 1;
-  else if (keys.a.pressed && lastKey === "a") background.position.x = background.position.x += 1;
-  else if (keys.s.pressed && lastKey === "s") background.position.y = background.position.y -= 1;
-  else if (keys.d.pressed && lastKey === "d") background.position.x = background.position.x -= 1;
+  if (keys.w.pressed && lastKey === "w")
+    background.position.y = background.position.y += 1;
+  else if (keys.a.pressed && lastKey === "a")
+    background.position.x = background.position.x += 1;
+  else if (keys.s.pressed && lastKey === "s")
+    background.position.y = background.position.y -= 1;
+  else if (keys.d.pressed && lastKey === "d")
+    background.position.x = background.position.x -= 1;
 }
 animate();
 
-let lastKey= "";
+let lastKey = "";
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "w":
@@ -94,7 +144,6 @@ window.addEventListener("keydown", (e) => {
       lastKey = "d";
       break;
   }
-  console.log(keys)
 });
 
 window.addEventListener("keyup", (e) => {
@@ -116,5 +165,4 @@ window.addEventListener("keyup", (e) => {
       keys.d.pressed = false;
       break;
   }
-  console.log(keys)
 });
