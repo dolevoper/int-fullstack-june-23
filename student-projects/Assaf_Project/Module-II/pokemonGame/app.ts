6dcb173a54ccc5eb4afcae2820c391a75a2cf144
Assaf Basri <Faskoman90@gmail.com>
2023-10-04 13:03:17 +0300

    collision detection

--- student-projects/Assaf_Project/Module-II/pokemonGame/app.ts
+++ student-projects/Assaf_Project/Module-II/pokemonGame/app.ts
@@ -20,16 +20,11 @@ class Boundary {
 
   draw() {
     c!.fillStyle = "red";
-    c!.fillRect(
-      this.position.x,
-      this.position.y,
-      this.width,
-      this.height
-    );
+    c!.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 }
 
-const boundaries = [];
+const boundaries: Boundary[] = [];
 const offset = {
   x: -1900,
   y: -975,
@@ -58,15 +53,46 @@ const playerImage = new Image();
 playerImage.src = "./assets/playerDown.png";
 
 class Sprite {
-  constructor({ position, image }) {
+  constructor({ position, image, frames = { max: 1 } }) {
     this.position = position;
     this.image = image;
+    this.frames = frames;
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max;
+      this.height = this.image.height;
+      console.log(this.width);
+      console.log(this.height);
+    };
   }
 
   draw() {
-    c?.drawImage(this.image, this.position.x, this.position.y);
+    c?.drawImage(
+      this.image,
+      0,
+      0,
+      this.image.width / this.frames.max,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      this.image.width / this.frames.max,
+      this.image.height
+    );
   }
 }
+
+// canvas!.width / 2 + this.image.width / 16,
+//       canvas!.height / 2 - this.image.height / 2,
+
+const player = new Sprite({
+  position: {
+    x: canvas!.width / 2 + 192 / 16,
+    y: canvas!.height / 2 - 68 / 2,
+  },
+  image: playerImage,
+  frames: {
+    max: 4,
+  },
+});
 
 const background = new Sprite({
   position: {
@@ -91,32 +117,48 @@ const keys = {
   },
 };
 
+const testBoundary = new Boundary({
+  position: {
+    x: 400,
+    y: 300,
+  },
+});
+
+const movables = [background, testBoundary];
 function animate() {
   window.requestAnimationFrame(animate);
   background.draw();
-  boundaries.forEach((boundary) => {
-    boundary.draw();
-  });
-  c?.drawImage(
-    playerImage,
-    0,
-    0,
-    playerImage.width / 4,
-    playerImage.height,
-    canvas!.width / 2 + playerImage.width / 16,
-    canvas!.height / 2 - playerImage.height / 2,
-    playerImage.width / 4,
-    playerImage.height
-  );
+  // boundaries.forEach((boundary) => {
+  //   boundary.draw();
+  // });
+  testBoundary.draw();
+  player.draw();
+  if (
+    player.position.x + player.width >= testBoundary.position.x &&
+    player.position.x <= testBoundary.position.x + testBoundary.width &&
+    player.position.y + player.height >= testBoundary.position.y + 5 &&
+    player.position.y <= testBoundary.position.y + testBoundary.height / 3
+  ) {
+    console.log("collide");
+  }
 
-  if (keys.w.pressed && lastKey === "w")
-    background.position.y = background.position.y += 1;
-  else if (keys.a.pressed && lastKey === "a")
-    background.position.x = background.position.x += 1;
-  else if (keys.s.pressed && lastKey === "s")
-    background.position.y = background.position.y -= 1;
-  else if (keys.d.pressed && lastKey === "d")
-    background.position.x = background.position.x -= 1;
+  if (keys.w.pressed && lastKey === "w") {
+    movables.forEach((movable) => {
+      movable.position.y += 1;
+    });
+  } else if (keys.a.pressed && lastKey === "a") {
+    movables.forEach((movable) => {
+      movable.position.x += 1;
+    });
+  } else if (keys.s.pressed && lastKey === "s") {
+    movables.forEach((movable) => {
+      movable.position.y -= 1;
+    });
+  } else if (keys.d.pressed && lastKey === "d") {
+    movables.forEach((movable) => {
+      movable.position.x -= 1;
+    });
+  }
 }
 animate();
 
