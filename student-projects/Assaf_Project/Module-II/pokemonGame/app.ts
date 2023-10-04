f65b8e446c3c2f35db59217a9a5849d9e3421fd0
Assaf Basri <Faskoman90@gmail.com>
2023-10-04 10:12:05 +0300

    added cropped player on starting pos

--- student-projects/Assaf_Project/Module-II/pokemonGame/app.ts
+++ student-projects/Assaf_Project/Module-II/pokemonGame/app.ts
@@ -11,6 +11,20 @@ c?.fillRect(0, 0, canvas!.width, canvas!.height);
 const backgroundImage = new Image();
 backgroundImage.src = "./assets/zoomedmap.png";
 
+const playerImage = new Image();
+playerImage.src = "./assets/playerDown.png";
+
 backgroundImage.onload = () => {
-  c?.drawImage(backgroundImage, -1950, -1000);
+  c?.drawImage(backgroundImage, -1900, -975);
+  c?.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas!.width / 2 + playerImage.width / 16,
+    canvas!.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  );
 };
