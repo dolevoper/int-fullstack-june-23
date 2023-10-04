395e9b2d319cc1276116063bda84798db0cab829
BraveVlad <giladvladpinker@gmail.com>
2023-10-04 04:11:28 +0300

    SmartForm custom onSubmit

--- student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-23/homeproject/boxout-app/SmartForm.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-23/homeproject/boxout-app/SmartForm.ts
@@ -4,48 +4,42 @@ type InputType = string | number | boolean;
 
 export class SmartForm {
 
-    private inputs!: ValidatableInput<InputType>[];
+    inputs!: ValidatableInput<InputType>[];
 
-    constructor(private form: HTMLFormElement) {
-
-
+    constructor(public form: HTMLFormElement) {
         this.initInputs();
-        this.initOnSubmit();
     }
 
     initInputs() {
         this.inputs = [];
         const inputElements = this.form.querySelectorAll("input")
 
-        inputElements.forEach( inputElement => {
-            console.log(inputElement);
+        inputElements.forEach(inputElement => {
             const smartInput = createSmartInput(inputElement);
             this.inputs.push(smartInput)
         });
 
     }
-    initOnSubmit() {
-        this.form.addEventListener("submit", (submitEvent) => {
 
-            submitEvent.preventDefault();
-
-            const formData = new FormData(this.form);
-
-            this.inputs.forEach(input => {
-                input.validate()
-            })
-
-            console.log(formData);
-        })
-
+    public onSubmit(submitEvent: EventListenerOrEventListenerObject) {
+        this.form.addEventListener("submit", submitEvent)
     }
 
     getInput(name: string): ValidatableInput<InputType> | undefined {
-        const foundInput: ValidatableInput<InputType> | undefined =  this.inputs.find((input) => {
+        const foundInput: ValidatableInput<InputType> | undefined = this.inputs.find((input) => {
             return input.getName() === name;
         });
 
         return foundInput;
+    }
+
+    validate(): boolean {
+        let allInputsPassed = true;
+        this.inputs.forEach(input => {
+            const validationResult = input.validate();
+            if (!validationResult) allInputsPassed = false;
+        })
+        return allInputsPassed;
     }
 }
 
@@ -61,22 +55,27 @@ export class ValidatableInput<T> {
     }
 
     addValidator(validator: Validator<T>, value?: T) {
-        if(value) 
+        if (value)
             validator.setValue(value);
 
         this.validators.push(validator);
     }
 
-    validate() {
-        const currentValue = this.element.value;
-        console.log(currentValue)
-
+    validate(): boolean {
+        const currentValue = this.element.value as T;
+        const isAllValidationsPassed = this.activateValidators(currentValue);
+        return isAllValidationsPassed;
     }
 
-    protected activateValidators(currentValue: T) {
+    protected activateValidators(currentValue: T): boolean {
+
+        let isAllValidationsPased = true;
         this.validators.forEach((validator) => {
-            validator.validate(this, currentValue);
+            const validationResult = validator.validate(this, currentValue);
+            if (!validationResult) isAllValidationsPased = false;
         })
+
+        return isAllValidationsPased;
     }
 
     getValue(): T {
@@ -94,9 +93,10 @@ class BooleanInput extends ValidatableInput<boolean> {
         super(element);
     }
 
-    validate(): void {
+    validate(): boolean {
         const currentValue = this.element.checked;
-        this.activateValidators(currentValue);
+        const isAllValidationsPassed = this.activateValidators(currentValue);
+        return isAllValidationsPassed;
     }
 
     getValue(): boolean {
@@ -110,9 +110,10 @@ class NumberInput extends ValidatableInput<number> {
         super(element);
     }
 
-    validate(): void {
+    validate(): boolean {
         const currentValue = this.element.valueAsNumber;
-        this.activateValidators(currentValue);
+        const isAllValidationsPassed = this.activateValidators(currentValue);
+        return isAllValidationsPassed;
 
     }
 
@@ -124,10 +125,10 @@ class TextInput extends ValidatableInput<string> {
         super(element);
     }
 
-    validate(): void {
+    validate(): boolean {
         const currentValue = this.element.value;
-        this.activateValidators(currentValue);
-
+        const isAllValidationsPassed = this.activateValidators(currentValue);
+        return isAllValidationsPassed;
     }
 
 }
@@ -135,11 +136,11 @@ class TextInput extends ValidatableInput<string> {
 function createSmartInput(element: HTMLInputElement): ValidatableInput<InputType> {
     const type = element.type;
 
-    if(type === "number" || type === "range")
+    if (type === "number" || type === "range")
         return new NumberInput(element);
-    if(type === "checkbox" || type === "radio")
+    if (type === "checkbox" || type === "radio")
         return new BooleanInput(element);
-    
+
     return new TextInput(element);
-    
+
 }
