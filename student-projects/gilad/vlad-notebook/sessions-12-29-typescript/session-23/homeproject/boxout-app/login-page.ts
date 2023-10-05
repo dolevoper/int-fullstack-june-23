1679cd16ced1bfab91169ea3746908b4fbab5095
BraveVlad <giladvladpinker@gmail.com>
2023-10-05 08:00:34 +0300

    Login Page current user, smart form validation and login

--- student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-23/homeproject/boxout-app/login-page.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-23/homeproject/boxout-app/login-page.ts
@@ -1,87 +1,78 @@
-import * as User from "./User.js";
-import { Validator, hasSpecialCharacters, isValidEmail, mustBeChecked, mustBeLongerThan, mustContain } from "./Validator.js";
 import { SmartForm, ValidatableInput } from "./SmartForm.js"
+import { InvalidPassword, UserNotFound, UsersManager } from "./UsersManager.js"
+import * as LoginValidators from "./LoginValidators.js"
+import { Admin, Client } from "./User.js"
+
+const usersManager = new UsersManager()
 
 function skipLoginIfAlreadySignedIn() {
-
-    const loadedUser = User.loadCurrentUser();
-
-    if (!loadedUser)
+    if (!usersManager.currentUser)
         return;
-
-    if (loadedUser instanceof User.Admin)
-        openAdminDashboard(loadedUser)
-    if (loadedUser instanceof User.Client)
-        openClientDashboard(loadedUser)
+    if (usersManager.isCurrentUserAdmin())
+        openAdminDashboard();
+    if (usersManager.isCurrentUserClient())
+        openClientDashboard();
 }
 
 function initLoginForm() {
 
-    const loginForm = document.forms.namedItem("login-form") as HTMLFormElement;
-    const smartForm = new SmartForm(loginForm);
+    const smartForm = new SmartForm("login-form");
 
     const username = smartForm.getInput("login-username") as ValidatableInput<string>;
-    username.addValidator(lengthValidation);
-    username.addValidator(mustBeValidEmail)
+    username.addValidator(LoginValidators.lengthValidation);
+    username.addValidator(LoginValidators.mustBeValidEmail)
 
     const password = smartForm.getInput("login-password") as ValidatableInput<string>;
-    password.addValidator(specialCharValidation);
-    password.addValidator(lengthValidation);
-    password.addValidator(mustContainVlad);
+    password.addValidator(LoginValidators.specialCharValidation);
+    password.addValidator(LoginValidators.lengthValidation);
+    password.addValidator(LoginValidators.mustContainVlad);
 
     const remember = smartForm.getInput("login-remember") as ValidatableInput<boolean>;
-    remember.addValidator(checkedValidation)
+    // remember.addValidator(LoginValidators.checkedValidation)
 
     smartForm.onSubmit((event) => {
         event.preventDefault();
 
-        if (smartForm.validate())
-            console.log("form passed validations!")
+        if (smartForm.validate()) {
+            const loginResult = usersManager.login(username.getValue(), password.getValue());
+
+            if (loginResult instanceof UserNotFound) {
+                console.log("user not found!")
+                return;
+            }
+            if (loginResult instanceof InvalidPassword) {
+                console.log("invalid password")
+                return;
+            }
+            if (remember.getValue())
+                usersManager.saveCurrentUser(loginResult);
+
+            if (UsersManager.isAdmin(loginResult)) {
+                openAdminDashboard();
+            }
+            else if (UsersManager.isClient(loginResult)) {
+                openClientDashboard();
+            }
+
+        }
     });
 }
 
-function openAdminDashboard(user: User.Admin) {
+function openAdminDashboard() {
     console.log("Opening admin dashboard")
 }
 
-function openClientDashboard(user: User.Client) {
+function openClientDashboard() {
     console.log("Opening client dashboard")
-
 }
 
 function main() {
-    console.log("login page");
     skipLoginIfAlreadySignedIn();
-
     initLoginForm();
 }
 
-
-const mustBeValidEmail = new Validator(isValidEmail);
-mustBeValidEmail.onFailed = (input) => {
-    console.log(`FAILED - input ${input.getName()} is not a valid email.`)
-};
-
-const lengthValidation = new Validator(mustBeLongerThan, 6);
-lengthValidation.onFailed = (input, target, tested) => {
-    console.log(`FAILED - input ${input.getName()} is shorter than. ${tested}`)
-}
-
-const specialCharValidation = new Validator(hasSpecialCharacters);
-specialCharValidation.onFailed = (input) => {
-    console.log(`FAILED - input ${input.getName()} has special characters!`)
-}
-
-
-const mustContainVlad = new Validator(mustContain, "Vlad");
-mustContainVlad.onFailed = (a, b, c) => {
-    console.log(`FAILED - input ${a.getName()} does not contain ${c}`)
-}
-
-const checkedValidation = new Validator(mustBeChecked);
-checkedValidation.onFailed = (input) => {
-    console.log(input.getName() + " must be checked")
-}
-
+usersManager.registerUser(Admin, "Admin@gmail.com", "1234Vlad")
+usersManager.registerUser(Client, "Client@gmail.com", "Vlad1234")
+usersManager.registerUser(Client, "Client2@gmail.com", "Vlad12345")
 
 main();
