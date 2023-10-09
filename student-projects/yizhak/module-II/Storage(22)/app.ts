1d2a8d4b22aed6aa05fcb8e83f5449813c35bece
yizhak148 <yizhak208@gmail.com>
2023-10-10 02:00:53 +0300

    c

--- student-projects/yizhak/module-II/Storage(22)/app.ts
+++ student-projects/yizhak/module-II/Storage(22)/app.ts
@@ -12,8 +12,14 @@ body.addEventListener("click", (e) => {
 const numberTostr = JSON.stringify(Count);
   localStorage.setItem("click", numberTostr);
 
-body.style.backgroundColor = "blue";
+body.style.backgroundColor = "white";
 
 //לבדןק למה זה לא עובד!
 
+const form = document.getElementById("form[login]") as HTMLFormElement;
+const username = document.getElementById("username") as HTMLFormElement;
+const password = document.getElementById("password") as HTMLFormElement;
 
+
+localStorage.setItem("username", username);
+localStorage.setItem("password", userpassword);
