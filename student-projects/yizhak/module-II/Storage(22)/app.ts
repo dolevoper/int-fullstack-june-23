92e35b1ffb117c1a15a9839162a57a7204bea098
yizhak148 <yizhak208@gmail.com>
2023-10-11 02:48:52 +0300

    yessss

--- student-projects/yizhak/module-II/Storage(22)/app.ts
+++ student-projects/yizhak/module-II/Storage(22)/app.ts
@@ -1,25 +1,32 @@
 const body = document.querySelector("body") as HTMLBodyElement;
 
-
 let Count = 0;
 
 body.addEventListener("click", (e) => {
-  
   Count++;
-  
 });
 
 const numberTostr = JSON.stringify(Count);
-  localStorage.setItem("click", numberTostr);
+localStorage.setItem("click", numberTostr);
 
 body.style.backgroundColor = "white";
 
 //לבדןק למה זה לא עובד!
 
-const form = document.getElementById("form[login]") as HTMLFormElement;
-const username = document.getElementById("username") as HTMLFormElement;
-const password = document.getElementById("password") as HTMLFormElement;
+const form = document.getElementById("form[login]")! as HTMLFormElement;
+const username = document.getElementById("username") as HTMLInputElement;
+const userpassword = document.getElementById("password") as HTMLInputElement;
 
+form.addEventListener("submit", (e) => {
+  localStorage.setItem("user name", username.value);
+  localStorage.setItem("user password", userpassword.value);
+  e.preventDefault();
+});
 
-localStorage.setItem("username", username);
-localStorage.setItem("password", userpassword);
+const resetbtn = document.getElementById("reset")!;
+
+resetbtn.addEventListener("click", (e) => {
+  username.textContent = ""
+  userpassword.textContent = ""
+});
+//עובדדדדד יש ברוך ה
