cd7e39a3edaab95dfc96277f18e9a53e110917c3
yizhak148 <yizhak208@gmail.com>
2023-10-22 00:04:42 +0300

    i fix it

--- student-projects/yizhak/module-II/Storage(22)/app.ts
+++ student-projects/yizhak/module-II/Storage(22)/app.ts
@@ -11,22 +11,43 @@ localStorage.setItem("click", numberTostr); */
 
 //לבדןק למה זה לא עובד!
 
-const form = document.getElementById("form[login]")! as HTMLFormElement;
+const form = document.querySelector("form[name='login']") as HTMLFormElement;
 const username = document.getElementById("username") as HTMLInputElement;
 const userpassword = document.getElementById("password") as HTMLInputElement;
+const resetbtn = document.getElementById("reset") as HTMLButtonElement;
+const send = document.getElementById("btnsend") as HTMLButtonElement;// לבדוק למה זה null
 
+
+type user = [
+  username: string,
+  userpassword: string
+]
+
+const userarr:user[] = []
+
+if (!form) {
+  console.error("Couldn't find the form.");
+} else {
 form.addEventListener("submit", (e) => {
+  e.preventDefault();
   localStorage.setItem("user name", username.value);
   localStorage.setItem("user password", userpassword.value);
-  e.preventDefault();
+  //userarr.push(username.value , userpassword.value)
+  console.log(userarr)
+  window.location.href = "page.html";
 });
+}
 
-const resetbtn = document.getElementById("reset")!;
 
 resetbtn.addEventListener("click", (e) => {
-  username.textContent = "";
-  userpassword.textContent = "";
+  username.value = "";
+  userpassword.value = "";
 });
+
+
+
+
+
 //עובדדדדד יש ברוך ה
 
 const darkmode = document.getElementById("color")!;
@@ -35,9 +56,9 @@ darkmode.addEventListener("click", (e) => {
   document.body.classList.toggle("darkmode");
 
   if (document.body.classList.contains("darkmode")) {
-    darkmode.textContent = "ligat-mode";
-  } else {
     darkmode.textContent = "light-mode";
+  } else {
+    darkmode.textContent = "dark-mode";
   }
 });
 //גם המצב לילה עובד ברוך ה
