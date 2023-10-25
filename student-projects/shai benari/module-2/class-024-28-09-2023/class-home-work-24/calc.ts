f2084ad3f1d8e29f5760ba2a2f6d6c41ad161b2f
shai-benari <s.benari1962@gmail.com>
2023-10-25 23:08:58 +0300

    js begin

--- student-projects/shai benari/module-2/class-024-28-09-2023/class-home-work-24/calc.ts
+++ student-projects/shai benari/module-2/class-024-28-09-2023/class-home-work-24/calc.ts
@@ -1,3 +1,45 @@
+class calculator{
+    constructor(topScrenTextElement, bottomScrenTextElement){
+        this.topScrenTextElement = topScrenTextElement
+        this.bottomScrenTextElement = bottomScrenTextElement
+        this.clear();
+    }
+}
+
+clear(){
+this.topScren ='';
+this.bottomScren = '';
+this.operation = undefined;
+}
+delet(){
+
+}
+appendNumber(number){
+this.bottomScren = number;
+}
+chooseOperation(operation){
+
+}
+compute(){
+
+}
+updateDisplay(){
+    this.bottomScrenTextElement.innerText = this.bottomScren;
+}
+
+
 const numberButton = document.querySelectorAll(`[data-number]`);
 const operationButton = document.querySelectorAll(`[data-operation]`);
 const equalButton = document.querySelector(`[data-equaln]`);
+const deletlButton = document.querySelector(`[data-delete]`);
+const allClearButton = document.querySelector(`[data-all-clear]`);
+const topScrenTextElement = document.querySelector(`[data-top-scren]`) as HTMLDivElement;
+const bottomScrenTextElement = document.querySelector(`[ data-bottom-scren]`) as HTMLDivElement;
+
+const calculator = new calculator(topScrenTextElement, bottomScrenTextElement);
+numberButton.forEach(button => {
+    button.addEventListener(`click`,() =>){
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    }
+});
