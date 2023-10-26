9d07dafc0b70399111c2afadad754cc201209897
shai-benari <s.benari1962@gmail.com>
2023-10-27 00:34:28 +0300

    near end project

--- student-projects/shai benari/module-2/class-024-28-09-2023/class-home-work-24/calc1.ts
+++ student-projects/shai benari/module-2/class-024-28-09-2023/class-home-work-24/calc1.ts
@@ -2,8 +2,9 @@ const topScren = document.querySelector('.top-scren');
 const bottomScren = document.querySelector('.bottom-scren');
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtens =  document.querySelectorAll('[data-operation]');
-const clearButton = document.querySelector('[ data-all-clear]');
-const deletButton = document.querySelector('[ data-delete]');
+const clearButton = document.querySelector('[data-all-clear]');
+const deletButton = document.querySelector('[data-delete]');
+const equalButton = document.querySelector('[data-equal]');
 
 let calculation = [];
 let acumeletCalculation
@@ -14,21 +15,28 @@ function calculate(button){
     acumeletCalculation = calculation.join('');
     topScren.innerText = acumeletCalculation;
 
-    // console.log(acumeletCalculation);
+    console.log(acumeletCalculation);
 }
 function clearAll(){
     acumeletCalculation = [''];
     topScren.innerText = acumeletCalculation;
     calculation = [];
+    bottomScren.innerText = '';
 }
 function deletOne(){
     acumeletCalculation = acumeletCalculation.slice(0,-1);
     topScren.innerText = acumeletCalculation;
     calculation = [];
+    
+}
+function compute(){
+    bottomScren.innerText = eval(acumeletCalculation);
 }
 
 numberButtons.forEach( button => button.addEventListener('click',() => calculate(button)));
+operationButtens.forEach( button => button.addEventListener('click',() => calculate(button)));
 clearButton.addEventListener('click',clearAll);
 deletButton.addEventListener('click',deletOne);
+equalButton.addEventListener('click',compute);
 
 
