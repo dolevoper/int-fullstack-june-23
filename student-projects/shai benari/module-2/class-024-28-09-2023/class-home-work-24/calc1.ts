25cd9495691c2738173970574254c9c7b93b73c3
shai-benari <s.benari1962@gmail.com>
2023-10-26 23:28:13 +0300

    del and clear

---
+++ student-projects/shai benari/module-2/class-024-28-09-2023/class-home-work-24/calc1.ts
@@ -0,0 +1,34 @@
+const topScren = document.querySelector('.top-scren');
+const bottomScren = document.querySelector('.bottom-scren');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtens =  document.querySelectorAll('[data-operation]');
+const clearButton = document.querySelector('[ data-all-clear]');
+const deletButton = document.querySelector('[ data-delete]');
+
+let calculation = [];
+let acumeletCalculation
+
+function calculate(button){
+    const value = button.innerText;
+    calculation.push(value);
+    acumeletCalculation = calculation.join('');
+    topScren.innerText = acumeletCalculation;
+
+    // console.log(acumeletCalculation);
+}
+function clearAll(){
+    acumeletCalculation = [''];
+    topScren.innerText = acumeletCalculation;
+    calculation = [];
+}
+function deletOne(){
+    acumeletCalculation = acumeletCalculation.slice(0,-1);
+    topScren.innerText = acumeletCalculation;
+    calculation = [];
+}
+
+numberButtons.forEach( button => button.addEventListener('click',() => calculate(button)));
+clearButton.addEventListener('click',clearAll);
+deletButton.addEventListener('click',deletOne);
+
+
