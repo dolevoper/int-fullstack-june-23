ce3e3776270c4409501810c0db09cf2f0d5dfca3
shai-benari <s.benari1962@gmail.com>
2023-11-09 15:44:23 +0200

    count first stage

--- student-projects/shai benari/module-2/class-025-06-11-2023/count.ts
+++ student-projects/shai benari/module-2/class-025-06-11-2023/count.ts
@@ -1,5 +1,5 @@
 const btnIncrease = document.getElementById('btn-increase')?.addEventListener('click', function(){
-const valueElement = document.getElementsByClassName('value');
+const valueElement = document.getElementById("value");
 if(!valueElement){
     return
 }
@@ -8,5 +8,24 @@ const curantValue = Number(valueElement.textContent);
 valueElement.textContent = `${curantValue + 1}`
 
 })
+const btndecrese = document.getElementById('btn-decrease')?.addEventListener('click', function(){
+    const valueElement = document.getElementById("value");
+    if(!valueElement){
+        return
+    }
+    const curantValue = Number(valueElement.textContent);
     
-
+    valueElement.textContent = `${curantValue - 1}`
+    
+    })
+          
+    const btndubel = document.getElementById('btn-dublicate')?.addEventListener('click', function(){
+        const valueElement = document.getElementById("value");
+        if(!valueElement){
+            return
+        }
+        const curantValue = Number(valueElement.textContent);
+        
+        valueElement.textContent = `${curantValue * 2}`
+        
+        })
