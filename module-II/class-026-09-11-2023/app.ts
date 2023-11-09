e88e59c42d82838985063ea0adde151fb3198b42
Omer Dolev <omerdolev90@gmail.com>
2023-11-09 19:54:53 +0200

    module

--- module-II/class-026-09-11-2023/app.ts
+++ module-II/class-026-09-11-2023/app.ts
@@ -1,19 +1,39 @@
+// [V] Implement coffee machine
+// [ ] Deliver orders
+//      - Color available orders green
+//      - Click on an available order to close it and remove items
+// [ ] Recieve score on delivered orders
 // [ ] Incoming orders
 // [ ] Time orders
 // [ ] Improve score acording to order supply time
 // [ ] Add items and order types
 // [ ] Resource management
-// [V] Implement coffee machine
+// [ ] Compound orders (2 double espresso)
 
-type Item =
-    | "espresso"
-    | "double espresso";
+import { Items, fromArray, empty, addItem, everyItem, hasItem, getItemCount } from "./items.js";
 
-const orders: Item[] = ["espresso", "double espresso"];
-const items: Item[] = [];
+type Order = {
+    name: string,
+    items: Items
+};
+
+const orders: Order[] = [
+    { name: "Espresso", items: fromArray(["espresso"]) },
+    { name: "Double espresso", items: fromArray(["espresso", "espresso"]) }
+];
+
+const items: Items = empty();
 
 function useCoffeeMachine() {
-    items.push("espresso");
+    addItem(items, "espresso");
+
+    // for (let i = 0; i < orders.length; i++) {
+    //      const order = orders[i];
+    for (const order of orders) {
+        if (orderIsAvailable(order)) {
+            markOrderAvailable(order);
+        }
+    }
 
     const itemsList = document.getElementById("items-list");
 
@@ -27,4 +47,17 @@ function useCoffeeMachine() {
     itemsList.append(li);
 }
 
-document.getElementById("coffee-machine")?.addEventListener("click", useCoffeeMachine);
+document
+    .getElementById("coffee-machine")
+    ?.addEventListener("click", useCoffeeMachine);
+
+function orderIsAvailable(order: Order) {
+    // TODO: run dynamically for every item type
+    return getItemCount(items, "espresso") === getItemCount(order.items, "espresso") &&
+        getItemCount(items, "milk") === getItemCount(order.items, "milk");
+}
+
+function markOrderAvailable(order: Order) {
+    console.log("marking available");
+}
+
