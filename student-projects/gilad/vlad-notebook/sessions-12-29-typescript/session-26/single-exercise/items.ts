28542168b58496923e49b2272539cb712588dbbc
BraveVlad <giladvladpinker@gmail.com>
2023-11-09 20:19:43 +0200

    fixed empty, fromArray, addItem

--- student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-26/single-exercise/items.ts
+++ student-projects/gilad/vlad-notebook/sessions-12-29-typescript/session-26/single-exercise/items.ts
@@ -2,29 +2,36 @@ export type Item =
     | "espresso"
     | "milk";
 
-export type Items = Item[];
-// export type Items = Record<Item, number>;
+// export type Items = Item[];
+export type Items = Record<Item, number>;
 
 export function empty(): Items {
-    return [];
+    return {
+        "espresso": 0,
+        "milk": 0,
+    };
 }
 
 export function fromArray(items: Item[]): Items {
-    return items;
+    const itemsRecord: Items = {
+        "espresso": 0,
+        "milk": 0
+    };
+    items.forEach(item => {
+        itemsRecord[item] = getItemCountInArray(items, item);
+    });
+    console.log(itemsRecord)
+    return itemsRecord;
 }
 
 export function addItem(items: Items, itemToAdd: Item) {
-    items.push(itemToAdd);
-}
-
-export function hasItem(items: Items, itemToFind: Item) {
-    return items.includes(itemToFind);
-}
-
-export function everyItem(items: Items, predicate: (value: "espresso", index: number, array: "espresso"[]) => unknown) {
-    return items.every(predicate);
+    items[itemToAdd] += 1;
 }
 
 export function getItemCount(items: Items, itemToCount: Item) {
+    return items.filter((item) => item === itemToCount).length;
+}
+
+function getItemCountInArray(items: Item[], itemToCount: Item) {
     return items.filter((item) => item === itemToCount).length;
 }
