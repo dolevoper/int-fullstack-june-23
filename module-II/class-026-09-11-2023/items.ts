d4ad77f67060e1d161ce224095b2e33994bd4824
Omer Dolev <omerdolev90@gmail.com>
2023-11-13 17:46:36 +0200

    modules solution

--- module-II/class-026-09-11-2023/items.ts
+++ module-II/class-026-09-11-2023/items.ts
@@ -1,23 +1,33 @@
-export const items = ["espresso", "milk"] as const;
+export const items = ["espresso", "milk", "ice"] as const;
 
 // export type Item = "espresso" | "milk";
 export type Item = typeof items[number];
 
-export type Items = Item[];
-// export type Items = Record<Item, number>;
+// export type Items = Item[];
+export type Items = Record<Item, number>;
 
 export function empty(): Items {
-    return [];
+    return {
+        espresso: 0,
+        milk: 0,
+        ice: 0
+    };
 }
 
 export function fromArray(items: Item[]): Items {
-    return items;
+    const result = empty();
+
+    for (const item of items) {
+        addItem(result, item);
+    }
+
+    return result;
 }
 
-export function addItem(items: Items, itemToAdd: Item) {
-    items.push(itemToAdd);
+export function addItem(items: Items, itemToAdd: Item): void {
+    items[itemToAdd]++;
 }
 
-export function getItemCount(items: Items, itemToCount: Item) {
-    return items.filter((item) => item === itemToCount).length;
+export function getItemCount(items: Items, itemToCount: Item): number {
+    return items[itemToCount];
 }
