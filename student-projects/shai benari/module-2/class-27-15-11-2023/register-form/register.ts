151e3503bfcb4287fa3c4ee64d1390e6ea1f37ea
shai-benari <s.benari1962@gmail.com>
2023-11-19 10:21:10 +0200

    register-end

--- student-projects/shai benari/module-2/class-27-15-11-2023/register-form/register.ts
+++ student-projects/shai benari/module-2/class-27-15-11-2023/register-form/register.ts
@@ -8,39 +8,39 @@ lable2.insertAdjacentHTML("beforebegin", '<label for="password"> Password</label
 const lable3 = document.getElementById('confirmPassword') as HTMLAnchorElement;
 lable3.insertAdjacentHTML("beforebegin", '<label for="confirmPassword"> Confirm Password</label>')
 
-// const checkInput = (event) => {
-// const errorEelement = Array.from(event.target.parentNode.querySelectorAll('span'));
-// if(event.target.value === " " && !errorEelement.length ){
-//    event.target.insertAdjacentHTML("afterend", '<span class="mesge" style="color: red;">Requierd</span>');
+const checkInput = (event) => {
+const errorEelement = Array.from(event.target.parentNode.querySelectorAll('span'));
+if(event.target.value === " " && !errorEelement.length ){
+   event.target.insertAdjacentHTML("afterend", '<span class="mesge" style="color: red;">Requierd</span>');
 
-// }
-// // if(errorEelement && event.target.value !== " "  ){
-// //     errorEelement.forEach(elem => elem.remove());
-// // }
-// }
-// document.getElementById("username")?.addEventListener('blur' , checkInput );
-// document.getElementById("password")?.addEventListener('blur' , checkInput );
-// document.getElementById("confirmPassword")?.addEventListener('blur' , checkInput );
+}
+if(errorEelement && event.target.value !== " "  ){
+    errorEelement.forEach(elem => elem.remove());
+}
+}
+document.getElementById("username")?.addEventListener('blur' , checkInput );
+document.getElementById("password")?.addEventListener('blur' , checkInput );
+document.getElementById("confirmPassword")?.addEventListener('blur' , checkInput );
 
-// document.getElementById('confirmPassword')?.addEventListener('blur',(e) =>{
-//     if(e.target !== document.getElementById("password")){
-//     e.target?.insertAdjacentHTML("afterend", '<span class="mesge" style="color: red;">Requierd</span>');
-//     }
-// });
+document.getElementById('confirmPassword')?.addEventListener('blur',(e) =>{
+    if(e.target !== document.getElementById("password")){
+    e.target?.insertAdjacentHTML("afterend", '<span class="mesge" style="color: red;">password dont much</span>');
+    }
+});
 
-// const btn = document.querySelector('button');
-// btn!.setAttribute('disabled', 'disabled');
-// document.getElementById("registrationForm")?.addEventListener('change',(event)=>{
-//     const formIsFilled = Array.from(document.querySelectorAll("input"))
-//     .every(input => input.value)
-//     if(formIsFilled){
-//        btn?.removeAttribute("disabled") 
-//     }else{
-//         btn?.setAttribute('disabled', 'disabled');
-//     }
+const btn = document.querySelector('button');
+btn!.setAttribute('disabled', 'disabled');
+document.getElementById("registrationForm")?.addEventListener('change',(event)=>{
+    const formIsFilled = Array.from(document.querySelectorAll("input"))
+    .every(input => input.value)
+    if(formIsFilled){
+       btn?.removeAttribute("disabled") 
+    }else{
+        btn?.setAttribute('disabled', 'disabled');
+    }
 //   console.log(formIsFilled)
 //   console.log(btn)
-// });
+});
 const form = document.getElementById('registrationForm');
 form?.addEventListener('submit', (e) => {
     e.preventDefault();
