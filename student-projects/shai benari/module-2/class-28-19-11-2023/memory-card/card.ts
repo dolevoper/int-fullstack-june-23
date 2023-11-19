b217fa4b30d442ce07e995fa1e63ee6b6b2cad7a
shai-benari <s.benari1962@gmail.com>
2023-11-19 13:20:14 +0200

    card array

---
+++ student-projects/shai benari/module-2/class-28-19-11-2023/memory-card/card.ts
@@ -0,0 +1,39 @@
+type Card = {
+    front: string,
+    back: string,
+};
+const cards: Card[] = [
+    {
+        front: "Pen",
+        back: "עט"
+    },
+    {
+        front: "man",
+        back: "איש"
+    },
+    {
+        front: "card",
+        back: "קלף",
+    },
+    {
+        front: "numbr",
+        back: "מספר",
+    },
+    {
+        front: "time",
+        back: "זמן",
+    }
+
+];
+let currenCardsIndx = 0;
+
+function displyCurrentCard(){
+    const currentCard = cards[currenCardsIndx];
+    document.querySelector("#currentCard .card__front")!.textContent = currentCard.front;
+    document.querySelector("#currentCard .card__back")!.textContent = currentCard.front;
+   
+console.log(cards[currenCardsIndx]);
+};
+
+displyCurrentCard();
+
