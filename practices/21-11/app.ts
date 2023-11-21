61140ddf0de7dcf7d229d0fee1c80f811c461f0f
gili <gilimena95@gmail.com>
2023-11-21 19:29:57 +0200

    WIP

--- practices/21-11/app.ts
+++ practices/21-11/app.ts
@@ -2,16 +2,37 @@ console.log("connected");
 
 const users = [
   { id: 1, email: "gili@gili.com", password: "Aa123456!", username: "gili" },
-  { id: 2, email: "lilach@gmail.com", password: "Qazwsx123456!", username: "lilach" },
+  {
+    id: 2,
+    email: "lilach@gmail.com",
+    password: "Qazwsx123456!",
+    username: "lilach",
+  },
 ];
 
 const onPickDate = (ev) => {
   // console.log(typeof(ev.target.value))
   const newDate = new Date(ev.target.value);
+  const today = new Date();
+
+  console.log(today.getMonth());
+  console.log(newDate.getMonth());
+  console.log(newDate.getTime())
+
+  if (today.getTime() > newDate.getTime()) {
+
+    console.log("past");
+  } else {
+    console.log("future")
+  }
 };
 
 function log(event) {
-  console.log(event.target.value);
+//   console.log(new Date().getTime())
+  
+  var id = "id" + Math.random().toString(16).slice(2)
+    
+  console.log(id)
 }
 
 const handleLogin = (event: SubmitEvent) => {
@@ -23,19 +44,19 @@ const handleLogin = (event: SubmitEvent) => {
     (user) => user.email === email && user.password === password
   );
   if (userDB) {
-    localStorage.setItem("userId", userDB.id.toString())
-    window.location.href = "./dashboard.html"
+    localStorage.setItem("userId", userDB.id.toString());
+    window.location.href = "./dashboard.html";
   }
 };
 
 const handleLoad = () => {
-    const userId = localStorage.getItem("userId")
-    const userDB = users.find((user) => user.id === Number(userId))
-    const usernameHello = document.querySelector("#username")
-    usernameHello?.innerHTML = `${userDB?.username}` 
-}
+  const userId = localStorage.getItem("userId");
+  const userDB = users.find((user) => user.id === Number(userId));
+  const usernameHello = document.querySelector("#username");
+  usernameHello?.innerHTML = `${userDB?.username}`;
+};
 
 const handleLogout = () => {
-    localStorage.removeItem("userId")
-    window.location.href = "./index.html"
-}
+  localStorage.removeItem("userId");
+  window.location.href = "./index.html";
+};
