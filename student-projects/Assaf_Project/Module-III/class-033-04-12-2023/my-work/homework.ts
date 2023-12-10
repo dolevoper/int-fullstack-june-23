0faebbe829aa44db35a725902792ec3d1f5ff1c7
Assaf Basri <Faskoman90@gmail.com>
2023-12-10 18:25:47 +0200

    feat(delete):

--- student-projects/Assaf_Project/Module-III/class-033-04-12-2023/my-work/homework.ts
+++ student-projects/Assaf_Project/Module-III/class-033-04-12-2023/my-work/homework.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync } from "fs";
 import * as readline from "node:readline";
 import { randomUUID } from "crypto";
 
@@ -6,10 +6,16 @@ type User = {
   id: string;
   firstName: string;
   lastName: string;
-  phoneNumber: string;
+  phoneNumber?: string;
 };
 
-const fileData = readFileSync("./users.txt", "utf-8");
+const filePath = "./users.json";
+
+if (!existsSync(filePath)) {
+  writeFileSync(filePath, "");
+}
+
+const fileData = readFileSync("./users.json", "utf-8");
 const fileContents: User[] = fileData.trim() ? JSON.parse(fileData) : [];
 
 const rl = readline.createInterface({
@@ -32,7 +38,23 @@ Available operations:
         console.log("list >>>");
         break;
       case "read":
-        console.log("read >>>");
+        rl.question("Enter user ID to show details: ", (input) => {
+          const matchingUser = fileContents.find((user) => user.id === input);
+
+          if (!matchingUser) {
+            console.log(`No user found with ID ${input}`);
+          } else {
+            console.log(
+              `Matching user's details:\nID: ${matchingUser.id}\nFirst Name: ${
+                matchingUser.firstName
+              }\nLast Name: ${matchingUser.lastName}\nPhone Number: ${
+                matchingUser.phoneNumber || "N/A"
+              }`
+            );
+          }
+
+          rl.close();
+        });
         break;
       case "create":
         rl.question("What is your first name? ", (firstNameAnswer) => {
@@ -40,13 +62,13 @@ Available operations:
             rl.question("What is your phone number? ", (phoneNumberAnswer) => {
               const newUser: User = {
                 id: randomUUID(),
-                firstName: firstNameAnswer.trim().toLowerCase(),
-                lastName: lastNameAnswer.trim().toLowerCase(),
+                firstName: capitalize(firstNameAnswer.trim().toLowerCase()),
+                lastName: capitalize(lastNameAnswer.trim().toLowerCase()),
                 phoneNumber: phoneNumberAnswer.trim(),
               };
               fileContents.push(newUser);
               writeFileSync(
-                "./users.txt",
+                "./users.json",
                 JSON.stringify(fileContents, null, 2)
               );
               console.log(
@@ -61,9 +83,60 @@ Available operations:
         console.log("update >>>");
         rl.close();
         break;
-      case "delete":
-        console.log("delete >>>");
+        case "delete":
+            rl.question("Enter user ID to DELETE: ", (input) => {
+              const matchingUserIndex = fileContents.findIndex((user) => user.id === input);
+          
+              if (!matchingUserIndex) {
+                console.log(`No user found with ID ${input}`);
+                rl.close();
+              } else {
+                const matchingUser = fileContents[matchingUserIndex];
+          
+                console.log(
+                  `Matching user's details:\nFirst Name: ${
+                    matchingUser.firstName
+                  }\nLast Name: ${matchingUser.lastName}\nPhone Number: ${
+                    matchingUser.phoneNumber || "N/A"
+                  }`
+                );
+          
+                rl.question("Are you sure you want to DELETE? (y/n) ", (answer) => {
+                  switch (answer.trim().toLowerCase()) {
+                    case "yes":
+                    case "y":
+                      fileContents.splice(matchingUserIndex, 1);
+          
+                      writeFileSync("./users.json", JSON.stringify(fileContents, null, 2));
+          
+                      console.log("User has been deleted!");
+                      break;
+                    case "no":
+                    case "n":
+                      console.log("Deletion canceled...");
+                      break;
+                  }
+                  rl.close();
+                });
+              }
+            });
+            break;
+      case "stop":
+      case "end":
+      case "quit":
+        console.log("Bye!");
+        rl.close();
         break;
+      default:
+        console.log(option + " is not an option...");
+        rl.close();
     }
   }
 );
+
+function capitalize(text: string) {
+  return text
+    .split(" ")
+    .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+    .join(" ");
+}
