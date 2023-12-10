95ed3ce904f508f71783458ed53f6398559a01aa
shai-benari <s.benari1962@gmail.com>
2023-12-11 01:23:21 +0200

    math

--- student-projects/shai benari/module-3/class-33-4-12-23/class-33-hp/user.ts
+++ student-projects/shai benari/module-3/class-33-4-12-23/class-33-hp/user.ts
@@ -29,7 +29,7 @@ let users: user[]=[
 }
 ];
 users.push({
-    id: Math.random().toString(16).slice(2,8),
+id: Math.random().toString(16).slice(2,8),
 firsName:  process.argv[2],
 lestName: process.argv[3],
 phoneNumber: process.argv[4],
@@ -42,10 +42,44 @@ const fileContents = readFileSync("./users.json", "utf-8");
 // console.log("The users are: " , fileContents);
 
 
-// for(let i =0; i<users.length; i++){
-//     let idList = Object.values(users[i]);
-//     console.log(idList)
-// }
+//  for(let i =0; i<users.length + 1; i++){
+//     let idValue = Object.values(users)[i];
+//     let idKey = Object.keys(users)[i];
+//     console.log();
+// };
+
+let ides = [''];
+let idVal = users.map((elem) => {
+    let getKeys = Object.keys(elem)[0];
+    let getvalue = Object.values(elem)[0];
+    ides.push(getvalue)
+  });
+ console.log(ides);
+
+
+
+
+// import * as readline from 'readline';
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('Is this example useful? [y/n] ', (answer:string) => {
+//     switch(answer.toLowerCase()) {
+//     case 'y':
+//         console.log('Super!');
+//         break;
+//     case 'n':
+//         console.log('Sorry!');
+//         break;
+//     default:
+//         console.log('Invalid answer!');
+//     }
+//     rl.close();
+// });
+
 
 import * as readline from 'readline';
 
@@ -54,16 +88,16 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Is this example useful? [y/n] ', (answer:string) => {
-    switch(answer.toLowerCase()) {
-    case 'y':
-        console.log('Super!');
-        break;
-    case 'n':
-        console.log('Sorry!');
-        break;
-    default:
-        console.log('Invalid answer!');
+rl.question('please enter id:  ', (answer:string) => {
+    for(let i =0; i<users.length+1; i++){
+        if(answer === ides[i]){
+            console.log(users[i-1]);
+        }
+        else{
+
+            console.log(`The id you entard ${answer} do not much ${ides[i]}`)
+        }
     }
+   
     rl.close();
 });
