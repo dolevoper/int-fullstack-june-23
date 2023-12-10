655dc527b72de2e1e92c1b0c42ceafa02f27edac
Assaf Basri <Faskoman90@gmail.com>
2023-12-10 17:06:14 +0200

    open file to get options

--- student-projects/Assaf_Project/Module-III/class-033-04-12-2023/my-work/homework.ts
+++ student-projects/Assaf_Project/Module-III/class-033-04-12-2023/my-work/homework.ts
@@ -17,42 +17,53 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-
-
-switch (process.argv[2]?.toLowerCase()) {
-  case "list":
-    console.log("list >>>");
-    break;
-  case "read":
-    console.log("read >>>")
-    break;
-  case "create":
-    rl.question("What is your first name? ", (firstNameAnswer) => {
-      rl.question("What is your last name? ", (lastNameAnswer) => {
-        rl.question("What is your phone number? ", (phoneNumberAnswer) => {
-          const newUser: User = {
-            id: randomUUID(),
-            firstName: firstNameAnswer.trim().toLowerCase(),
-            lastName: lastNameAnswer.trim().toLowerCase(),
-            phoneNumber: phoneNumberAnswer.trim(),
-          };
-          fileContents.push(newUser);
-          writeFileSync("./users.txt", JSON.stringify(fileContents, null, 2));
-          console.log(`
-            New user added:
-            Name: ${newUser.firstName} ${newUser.lastName}
-            Phone number: ${newUser.phoneNumber}
-            ID: ${newUser.id}`);
-          rl.close();
+rl.question(
+  `Welcome to the User Management App!
+Available operations:
+- List - Show a list of all the user ID's
+- Read - Given a user ID, show the user's full information
+- Create - Create a new user
+- Update - Update all the given details for the user with the given ID
+- Delete - Given a user ID, delete the user from the system
+`,
+  (option) => {
+    switch (option.toLowerCase().trim()) {
+      case "list":
+        console.log("list >>>");
+        break;
+      case "read":
+        console.log("read >>>");
+        break;
+      case "create":
+        rl.question("What is your first name? ", (firstNameAnswer) => {
+          rl.question("What is your last name? ", (lastNameAnswer) => {
+            rl.question("What is your phone number? ", (phoneNumberAnswer) => {
+              const newUser: User = {
+                id: randomUUID(),
+                firstName: firstNameAnswer.trim().toLowerCase(),
+                lastName: lastNameAnswer.trim().toLowerCase(),
+                phoneNumber: phoneNumberAnswer.trim(),
+              };
+              fileContents.push(newUser);
+              writeFileSync(
+                "./users.txt",
+                JSON.stringify(fileContents, null, 2)
+              );
+              console.log(
+                `New user added!\nName: ${newUser.firstName} ${newUser.lastName}\nPhone number: ${newUser.phoneNumber}\nID: ${newUser.id}`
+              );
+              rl.close();
+            });
+          });
         });
-      });
-    });
-    break;
-  case "update":
-    console.log("update >>>");
-    rl.close();
-    break;
-  case "delete":
-    console.log("delete >>>");
-    break;
-}
+        break;
+      case "update":
+        console.log("update >>>");
+        rl.close();
+        break;
+      case "delete":
+        console.log("delete >>>");
+        break;
+    }
+  }
+);
