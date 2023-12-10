e1b0fb268d2802740f2c73a4b19be16be8a64914
shai-benari <s.benari1962@gmail.com>
2023-12-11 01:23:21 +0200

    math

--- student-projects/shai benari/module-3/class-33-4-12-23/server.ts
+++ student-projects/shai benari/module-3/class-33-4-12-23/server.ts
@@ -1,6 +1,20 @@
-let x = 2
-let y = 3
-console.log(x+y)
-console.log("helow world");
-const add = 5+6;
-console.log(`add result : ${add} ` );
+// let x = 2
+// let y = 3
+// console.log(x+y)
+// console.log("helow world");
+// const add = 5+6;
+// console.log(`add result : ${add} ` );
+
+// import os from "os";
+
+// import path from "path";
+
+import { readFileSync } from "fs";
+
+import {add, substract, devide, multiply} from './math.js';
+
+
+console.log( "add :" ,add(6,2));
+console.log( "substract :" , substract(6,2));
+console.log("devide :" ,devide(6,2));
+console.log( "multiply :" , multiply(6,2));
