13608e522b6a32383581553c70b7e6edfe85243f
Lilach E <lilachelayza@gmail.com>
2023-12-11 17:34:48 +0200

    class-033

---
+++ student-projects/lilach-elayza/module-III/class-033-04-12-2023/index.ts
@@ -0,0 +1,15 @@
+// console.log("hello world");
+
+// const number = 1 + 5;
+
+// console.log(`Here is a number: ${number}`);
+
+// // Doesn't work in node!
+// // localStorage.setItem("omer", "5");
+// // console.log(localStorage.getItem("omer"));
+
+// console.log(process.argv);
+
+// console.log("hello", process.argv[2]);
+
+console.log(Number(process.argv[2]) + Number(process.argv[3]));
