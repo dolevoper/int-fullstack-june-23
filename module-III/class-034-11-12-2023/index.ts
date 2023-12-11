155443e4a1146c184c6b10c75b8c396cae4eb9ef
Omer Dolev <omerdolev90@gmail.com>
2023-12-11 19:31:42 +0200

    more complex server

--- module-III/class-034-11-12-2023/index.ts
+++ module-III/class-034-11-12-2023/index.ts
@@ -2,7 +2,22 @@ import { createServer } from "http";
 
 const server = createServer((req, res) => {
     console.log(req.url);
-    res.write("<h1>hello world!</h1>");
+
+    res.setHeader("Content-Type", "text/html");
+
+    if (req.url === "/") {
+        res.write("<h1>hello world!</h1>");
+    }
+
+    const urlParts = req.url?.split("/") ?? [];
+
+    if (urlParts.length === 3 && urlParts[1] === "count" && !isNaN(Number(urlParts[2]))) {
+        res.write(`<h1>${urlParts[2]}</h1>`);
+    } else {
+        res.writeHead(404);
+        res.write("<p>This page does not exist</p>");
+    }
+
     res.end();
 });
 
