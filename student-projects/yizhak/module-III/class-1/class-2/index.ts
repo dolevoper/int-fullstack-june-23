bc966dc67c338a56b26c4e25d0c17aa9afa156a9
yizhak148 <yizhak208@gmail.com>
2023-12-12 00:14:42 +0200

    index.ts

--- student-projects/yizhak/module-III/class-1/class-2/index.ts
+++ student-projects/yizhak/module-III/class-1/class-2/index.ts
@@ -1,8 +1,10 @@
 import { createServer } from "http";
 
 const server = createServer((req, res) => {
+    //ה"רק" זה הבקשה, וה"רס" זה התשובה ואפשר לגשת אליהם
     console.log(req.url)
     res.write("<h1>hello world!</h1>");
+    //סוגר את הטעינה של האתר יעני האתר לא מחכה לתשובה יותר
     res.end();
 });
 
