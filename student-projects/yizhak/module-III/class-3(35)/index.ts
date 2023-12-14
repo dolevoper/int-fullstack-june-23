cc7c76335a401e102758634ec7aea1617eb98aea
yizhak148 <yizhak208@gmail.com>
2023-12-14 19:59:12 +0200

    w

--- student-projects/yizhak/module-III/class-3(35)/index.ts
+++ student-projects/yizhak/module-III/class-3(35)/index.ts
@@ -2,24 +2,42 @@ import { readFileSync } from "fs";
 import { createServer } from "http";
 import express from "express";
 
+const todosFilePath = "./todos.json";
 const todos = JSON.parse(readFileSync("./todos.json", "utf-8")) as string[];
 
 const app = express();
 let counter = 0;
 app.set("view engine", "ejs");
 
+const logRequests = (prefix = ""): express.RequestHandler => (req, _, next) => {
+    console.log(prefix, req.method, req.url);
+    next();
+}
 app.use((req, res, next) => {
     console.log(req.method, req.url);
     next();
 });
+app.use(logRequests("Todos App"));
 
 app.get("/", (req, res) => {
     res.render("todos", { todos, counter: counter++});
 });
 
-app.use((req, res) => {
-    counter++
-})
+app.get("/addTodo", (req, res) => {
+    const newTodo = req.query.todo?.toString();
+
+    if (!newTodo) {
+        res.status(400);
+        res.send("Must provide a todo to add");
+        return;
+    }
+
+    todos.push(newTodo);
+    writeFileSync(todosFilePath, JSON.stringify(todos));
+
+    res.redirect("/");
+});
+
 
 const server = createServer(app);
 
