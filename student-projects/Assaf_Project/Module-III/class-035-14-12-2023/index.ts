e80d45b0bccccdb52839247563e2ca2cf0e703fc
Assaf Basri <Faskoman90@gmail.com>
2023-12-14 19:57:55 +0200

    some more class work

--- student-projects/Assaf_Project/Module-III/class-035-14-12-2023/index.ts
+++ student-projects/Assaf_Project/Module-III/class-035-14-12-2023/index.ts
@@ -1,26 +1,45 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import { createServer } from "http";
 import express from "express";
 
-const todos = JSON.parse(readFileSync("./todos.json", "utf-8")) as string[];
-
-let counter = 0
+const todosFilePath = "./todos.json";
+const todos = JSON.parse(readFileSync(todosFilePath, "utf-8")) as string[];
 
 const app = express();
 
+let visits = 0;
+
 app.set("view engine", "ejs");
 
-app.use((req, _res, next) => {
-    console.log(req.method, req.url);
+const logRequests =
+  (prefix = ""): express.RequestHandler =>
+  (req, _, next) => {
+    console.log(prefix, req.method, req.url);
     next();
+  };
+
+app.use(logRequests("Todos App"));
+
+app.get("/", (_, res) => {
+  res.render("todos", { todos, visits: ++visits });
 });
 
-app.get("/", (_req, res) => {
-    counter++
-    res.render("todos", { todos, counter });
+app.get("/addTodo", (req, res) => {
+  const newTodo = req.query.todo?.toString();
+
+  if (!newTodo) {
+    res.status(400);
+    res.send("Must provide a todo to add");
+    return;
+  }
+
+  todos.push(newTodo);
+  writeFileSync(todosFilePath, JSON.stringify(todos));
+
+  res.redirect("/");
 });
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 const server = createServer(app);
 
