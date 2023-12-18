6428b5a11f8f00264ec92d89d4fc1ac7040e36d1
Assaf Basri <Faskoman90@gmail.com>
2023-12-18 13:51:19 +0200

    toggleBtn working

--- student-projects/Assaf_Project/Module-III/class-035-14-12-2023/my_work/index.ts
+++ student-projects/Assaf_Project/Module-III/class-035-14-12-2023/my_work/index.ts
@@ -2,9 +2,16 @@ import { readFileSync, writeFileSync } from "fs";
 import { createServer } from "http";
 import express from "express";
 import { urlencoded } from "body-parser";
+import { randomUUID } from "crypto";
+
+type Todo = {
+  id: string;
+  text: string;
+  isDone: boolean;
+};
 
 const todosFilePath = "./todos.json";
-const todos = JSON.parse(readFileSync(todosFilePath, "utf-8")) as string[];
+let todos = JSON.parse(readFileSync(todosFilePath, "utf-8")) as Todo[];
 
 const app = express();
 
@@ -12,38 +19,69 @@ let visits = 0;
 
 app.set("view engine", "ejs");
 
-const logRequests = (prefix = ""): express.RequestHandler => (req, _, next) => {
+const logRequests =
+  (prefix = ""): express.RequestHandler =>
+  (req, _, next) => {
     console.log(prefix, req.method, req.url);
     next();
-};
+  };
 
-app.use(urlencoded());
+app.use(urlencoded({ extended: true }));
 
 app.use(logRequests("Todos App"));
 
 app.get("/", (_, res) => {
-    res.render("todos", { todos, visits: ++visits });
+  res.render("todos", { todos, visits: ++visits });
 });
 
 app.post("/addTodo", (req, res) => {
-    const newTodo = req.body.todo?.toString();
+  const newTodo = req.body.todo;
 
-    if (!newTodo) {
-        res.status(400);
-        res.send("Must provide a todo to add");
-        return;
-    }
+  if (!newTodo) {
+    res.status(400);
+    res.send("Must provide a todo to add");
+    return;
+  }
 
-    todos.push(newTodo);
-    writeFileSync(todosFilePath, JSON.stringify(todos));
+  todos.push({
+    id: randomUUID(),
+    text: newTodo,
+    isDone: false,
+  });
 
-    res.redirect("/");
+  writeFileSync(todosFilePath, JSON.stringify(todos));
+
+  res.redirect("/");
+});
+
+app.post("/toggle", (req, res) => {
+  const toggleId = req.body.id;
+  console.log(toggleId);
+
+  if (!toggleId) {
+    res.status(400);
+    res.send("Must provide Todo ID");
+    return;
+  }
+
+  const todo = todos.find((todo) => todo.id === toggleId);
+
+  if (!todo) {
+    res.status(400);
+    res.send(`Todo ${toggleId} not found`);
+    return;
+  }
+
+  todo.isDone = !todo.isDone;
+  saveTodos();
+
+  res.redirect("/");
 });
 
 app.post("/resetTodos", (_, res) => {
-    todos.splice(0, todos.length);
-    writeFileSync(todosFilePath, "[]");
-    res.redirect("/");
+  todos.splice(0, todos.length);
+  writeFileSync(todosFilePath, "[]");
+  res.redirect("/");
 });
 
 app.use(express.static("public"));
@@ -51,3 +89,7 @@ app.use(express.static("public"));
 const server = createServer(app);
 
 server.listen(3000, () => console.log("server listening on port 3000"));
+
+function saveTodos() {
+  writeFileSync(todosFilePath, JSON.stringify(todos));
+}
