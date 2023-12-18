e2cfe46cfc81991061ebab3da10f4c65eee12dd5
Assaf Basri <Faskoman90@gmail.com>
2023-12-18 10:32:49 +0200

    added class 35 files

--- student-projects/Assaf_Project/Module-III/class-035-14-12-2023/index.ts
+++ student-projects/Assaf_Project/Module-III/class-035-14-12-2023/index.ts
@@ -1,6 +1,7 @@
 import { readFileSync, writeFileSync } from "fs";
 import { createServer } from "http";
 import express from "express";
+import { urlencoded } from "body-parser";
 
 const todosFilePath = "./todos.json";
 const todos = JSON.parse(readFileSync(todosFilePath, "utf-8")) as string[];
@@ -11,32 +12,49 @@ let visits = 0;
 
 app.set("view engine", "ejs");
 
-const logRequests =
-  (prefix = ""): express.RequestHandler =>
-  (req, _, next) => {
+const logRequests = (prefix = ""): express.RequestHandler => (req, _, next) => {
     console.log(prefix, req.method, req.url);
     next();
-  };
+};
+
+app.use(urlencoded());
 
 app.use(logRequests("Todos App"));
 
 app.get("/", (_, res) => {
-  res.render("todos", { todos, visits: ++visits });
+    res.render("todos", { todos, visits: ++visits });
 });
 
-app.get("/addTodo", (req, res) => {
-  const newTodo = req.query.todo?.toString();
+app.post("/addTodo", (req, res) => {
+    const newTodo = req.body.todo?.toString();
 
-  if (!newTodo) {
-    res.status(400);
-    res.send("Must provide a todo to add");
-    return;
-  }
+    if (!newTodo) {
+        res.status(400);
+        res.send("Must provide a todo to add");
+        return;
+    }
 
-  todos.push(newTodo);
-  writeFileSync(todosFilePath, JSON.stringify(todos));
+    todos.push(newTodo);
+    writeFileSync(todosFilePath, JSON.stringify(todos));
 
-  res.redirect("/");
+    res.redirect("/");
+});
+
+app.post("/toggle", (req, res) => {
+    const updatedToggle = todos
+    .replace(`{counter}`, counter)
+    .replace(`{title}`, "Users ID's: ")
+    .replace(`{users}`, usersId.join(""));
+
+  res.send(updatedHTML);
+
+    res.redirect("/");
+})
+
+app.post("/resetTodos", (_, res) => {
+    todos.splice(0, todos.length);
+    writeFileSync(todosFilePath, "[]");
+    res.redirect("/");
 });
 
 app.use(express.static("public"));
