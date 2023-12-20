4b24cbd954b5f9093af4c0e33bd5118ff7aa44e3
shai-benari <s.benari1962@gmail.com>
2023-12-20 10:03:18 +0200

    exer 34 first stage

--- student-projects/shai benari/module-3/class-035-14-12-2023/class-exer/index.ts
+++ student-projects/shai benari/module-3/class-035-14-12-2023/class-exer/index.ts
@@ -6,17 +6,28 @@ import { todo } from "node:test";
 const todos = JSON.parse(readFileSync("./todos.json","utf-8")) as string[];
 
 const app = express();
+
 app.set("view engine" , "ejs");
+let visits = 0
 
 app.use((req, res, next) =>{
     console.log(req.method, req.url);
     next();
 });
-app.get("/",(req, res) =>{
+
+
+app.get("/",(req, res, next) =>{
     res.render("Todos", {count: todos.length, todos});
     // res.send(`<ul> ${todos.map((todo)=> `<li>${todo}</li>`).join("\n\t")}<ul>`);
-
+   next();
 });
+
+// app.post("/",(req, res, next) =>{
+//     res.render("Todos", {todos, visits: ++visits});
+   
+// });
+
+
 
 const server = createServer(app);
 
