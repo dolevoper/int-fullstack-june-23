411f96cecc51491ace987eb4668a9650757a6673
Ayala p <84629328+123456249@users.noreply.github.com>
2024-01-11 18:30:28 +0200

    Ayala/hp/class 033 (#225)
    * hp class-017
    
    * hp-class-017
    
    * add
    
    * class-017
    
    * gi
    
    * ayala\hp\class-033

---
+++ student-projects/Ayala-perlovich/module-lll/class-033/count-lines.ts
@@ -0,0 +1,14 @@
+import {readFileSync} from "fs"
+
+const fileContents = readFileSync("./input.txt", "utf-8");
+
+console.log(fileContents.split("\n").length);
+
+console.log(fileContents);
+
+
+
+  
+
+
+
